avsm/unpac

commit 3d8346dc436c87d6b0a0337617ae2364231da54b

Added two-argument form of add-feed

vendor/opam/0install-solver/zeroinstall/cmd/remove_feed.py
@@ -16,7 +16,7 @@ add_options = add_feed.add_options
 def handle(config, options, args):
 	if len(args) == 2:
 		iface = config.iface_cache.get_interface(model.canonical_iface_uri(args[0]))
-		feed_url = args[1]
+		feed_url = model.canonical_iface_uri(args[1])
 
 		feed_import = add_feed.find_feed_import(iface, feed_url)
 		if not feed_import:

vendor/opam/0install-solver/0install.1
@@ -1,4 +1,4 @@
-.TH 0INSTALL 1 "2011" "Thomas Leonard" ""
+.TH 0INSTALL 1 "2012" "Thomas Leonard" ""
 .SH NAME
 0install \(em a decentralised software installation system
 
@@ -22,7 +22,7 @@
 
 .B 0install list \fBPATTERN\fP
 
-.B 0install add-feed \fBFEED\fP
+.B 0install add-feed [\fBINTERFACE\fP] \fBFEED\fP
 
 .B 0install remove-feed [\fBINTERFACE\fP] \fBFEED\fP
 
@@ -258,7 +258,7 @@ signature, as when fetching from the network.
 It is also useful when installing a feed from a CD or similar. Note: to create
 a full bundle, for archiving or distribution on CD, see 0export(1).
 
-.SS 0install add-feed FEED
+.SS 0install add-feed [INTERFACE] FEED
 
 .PP
 Register an additional source of implementations (versions) of a program.
@@ -376,7 +376,7 @@ See the 0store(1) man page for more information.
 
 .SH LICENSE
 .PP
-Copyright (C) 2011 Thomas Leonard.
+Copyright (C) 2012 Thomas Leonard.
 
 .PP
 You may redistribute copies of this program under the terms of the GNU Lesser General Public License.

vendor/opam/0install-solver/zeroinstall/cmd/add_feed.py
@@ -12,7 +12,7 @@ from zeroinstall.support import tasks
 from zeroinstall.cmd import UsageError
 from zeroinstall.injector import model, writer
 
-syntax = "NEW-FEED"
+syntax = "[INTERFACE] NEW-FEED"
 
 def add_options(parser):
 	parser.add_option("-o", "--offline", help=_("try to avoid using the network"), action='store_true')
@@ -24,7 +24,18 @@ def find_feed_import(iface, feed_url):
 	return None
 
 def handle(config, options, args, add_ok = True, remove_ok = False):
-	if len(args) != 1: raise UsageError()
+	if len(args) == 2:
+		iface = config.iface_cache.get_interface(model.canonical_iface_uri(args[0]))
+		feed_url = model.canonical_iface_uri(args[1])
+
+		feed_import = find_feed_import(iface, feed_url)
+		if feed_import:
+			raise SafeException(_('Interface %(interface)s already has a feed %(feed)s') %
+						{'interface': iface.uri, 'feed': feed_url})
+		iface.extra_feeds.append(model.Feed(feed_url, arch = None, user_override = True))
+		writer.save_interface(iface)
+		return
+	elif len(args) != 1: raise UsageError()
 
 	x = args[0]
 

commit 22acb43b219f1313e88a855ccaa882316d2ba555

Fixed remove-feed for the case where the file no longer exists
Was broken by 3d8346dc43.

vendor/opam/0install-solver/zeroinstall/cmd/remove_feed.py
@@ -16,7 +16,10 @@ add_options = add_feed.add_options
 def handle(config, options, args):
 	if len(args) == 2:
 		iface = config.iface_cache.get_interface(model.canonical_iface_uri(args[0]))
-		feed_url = model.canonical_iface_uri(args[1])
+		try:
+			feed_url = model.canonical_iface_uri(args[1])
+		except SafeException:
+			feed_url = args[1]		# File might not exist any longer
 
 		feed_import = add_feed.find_feed_import(iface, feed_url)
 		if not feed_import: