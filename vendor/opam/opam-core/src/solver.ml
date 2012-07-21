avsm/unpac

commit 8a025c0144c5bc5bd46b150e8f1bdf22ed47e487

[solver optimization] New packages returned by the DOSE solver (not present in the request) could have an old version (among available versions). We introduce an optimization trying to always return the last version of newly computed packages.
Note that this remains an optimization as the solving problem being NP. For instance, the "opam upgrade" performed in #43 just after the "opam install" succeeds but could be unsuccessful in some other cases.
However, this optimization should be useful in practice as the user targets the last version in general.

vendor/opam/opam-core/src/solver.ml
@@ -262,11 +262,99 @@ end = struct
       log "INTERNAL(resolve) %s" (String.concat " " (List.map string_of_cudf_package l));
         try 
           let diff = Common.CudfDiff.diff univ_init (Cudf.load_universe l) in
-          Some [f_diff diff]
+          Some (f_diff diff)
         with
           Cudf.Constraint_violation _ -> 
             log "INTERNAL(resolve) constraint violation";
             None
+
+  let resolve_with_optimization = 
+    let nb_iter_max = 20 in 
+    (** maximum number of request we can send to the solver 
+        before the algorithm stops and returns the current solution *)
+
+    fun f_diff univ req ->
+    let string_of_c l = 
+      String.concat " " (List.map (string_of_internal_action string_of_cudf_package) l) in
+
+    match req, resolve f_diff univ req with
+      | { i_wish_remove = [] ; i_wish_upgrade = [] ; _ }, Some ans -> 
+
+        let rec aux nb_iter univ l_ans req ans =
+          match
+            List.partition
+              (function
+                | I_to_change (None, p) -> List.exists (fun (p_wish, _) -> p.Cudf.package = p_wish) req
+                | I_to_change (Some _, _)
+                | I_to_delete _
+                | I_to_recompile _ -> true) 
+              ans
+          with
+          | _, [] -> l_ans
+          | l_p_ans, l_new_pkg ->
+              log "INTERNAL(optimization) requested=%s, new_computed=%s" (string_of_c l_p_ans) (string_of_c l_new_pkg);
+
+              (** we preserve versions of packages that were explicitely requested *)
+              let l_p_ans = 
+                Utils.filter_map
+                  (function 
+                    | I_to_change (_, p)
+                    | I_to_recompile p -> Some (p.Cudf.package, Some (`Eq, p.Cudf.version))
+                    | I_to_delete _ -> None)
+                  l_p_ans in
+
+              (** we compute the last version of every new package (not requested) *)
+              let to_continue, l_new_pkg = 
+                let univ_map = (** similar as [univ] but represented with map *)
+                  List.fold_left (fun map p -> 
+                    Utils.StringMap.add p.Cudf.package
+                      (Utils.IntMap.add 
+                         p.Cudf.version
+                         p
+                         (try Utils.StringMap.find p.Cudf.package map with Not_found -> Utils.IntMap.empty))
+                      map) Utils.StringMap.empty (Cudf.get_packages univ) in
+                
+                let find_max p univ =
+                  snd (Utils.IntMap.max_binding (Utils.StringMap.find p.Cudf.package univ)) in
+
+                List.fold_left 
+                  (fun (to_continue, l_new_pkg) -> function
+                    | I_to_change (None, p) -> 
+                        let v_max = (find_max p univ_map).Cudf.version in
+                        to_continue || p.Cudf.version <> v_max, (p.Cudf.package, Some (`Eq, v_max)) :: l_new_pkg
+                    | I_to_change (Some _, _)
+                    | I_to_delete _
+                    | I_to_recompile _ -> assert false (* already filtered before *))
+                  (false, []) 
+                  l_new_pkg in
+
+              if to_continue then 
+                let i_wish_upgrade = l_p_ans @ l_new_pkg in
+                match resolve f_diff univ { i_wish_install = [] ; i_wish_remove = [] ; i_wish_upgrade } with
+                  | None -> 
+                      let () = Globals.warning "INTERNAL(optimization) no solution" in
+                      l_ans
+                  | Some ans_new -> 
+                      if ans_new = ans then (* NOTE the strict equality is a bit strong. 
+                                               For instance, we can just require that [ans_new] is a permutation of [ans]. *)
+                        l_ans
+                      else if nb_iter > nb_iter_max then
+                        let () = Globals.warning "number of iteration exceeded" in
+                        (* We stop the loop. 
+                           Even if it is correct to stop at any time, 
+                           determine if the equality above is too strong
+                           or if we have reached a cycle of answer (i1, i2, ..., iN, i1) so that (forall J, iJ <> iJ+1). *)
+                        l_ans
+                      else
+                        aux (succ nb_iter) univ (ans_new :: l_ans) i_wish_upgrade ans_new
+              else
+                let _ = log "INTERNAL(optimization) the answer does not contain some package with a more recent version to try" in
+                l_ans in
+        
+        Some (aux 0 univ [ans] req.i_wish_install ans)
+
+      | _, Some ans -> Some [ans]
+      | _, None -> None
 
   let resolve_diff =
     let f_diff diff =
@@ -292,7 +380,7 @@ end = struct
         | None      , None       -> acc
       ) diff []
     in
-    resolve f_diff
+    resolve_with_optimization f_diff
 
 end
 
@@ -451,7 +539,7 @@ struct
       wish_upgrade = List.filter filter req.wish_upgrade;
     } in
     log "universe=%s request=<%s>"
-      (string_of_packages l_pkg_pb)
+      (string_of_packages (List.rev l_pkg_pb))
       (string_of_request req);
     get_table l_pkg_pb 
       (fun table pkglist ->

vendor/opam/opam-core/src/utils.ml
@@ -28,6 +28,7 @@ let set_of_list empty add l =
 let map_of_list empty add = set_of_list empty (fun (k, v) -> add k v)
 
 module IntMap = Map.Make(struct type t = int let compare = compare end)  
+module StringMap = Map.Make(struct type t = string let compare = compare end)  
 module IntSet = Set.Make(struct type t = int let compare = compare end)  
 
 let (|>) f g x = g (f x) 

commit 29edf37a39014fe5d77bece9e9ee9d60b4973bac

Fix installation issue, regression introduced by 8a025c01. The installed packages which were not modified by the install were unfortunately removed ...

vendor/opam/opam-core/src/solver.ml
@@ -335,8 +335,11 @@ end = struct
                   (false, []) 
                   l_new_pkg in
 
-              if to_continue then 
-                let i_wish_upgrade = l_p_ans @ l_new_pkg in
+              if to_continue then
+                let i_wish_upgrade =
+                  let l = l_p_ans @ l_new_pkg in
+                  let s = List.filter (fun (p,_) -> not (List.mem_assoc p l)) req in
+                  l @ s in
                 match resolve f_diff univ { i_wish_install = [] ; i_wish_remove = [] ; i_wish_upgrade } with
                   | None -> 
                       let () = Globals.warning "INTERNAL(optimization) no solution" in