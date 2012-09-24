avsm/unpac

commit 8e7b296e53f32ff1f2f6be3a5165064bedc42996

When internet is down, do not remove urls.txt on update in curl repositories. This will make subsequent installations to continue to work in that case.

vendor/opam/opam-core/src/repo/curl.ml
@@ -18,6 +18,7 @@ type state = {
 let state_cache = ref []
 
 let index_file local_path = local_path // "urls.txt"
+let index_file_save local_path = local_path // "urls.txt.0"
 let index_archive local_path = local_path // "index.tar.gz"
 
 let make_state ~download_index remote_path =
@@ -29,11 +30,18 @@ let make_state ~download_index remote_path =
     let local_path = Path.R.root local_repo in
     let remote_index_file = remote_path // "urls.txt" in
     let local_index_file = index_file local_path in
+    let local_index_file_save = index_file_save local_path in
     let remote_index_archive = remote_path // "index.tar.gz" in
     let index =
       if download_index then (
-        Filename.remove local_index_file;
-        Filename.download remote_index_file local_path
+        Filename.move local_index_file local_index_file_save;
+        try
+          let file = Filename.download remote_index_file local_path in
+          Filename.remove local_index_file_save;
+          file;
+        with e ->
+          Filename.move local_index_file_save local_index_file;
+          raise e
       ) else
         local_index_file in
     let remote_local, local_remote, local_files, file_permissions, file_digests =

commit d3f1964aa692ef865c260d8225dc83c04e1c7105

[init] fix regression in the curl backend introduced by 8e7b296e53f32ff1f2f6be3a5165064bedc42996
This was causing 'opam init' to fail on fresh installs (*very very very very* bad regression)

vendor/opam/opam-core/src/repo/curl.ml
@@ -34,13 +34,15 @@ let make_state ~download_index remote_path =
     let remote_index_archive = remote_path // "index.tar.gz" in
     let index =
       if download_index then (
-        Filename.move local_index_file local_index_file_save;
+        if Filename.exists local_index_file then
+          Filename.move local_index_file local_index_file_save;
         try
           let file = Filename.download remote_index_file local_path in
           Filename.remove local_index_file_save;
           file;
         with e ->
-          Filename.move local_index_file_save local_index_file;
+          if Filename.exists local_index_file_save then
+            Filename.move local_index_file_save local_index_file;
           raise e
       ) else
         local_index_file in