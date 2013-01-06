avsm/unpac

commit 193d7c1377db0a1aca6dabf777d188fa4f6d82fc

[remove] if the remove command only use 'ocamlfind', then it is not necessary to extract the archive again.

vendor/opam/opam-core/src/client/opamSolution.ml
@@ -205,7 +205,7 @@ let extract_package t nv =
       | Result _
       | Up_to_date _  -> ()
     ) else
-      OpamGlobals.msg "The pinned package has already been initialized, using the cached version.\n";
+      OpamGlobals.msg "Synchronization: nothing to do as the pinned package has already been initialized.\n";
     let _files = OpamState.with_repository t nv (fun repo _ ->
       OpamFilename.in_dir pinned_dir (fun () -> OpamRepository.copy_files repo nv)
     ) in
@@ -233,7 +233,15 @@ let proceed_to_delete ~rm_build t nv =
       (* We try to run the remove scripts in the folder where it was extracted
          If it does not exist, we try to download and extract the archive again,
          if that fails, we don't really care. *)
-      if not (OpamFilename.exists_dir p_build) && OpamState.mem_repository t nv then (
+      (* We also use a small hack: if the remove command is simply 'ocamlfind remove xxx'
+         then, no need to extract the archive again. *)
+      let use_ocamlfind = function
+        | [] -> true
+        | "ocamlfind" :: _ -> true
+        | _ -> false in
+      if not (OpamFilename.exists_dir p_build)
+      && OpamState.mem_repository t nv
+      && not (List.for_all use_ocamlfind remove) then (
         try extract_package t nv
         with _ -> OpamFilename.mkdir p_build;
       );

commit dafb7296801db186fd49f08314b564ade43e5e90

Really fix the weird uninstall problem
I've finally nailed down issue #348 (and #362). The issue was indeed caused by 193d7c1377db0a1aca6dabf777d188fa4f6d82fc:
* This commit is implementing an useful optimization: in case the remove command is 'ocamlfind remove XXX' only, then OPAM don't create ~/.opam/<switch>/build/<pkg> anymore. Which means increased uninstall time.
* However, the uninstall command was still run in ~/.opam/<switch>/build/<pkg>. If that directory does not exists, the command was just dropped silently.
* OPAM automatically removes ~/.opam/<switch>/lib/<pkg>, so when the package was not using C bindings (with stub files in ~/.opam/<switch>/lib/stublibs) the bug was hidden
* Using OPAM with OPAMKEEPBUILDIR=1 (which means that the build dir wasn't removed during upgrades) hides the bug

So now I've fixed the bug:
* when we do an exec in a given dir, OPAM fails if the dir doesn't exist
* on uninstall, if the build dir is not there run the uninstall command at the OPAM root

This should fix #362

vendor/opam/opam-core/src/client/opamSolution.ml
@@ -256,14 +256,18 @@ let proceed_to_delete ~rm_build t nv =
       && OpamState.mem_repository t nv
       && not (List.for_all use_ocamlfind remove) then (
         try extract_package t nv
-        with _ -> OpamFilename.mkdir p_build;
+        with _ -> ()
       );
+      let exec_dir =
+        if OpamFilename.exists_dir p_build
+        then p_build
+        else t.root in
       try
         OpamGlobals.msg "%s\n" (string_of_commands remove);
         OpamFilename.exec
           ~add_to_env:env.add_to_env
           ~add_to_path:[OpamPath.Switch.bin t.root t.switch]
-          p_build
+          exec_dir
           remove
       with _ ->
         ();

vendor/opam/opam-core/src/core/opamFilename.ml
@@ -61,7 +61,7 @@ let in_dir dirname fn =
     OpamGlobals.error_and_exit "%s does not exist!" dirname
 
 let exec dirname ?(add_to_env=[]) ?(add_to_path=[]) cmds =
-  OpamSystem.in_dir (Dir.to_string dirname)
+  in_dir dirname
     (fun () ->
       OpamSystem.commands
         ~add_to_env