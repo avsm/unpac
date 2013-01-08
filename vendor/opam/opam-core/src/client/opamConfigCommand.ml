avsm/unpac

commit 7cc3308db136b8c5353c5f08fef18afddeb80898

Fix a bad bug with wrong environment setting.
Sometime, it 'opam config env' hadn't been called in the current shell, some package build was failing miserably. This commit clean-up a lot of small things in the management of environements, so now 'opam install XXX' should always work even if the user has a wrong environment set-upt.

vendor/opam/opam-core/src/client/opamConfigCommand.ml
@@ -240,23 +240,15 @@ let config_compil t c =
   log "OUTPUT: %S" output;
   OpamGlobals.msg "%s\n" output
 
-let empty_env = {
-  add_to_env  = [];
-  add_to_path = OpamFilename.raw_dir "";
-  new_env     = []
-}
-
 let print_env env =
-  if env <> empty_env then
-    List.iter (fun (k,v) ->
-      OpamGlobals.msg "%s=%s; export %s;\n" k v k;
-    ) env.new_env
+  List.iter (fun (k,v) ->
+    OpamGlobals.msg "%s=%s; export %s;\n" k v k;
+  ) env
 
 let print_csh_env env =
-  if env <> empty_env then
-    List.iter (fun (k,v) ->
-      OpamGlobals.msg "setenv %s %S;\n" k v;
-    ) env.new_env
+  List.iter (fun (k,v) ->
+    OpamGlobals.msg "setenv %s %S;\n" k v;
+  ) env
 
 let config request =
   log "config %s" (string_of_config request);

vendor/opam/opam-core/src/client/opamSolution.ml
@@ -227,7 +227,7 @@ let string_of_commands commands =
 
 let compilation_env t opam =
   let env0 = OpamState.get_env t in
-  OpamState.update_env t env0 (OpamFile.OPAM.build_env opam)
+  OpamState.add_to_env t env0 (OpamFile.OPAM.build_env opam)
 
 let proceed_to_delete ~rm_build t nv =
   log "deleting %s" (OpamPackage.to_string nv);
@@ -264,11 +264,7 @@ let proceed_to_delete ~rm_build t nv =
         else t.root in
       try
         OpamGlobals.msg "%s\n" (string_of_commands remove);
-        OpamFilename.exec
-          ~add_to_env:env.add_to_env
-          ~add_to_path:[OpamPath.Switch.bin t.root t.switch]
-          exec_dir
-          remove
+        OpamFilename.exec ~env exec_dir remove
       with _ ->
         ();
   );
@@ -388,17 +384,13 @@ let proceed_to_change t nv_old nv =
 
   (* Generate an environnement file *)
   let env_f = OpamPath.Switch.build_env t.root t.switch nv in
-  OpamFile.Env.write env_f env.new_env;
+  OpamFile.Env.write env_f env;
 
   (* Exec the given commands. *)
   let exec name f =
     let commands = OpamState.filter_commands t (f opam) in
     OpamGlobals.msg "%s:\n%s\n" name (string_of_commands commands);
-    OpamFilename.exec
-      ~add_to_env:env.add_to_env
-      ~add_to_path:[env.add_to_path]
-      p_build
-      commands in
+    OpamFilename.exec ~env p_build commands in
     try
       (* First, we build the package. *)
       exec ("Building " ^ OpamPackage.to_string nv) OpamFile.OPAM.build;

vendor/opam/opam-core/src/client/opamState.ml
@@ -604,7 +604,7 @@ let filter_command t (l, f) =
 let filter_commands t l =
   OpamMisc.filter_map (filter_command t) l
 
-let expand_env t env =
+let expand_env t (env: env_updates) : env =
   List.map (fun (ident, symbol, string) ->
     let string = substitute_string t string in
     let read_env () =
@@ -620,43 +620,37 @@ let expand_env t env =
     | _    -> failwith (Printf.sprintf "expand_env: %s is an unknown symbol" symbol)
   ) env
 
-let update_env t env e =
-  let expanded = expand_env t e in
-  { env with
-    add_to_env = expanded @ env.add_to_env;
-    new_env    = expanded @ env.new_env }
+let add_to_env t (env: env) (updates: env_updates) =
+  let env = List.filter (fun (k,_) -> List.for_all (fun (u,_,_) -> u <> k) updates) env in
+  env @ expand_env t updates
 
 let get_env t =
   let comp = compiler t t.compiler in
 
   let add_to_path = OpamPath.Switch.bin t.root t.switch in
   let new_path = "PATH", "+=", OpamFilename.Dir.to_string add_to_path in
-
-  let add_to_env = OpamFile.Comp.env comp in
   let toplevel_dir =
     "OCAML_TOPLEVEL_PATH", "=", OpamFilename.Dir.to_string (OpamPath.Switch.toplevel t.root t.switch) in
   let man_path =
     "MANPATH", ":=", OpamFilename.Dir.to_string (OpamPath.Switch.man_dir t.root t.switch) in
-  let new_env = new_path :: man_path :: toplevel_dir :: add_to_env in
-
-  let add_to_env = expand_env t add_to_env in
-  let new_env = expand_env t new_env in
-
-  (* if --root <dir> is passed on the command line, or if OPAMROOT is set. *)
-  let new_env =
+  let comp_env = OpamFile.Comp.env comp in
+  let root =
     if !OpamGlobals.root_dir <> OpamGlobals.default_opam_dir then
-      ("OPAMROOT", !OpamGlobals.root_dir) :: new_env
+      [ "OPAMROOT", "=", !OpamGlobals.root_dir ]
     else
-      new_env in
+      [] in
 
-  { add_to_env; add_to_path; new_env }
+  let updates = new_path :: man_path :: toplevel_dir :: (root @ comp_env) in
+  let env0 = OpamMisc.env () in
+
+  add_to_env t env0 updates
 
 let print_env_warning ?(add_profile = false) t =
   match
     List.filter
       (fun (s, v) ->
         Some v <> try Some (OpamMisc.getenv s) with _ -> None)
-      (get_env t).new_env
+      (get_env t)
   with
     | [] -> () (* every variables are correctly set *)
     | l ->

vendor/opam/opam-core/src/client/opamState.mli
@@ -91,7 +91,7 @@ val unknown_compiler: compiler -> 'a
 val get_env: state -> env
 
 (** Update an environment. *)
-val update_env: state -> env -> (string * string * string) list -> env
+val add_to_env: state -> env -> (string * string * string) list -> env
 
 (** Print a warning if the environment is not set-up properly. *)
 val print_env_warning: ?add_profile:bool -> state -> unit

vendor/opam/opam-core/src/core/opamFile.ml
@@ -1104,7 +1104,7 @@ module Env = struct
 
   let internal = "env"
 
-  type t = (string * string) list
+  type t = env
 
   let empty = []
 

vendor/opam/opam-core/src/core/opamFile.mli
@@ -199,7 +199,7 @@ module Reinstall: IO_FILE with type t = package_set
 module Updated: IO_FILE with type t = package_set
 
 (** Environement variables *)
-module Env: IO_FILE with type t = (string * string) list
+module Env: IO_FILE with type t = env
 
 (** Compiler version [$opam/compilers/] *)
 module Comp: sig

vendor/opam/opam-core/src/core/opamFilename.ml
@@ -60,13 +60,12 @@ let in_dir dirname fn =
   else
     OpamGlobals.error_and_exit "%s does not exist!" dirname
 
-let exec dirname ?(add_to_env=[]) ?(add_to_path=[]) cmds =
+let exec dirname ?env cmds =
+  let env = match env with
+    | None   -> None
+    | Some l -> Some (Array.of_list (List.map (fun (k,v) -> k^"="^v) l)) in
   in_dir dirname
-    (fun () ->
-      OpamSystem.commands
-        ~add_to_env
-        ~add_to_path:(List.map Dir.of_string add_to_path)
-        cmds)
+    (fun () -> OpamSystem.commands ?env cmds)
 
 let move_dir src dst =
   OpamSystem.command [ "mv"; Dir.to_string src; Dir.to_string dst ]

vendor/opam/opam-core/src/core/opamFilename.mli
@@ -37,9 +37,7 @@ val list_dirs: Dir.t -> Dir.t list
 val in_dir: Dir.t -> (unit -> 'a) -> 'a
 
 (** Execute a list of commands in a given directory *)
-val exec: Dir.t
-  -> ?add_to_env:(string * string) list
-  -> ?add_to_path:Dir.t list -> string list list -> unit
+val exec: Dir.t -> ?env:(string * string) list -> string list list -> unit
 
 (** Move a directory *)
 val move_dir: Dir.t -> Dir.t -> unit

commit b09fc63997ed62e692a8294207b408c40dc8243e

Fix 'opam config env'
This is a regression introduced in 7cc3308db136b8c5353c5f08fef18afddeb80898 which causes to display the full environment when doing 'opam config env' instead of just the bits modified by OPAM.

This should fix #365

vendor/opam/opam-core/src/client/opamConfigCommand.ml
@@ -255,7 +255,7 @@ let config request =
   let t = OpamState.load_state () in
   match request with
   | CEnv csh                  ->
-    let env = OpamState.get_env t in
+    let env = OpamState.get_opam_env t in
     if csh
     then print_csh_env env
     else print_env env

vendor/opam/opam-core/src/client/opamSolution.ml
@@ -226,7 +226,7 @@ let string_of_commands commands =
     "Nothing to do."
 
 let compilation_env t opam =
-  let env0 = OpamState.get_env t in
+  let env0 = OpamState.get_full_env t in
   OpamState.add_to_env t env0 (OpamFile.OPAM.build_env opam)
 
 let proceed_to_delete ~rm_build t nv =

vendor/opam/opam-core/src/client/opamState.ml
@@ -624,7 +624,7 @@ let add_to_env t (env: env) (updates: env_updates) =
   let env = List.filter (fun (k,_) -> List.for_all (fun (u,_,_) -> u <> k) updates) env in
   env @ expand_env t updates
 
-let get_env t =
+let env_updates t =
   let comp = compiler t t.compiler in
 
   let add_to_path = OpamPath.Switch.bin t.root t.switch in
@@ -640,17 +640,21 @@ let get_env t =
     else
       [] in
 
-  let updates = new_path :: man_path :: toplevel_dir :: (root @ comp_env) in
-  let env0 = OpamMisc.env () in
+  new_path :: man_path :: toplevel_dir :: (root @ comp_env)
 
-  add_to_env t env0 updates
+let get_opam_env t =
+  add_to_env t [] (env_updates t)
+
+let get_full_env t =
+  let env0 = OpamMisc.env () in
+  add_to_env t env0 (env_updates t)
 
 let print_env_warning ?(add_profile = false) t =
   match
     List.filter
       (fun (s, v) ->
         Some v <> try Some (OpamMisc.getenv s) with _ -> None)
-      (get_env t)
+      (get_opam_env t)
   with
     | [] -> () (* every variables are correctly set *)
     | l ->

vendor/opam/opam-core/src/client/opamState.mli
@@ -87,8 +87,11 @@ val unknown_compiler: compiler -> 'a
 
 (** {2 Environment} *)
 
-(** Get the current environment *)
-val get_env: state -> env
+(** Get the current environment. *)
+val get_full_env: state -> env
+
+(** Get only environment modified by OPAM. *)
+val get_opam_env: state -> env
 
 (** Update an environment. *)
 val add_to_env: state -> env -> (string * string * string) list -> env