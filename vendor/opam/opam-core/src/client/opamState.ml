avsm/unpac

commit 1fb04d39b5164e9560ebb8476ef427749059ffe3

Speed-up the 'opam config env' command.
This should fix #412

vendor/opam/opam-core/src/client/opamState.ml
@@ -309,9 +309,9 @@ let upgrade_system_compiler =
   ref (fun _ -> assert false)
 
 (* Only used during init: load only repository-related information *)
-let load_repository_state () =
+let load_repository_state call_site =
+  log "LOAD-REPO-STATE(%s)" call_site;
   let root = OpamPath.default () in
-  log "load_partial_state root=%s" (OpamFilename.Dir.to_string root);
   let config_p = OpamPath.config root in
   let config = OpamFile.Config.read config_p in
   let repositories =
@@ -325,6 +325,35 @@ let load_repository_state () =
     | Some a -> OpamSwitch.of_string a in
 
   (* evertything else is empty *)
+  let aliases = OpamSwitch.Map.empty in
+  let compiler = OpamCompiler.of_string "none" in
+  let compiler_version = OpamCompiler.Version.of_string "none" in
+  let opams = OpamPackage.Map.empty in
+  let packages = OpamPackage.Set.empty in
+  let available_packages = lazy OpamPackage.Set.empty in
+  let installed = OpamPackage.Set.empty in
+  let installed_roots = OpamPackage.Set.empty in
+  let reinstall = OpamPackage.Set.empty in
+  let repo_index = OpamPackage.Name.Map.empty in
+  let pinned = OpamPackage.Name.Map.empty in
+  {
+    root; switch; compiler; compiler_version; repositories; opams;
+    packages; available_packages; installed; installed_roots; reinstall;
+    repo_index; config; aliases; pinned;
+  }
+
+(* load partial state to be able to read env variables *)
+let load_env_state call_site =
+  log "LOAD-ENV-STATE(%s)" call_site;
+  let root = OpamPath.default () in
+  let config_p = OpamPath.config root in
+  let config = OpamFile.Config.read config_p in
+  let switch = match !OpamGlobals.switch with
+    | None   -> OpamFile.Config.switch config
+    | Some a -> OpamSwitch.of_string a in
+
+  (* evertything else is empty *)
+  let repositories = OpamRepositoryName.Map.empty in
   let aliases = OpamSwitch.Map.empty in
   let compiler = OpamCompiler.of_string "none" in
   let compiler_version = OpamCompiler.Version.of_string "none" in
@@ -441,8 +470,8 @@ let loads = ref []
 let print_stats () =
   List.iter (Printf.printf "load-state: %.2fs\n") !loads
 
-let load_state orig =
-  log "LOAD-STATE %s\n" orig;
+let load_state call_site =
+  log "LOAD-STATE(%s)\n" call_site;
   let t0 = Unix.gettimeofday () in
   let root = OpamPath.default () in
   log "load_state root=%s" (OpamFilename.Dir.to_string root);

vendor/opam/opam-core/src/client/opamClient.ml
@@ -699,7 +699,7 @@ let init repo compiler cores =
 
     (* Load the partial state, and update the repository state *)
     log "updating repository state";
-    let t = OpamState.load_repository_state () in
+    let t = OpamState.load_repository_state "init" in
     update_repositories t ~show_compilers:false t.repositories;
 
     (* Load the partial state, and update the packages state *)

vendor/opam/opam-core/src/client/opamConfigCommand.ml
@@ -252,17 +252,26 @@ let print_csh_env env =
 
 let config request =
   log "config %s" (string_of_config request);
-  let t = OpamState.load_state "config" in
+  let with_full f =
+    let t = OpamState.load_state "config" in
+    f t in
+  let with_partial f =
+    let t = OpamState.load_env_state "config" in
+    f t in
   match request with
-  | CEnv csh                  ->
-    let env = OpamState.get_opam_env t in
-    if csh
-    then print_csh_env env
-    else print_env env
-  | CList ns                  -> config_list t ns
-  | CSubst fs                 -> List.iter (OpamState.substitute_file t) fs
-  | CIncludes (is_rec, names) -> config_includes t is_rec names
-  | CCompil c                 -> config_compil t c
+  | CEnv csh ->
+    with_partial (fun t ->
+        let env = OpamState.get_opam_env t in
+        if csh
+        then print_csh_env env
+        else print_env env
+      )
+  | CList ns                  -> with_full (fun t -> config_list t ns)
+  | CSubst fs                 -> with_full (fun t -> List.iter (OpamState.substitute_file t) fs)
+  | CIncludes (is_rec, names) -> with_full (fun t -> config_includes t is_rec names)
+  | CCompil c                 -> with_full (fun t -> config_compil t c)
   | CVariable v               ->
-    let contents = OpamState.contents_of_variable t v in
-    OpamGlobals.msg "%s\n" (OpamVariable.string_of_variable_contents contents)
+    with_full (fun t ->
+        let contents = OpamState.contents_of_variable t v in
+        OpamGlobals.msg "%s\n" (OpamVariable.string_of_variable_contents contents)
+      )

vendor/opam/opam-core/src/client/opamState.mli
@@ -74,8 +74,11 @@ val load_state: string -> state
 (** Display stats *)
 val print_stats: unit -> unit
 
-(** Load repository related states only. All the other fields are let empty. *)
-val load_repository_state: unit -> state
+(** Load repository related states only. All the other fields are left empty. *)
+val load_repository_state: string -> state
+
+(** Load state associated to env variables. All other fields are left empty. *)
+val load_env_state: string -> state
 
 (** Create a universe from the current state *)
 val universe: state -> user_action -> universe

commit 73f24df7ccac30c27aacc97b46134a91d8c1641f

Fix broken `opam config env` (introduced by 1fb04d39b5164e9560ebb8476ef427749059ffe3)
CAML_LIBRARY_PATH and custom compiler variables were not displayed properly.

This should fix #417

vendor/opam/opam-core/src/client/opamState.ml
@@ -308,11 +308,16 @@ let load_env_state call_site =
   let switch = match !OpamGlobals.switch with
     | None   -> OpamFile.Config.switch config
     | Some a -> OpamSwitch.of_string a in
+  let aliases = OpamFile.Aliases.safe_read (OpamPath.aliases root) in
+  let compiler =
+    try OpamSwitch.Map.find switch aliases
+    with Not_found ->
+      OpamGlobals.error_and_exit
+        "The current switch (%s) is an unknown compiler switch."
+        (OpamSwitch.to_string switch) in
 
   (* evertything else is empty *)
   let repositories = OpamRepositoryName.Map.empty in
-  let aliases = OpamSwitch.Map.empty in
-  let compiler = OpamCompiler.of_string "none" in
   let compiler_version = OpamCompiler.Version.of_string "none" in
   let opams = OpamPackage.Map.empty in
   let packages = OpamPackage.Set.empty in