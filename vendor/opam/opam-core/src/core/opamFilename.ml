avsm/unpac

commit 879b8de4fdb27f187f19bca86143af5edc3b1ee9

Expand ~ in filename strings
This fixes `opam config setup --dot-profile=~/.bashrc`

Remark: this was already working fine if you do `opam config setup --dot-profile ~/.bashrc` (as this was shell-expanded)

vendor/opam/opam-core/src/core/opamFilename.ml
@@ -22,10 +22,18 @@ module Dir = struct
   include OpamMisc.Base
 
   let of_string dirname =
-    if not (Filename.is_relative dirname) then
-      dirname
-    else
+    if (String.length dirname > 1 && dirname.[0] = '~') then
+      let home = OpamMisc.getenv "HOME" in
+      match dirname with
+      | "~" -> home
+      | _   ->
+        let prefix = Filename.concat "~" "" in
+        let suffix = OpamMisc.remove_prefix ~prefix dirname in
+        Filename.concat home suffix
+    else if not (Filename.is_relative dirname) then
       OpamSystem.real_path dirname
+    else
+      dirname
 
   let to_string dirname =
     if dirname.[String.length dirname - 1] = Filename.dir_sep.[0] then

commit e071769078bfe736dd804ae2db1a8336f2cc2604

Fix silly regression introduced in 879b8de4fdb27f187f19bca86143af5edc3b1ee9

vendor/opam/opam-core/src/core/opamFilename.ml
@@ -30,7 +30,7 @@ module Dir = struct
         let prefix = Filename.concat "~" "" in
         let suffix = OpamMisc.remove_prefix ~prefix dirname in
         Filename.concat home suffix
-    else if not (Filename.is_relative dirname) then
+    else if Filename.is_relative dirname then
       OpamSystem.real_path dirname
     else
       dirname