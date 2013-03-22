avsm/unpac

commit ab312794847e32c1c4e67edf72d4d823fc0ec4d1

Added <remove> recipe step, and added optional `dest` attribute to <archive>
 - remove takes a single "path" attribute, and works on files and folders
 - `dest` unpacks the archive to a subfolder
 - `dest` locations for both <archive> and <rename> steps are created as needed

vendor/opam/0install-solver/zeroinstall/injector/fetch.py
@@ -110,7 +110,7 @@ class Fetcher(object):
 
 	# (force is deprecated and ignored)
 	@tasks.async
-	def cook(self, required_digest, recipe, stores, force = False, impl_hint = None):
+	def cook(self, required_digest, recipe, stores, force = False, impl_hint = None, dry_run = False):
 		"""Follow a Recipe.
 		@param impl_hint: the Implementation this is for (if any) as a hint for the GUI
 		@see: L{download_impl} uses this method when appropriate"""
@@ -145,7 +145,7 @@ class Fetcher(object):
 					for step in steps:
 						step.apply(tmpdir)
 					# Check that the result is correct and store it in the cache
-					store.check_manifest_and_rename(required_digest, tmpdir)
+					store.check_manifest_and_rename(required_digest, tmpdir, dry_run=dry_run)
 					tmpdir = None
 				finally:
 					# If unpacking fails, remove the temporary directory
@@ -401,27 +401,20 @@ class Fetcher(object):
 		def download_impl(method):
 			original_exception = None
 			while True:
-				try:
-					if isinstance(method, DownloadSource):
-						blocker, stream = self.download_archive(method, impl_hint = impl,
-									may_use_mirror = original_exception is None)
-						try:
-							yield blocker
-							tasks.check(blocker)
+				if isinstance(method, DownloadSource):
+					# turn a DownloadSource implementation into a single-step DownloadSource Recipe
+					step = method
+					method = Recipe()
+					method.steps.append(step)
+				elif not isinstance(method, Recipe):
+					raise Exception(_("Unknown download type for '%s'") % method)
 
-							stream.seek(0)
-							if self.external_store:
-								self._add_to_external_store(required_digest, [method], [stream])
-							else:
-								self._add_to_cache(required_digest, stores, method, stream)
-						finally:
-							stream.close()
-					elif isinstance(method, Recipe):
-						blocker = self.cook(required_digest, method, stores, impl_hint = impl)
-						yield blocker
-						tasks.check(blocker)
-					else:
-						raise Exception(_("Unknown download type for '%s'") % method)
+				try:
+					blocker = self.cook(required_digest, method, stores,
+							impl_hint = impl,
+							dry_run = self.handler.dry_run)
+					yield blocker
+					tasks.check(blocker)
 				except download.DownloadError as ex:
 					if original_exception:
 						logger.info("Error from mirror: %s", ex)
@@ -444,12 +437,6 @@ class Fetcher(object):
 
 			self.handler.impl_added_to_store(impl)
 		return download_impl(retrieval_method)
-
-	def _add_to_cache(self, required_digest, stores, retrieval_method, stream):
-		assert isinstance(retrieval_method, DownloadSource)
-		stores.add_archive_to_cache(required_digest, stream, retrieval_method.url, retrieval_method.extract,
-						 type = retrieval_method.type, start_offset = retrieval_method.start_offset or 0,
-						 dry_run = self.handler.dry_run)
 
 	def _add_to_external_store(self, required_digest, steps, streams):
 		from zeroinstall.zerostore.unpack import type_from_url
@@ -679,7 +666,17 @@ class RenameStepRunner(StepRunner):
 	def apply(self, basedir):
 		source = native_path_within_base(basedir, self.stepdata.source)
 		dest = native_path_within_base(basedir, self.stepdata.dest)
+		_ensure_dir_exists(os.path.dirname(dest))
 		os.rename(source, dest)
+
+class RemoveStepRunner(StepRunner):
+	"""A step runner for the <remove> step."""
+
+	model_type = model.RemoveStep
+
+	def apply(self, basedir):
+		path = native_path_within_base(basedir, self.stepdata.path)
+		support.ro_rmtree(path)
 
 class DownloadStepRunner(StepRunner):
 	"""A step runner for the <archive> step.
@@ -695,6 +692,9 @@ class DownloadStepRunner(StepRunner):
 	def apply(self, basedir):
 		from zeroinstall.zerostore import unpack
 		assert self.blocker.happened
+		if self.stepdata.dest is not None:
+			basedir = native_path_within_base(basedir, self.stepdata.dest)
+			_ensure_dir_exists(basedir)
 		unpack.unpack_archive_over(self.stepdata.url, self.stream, basedir,
 				extract = self.stepdata.extract,
 				type=self.stepdata.type,
@@ -718,3 +718,7 @@ def native_path_within_base(base, crossplatform_path):
 	if not fullpath.startswith(base + os.path.sep):
 		raise SafeException("path %r is not within the base directory" % (crossplatform_path,))
 	return fullpath
+
+def _ensure_dir_exists(dest):
+	if not os.path.isdir(dest):
+		os.makedirs(dest)

vendor/opam/0install-solver/tests/testdownload.py
@@ -397,7 +397,7 @@ class TestDownload(BaseTest):
 		finally:
 			sys.stdout = old_out
 	
-	def testRename(self):
+	def testRecipeRename(self):
 		with output_suppressed():
 			run_server(('HelloWorld.tar.bz2',))
 			requirements = Requirements(os.path.abspath('RecipeRename.xml'))
@@ -409,6 +409,75 @@ class TestDownload(BaseTest):
 			assert os.path.exists(os.path.join(path, 'HelloUniverse', 'minor'))
 			assert not os.path.exists(os.path.join(path, 'HelloWorld'))
 			assert not os.path.exists(os.path.join(path, 'HelloUniverse', 'main'))
+
+	def testRecipeRenameToNewDest(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2',))
+			requirements = Requirements(os.path.abspath('RecipeRenameToNewDest.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert os.path.exists(os.path.join(path, 'HelloWorld', 'bin', 'main'))
+			assert not os.path.exists(os.path.join(path, 'HelloWorld', 'main'))
+
+	def testRecipeRemoveFile(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2',))
+			requirements = Requirements(os.path.abspath('RecipeRemove.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert os.path.exists(os.path.join(path, 'HelloWorld'))
+			assert not os.path.exists(os.path.join(path, 'HelloWorld', 'main'))
+
+	def testRecipeRemoveDir(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2',))
+			requirements = Requirements(os.path.abspath('RecipeRemoveDir.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert not os.path.exists(os.path.join(path, 'HelloWorld'))
+
+	def testRecipeExtractToNewSubdirectory(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2',))
+			requirements = Requirements(os.path.abspath('RecipeExtractToNewDest.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert os.path.exists(os.path.join(path, 'src', 'HelloWorld', 'main'))
+
+	def testRecipeExtractToExistingSubdirectory(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2','HelloWorld.tar.bz2'))
+			requirements = Requirements(os.path.abspath('RecipeExtractToExistingDest.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert os.path.exists(os.path.join(path, 'HelloWorld', 'main')) # first archive's main
+			assert os.path.exists(os.path.join(path, 'HelloWorld', 'HelloWorld', 'main')) # second archive, extracted to HelloWorld/
+
+	def testExtractToNewSubdirectory(self):
+		with output_suppressed():
+			run_server(('HelloWorld.tar.bz2',))
+			requirements = Requirements(os.path.abspath('HelloExtractToNewDest.xml'))
+			requirements.command = None
+			driver = Driver(requirements = requirements, config = self.config)
+			driver_download(driver)
+			digests = driver.solver.selections[requirements.interface_uri].digests
+			path = self.config.stores.lookup_any(digests)
+			assert os.path.exists(os.path.join(path, 'src', 'HelloWorld', 'main'))
 
 	def testSymlink(self):
 		old_out = sys.stdout

vendor/opam/0install-solver/tests/HelloExtractToNewDest.xml
@@ -1,0 +1,10 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar' dest="src"/>
+    <manifest-digest sha256new="UKC5NISGPFBIXBKVIXZ3B4J3X3QHJGJLIX5HSNCCQJU34643GOFA"/>
+  </implementation>
+</interface>

vendor/opam/0install-solver/tests/RecipeExtractToExistingDest.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <recipe>
+      <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar'/>
+      <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar' dest='HelloWorld'/>
+    </recipe>
+    <manifest-digest sha256new="K4DOVGAI7F44L2CG2TYWD7QV3DOJ4Z7BLRYPIW4GQJGVRU44DJVQ"/>
+  </implementation>
+</interface>

vendor/opam/0install-solver/tests/RecipeExtractToNewDest.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <recipe>
+      <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar' dest="src"/>
+    </recipe>
+    <manifest-digest sha256new="UKC5NISGPFBIXBKVIXZ3B4J3X3QHJGJLIX5HSNCCQJU34643GOFA"/>
+  </implementation>
+</interface>

vendor/opam/0install-solver/tests/RecipeRemove.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <recipe>
+     <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar'/>
+     <remove path="HelloWorld/main"/>
+    </recipe>
+    <manifest-digest sha256new="ZZWFLIGPTPVN6K5QHE6K65FHYKV3GPWMAGMLC4IUCKDCS32D25BQ"/>
+  </implementation>
+</interface>

vendor/opam/0install-solver/tests/RecipeRemoveDir.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <recipe>
+     <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar'/>
+     <remove path="HelloWorld"/>
+    </recipe>
+    <manifest-digest sha256new="4OYMIQUY7QOBJGX36TEJS35ZEQT24QPEMSNZGTFESWMRW6CSXBKQ"/>
+  </implementation>
+</interface>

vendor/opam/0install-solver/tests/RecipeRenameToNewDest.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" ?>
+<interface xmlns="http://zero-install.sourceforge.net/2004/injector/interface">
+  <name>Recipe</name>
+  <summary>Recipe</summary>
+  <description>Recipe</description>
+  <implementation id='v0.1' version='0.1'>
+    <recipe>
+      <archive href='http://localhost:8000/HelloWorld.tar.bz2' size='185' type='application/x-bzip-compressed-tar'/>
+      <rename source="HelloWorld/main" dest="HelloWorld/bin/main"/>
+    </recipe>
+    <manifest-digest sha256new="26KA623AQUCRO3464IRQOM7TO7IFLZJB3SXB6N5UIIJXAMUQN2JA"/>
+  </implementation>
+</interface>

commit e39219c24528ae76a12ba29f299288f5189ba818

Don't use mirror if fetching from the mirror fails
If we ask the mirror to send us a complete implementation and it can't,
there's no point asking the mirror if it has a mirror of its own archive.

Was broken by ab312794.

vendor/opam/0install-solver/zeroinstall/injector/fetch.py
@@ -110,7 +110,7 @@ class Fetcher(object):
 
 	# (force is deprecated and ignored)
 	@tasks.async
-	def cook(self, required_digest, recipe, stores, force = False, impl_hint = None, dry_run = False):
+	def cook(self, required_digest, recipe, stores, force = False, impl_hint = None, dry_run = False, may_use_mirror = True):
 		"""Follow a Recipe.
 		@param impl_hint: the Implementation this is for (if any) as a hint for the GUI
 		@see: L{download_impl} uses this method when appropriate"""
@@ -122,7 +122,7 @@ class Fetcher(object):
 		try:
 			for stepdata in recipe.steps:
 				cls = StepRunner.class_for(stepdata)
-				step = cls(stepdata, impl_hint=impl_hint)
+				step = cls(stepdata, impl_hint = impl_hint, may_use_mirror = may_use_mirror)
 				step.prepare(self, blockers)
 				steps.append(step)
 
@@ -130,7 +130,6 @@ class Fetcher(object):
 				yield blockers
 				tasks.check(blockers)
 				blockers = [b for b in blockers if not b.happened]
-
 
 			if self.external_store:
 				# Note: external_store will not yet work with non-<archive> steps.
@@ -412,7 +411,8 @@ class Fetcher(object):
 				try:
 					blocker = self.cook(required_digest, method, stores,
 							impl_hint = impl,
-							dry_run = self.handler.dry_run)
+							dry_run = self.handler.dry_run,
+							may_use_mirror = original_exception is None)
 					yield blocker
 					tasks.check(blocker)
 				except download.DownloadError as ex:
@@ -639,9 +639,10 @@ class StepRunner(object):
 	"""The base class of all step runners.
 	@since: 1.10"""
 
-	def __init__(self, stepdata, impl_hint):
+	def __init__(self, stepdata, impl_hint, may_use_mirror = True):
 		self.stepdata = stepdata
 		self.impl_hint = impl_hint
+		self.may_use_mirror = may_use_mirror
 
 	def prepare(self, fetcher, blockers):
 		pass
@@ -685,7 +686,7 @@ class DownloadStepRunner(StepRunner):
 	model_type = model.DownloadSource
 
 	def prepare(self, fetcher, blockers):
-		self.blocker, self.stream = fetcher.download_archive(self.stepdata, impl_hint = self.impl_hint, may_use_mirror = True)
+		self.blocker, self.stream = fetcher.download_archive(self.stepdata, impl_hint = self.impl_hint, may_use_mirror = self.may_use_mirror)
 		assert self.stream
 		blockers.append(self.blocker)
 	

vendor/opam/0install-solver/tests/testdownload.py
@@ -14,6 +14,7 @@ os.environ["http_proxy"] = "localhost:8000"
 from zeroinstall import helpers
 from zeroinstall.injector import model, gpg, download, trust, background, arch, selections, qdom, run
 from zeroinstall.injector.requirements import Requirements
+from zeroinstall.injector.scheduler import Site
 from zeroinstall.injector.driver import Driver
 from zeroinstall.zerostore import NotStored
 from zeroinstall.support import basedir, tasks, ro_rmtree
@@ -139,6 +140,14 @@ def run_server(*args):
 
 real_get_selections_gui = helpers.get_selections_gui
 
+# Count how many downloads we request so we can check it
+traced_downloads = None
+orig_download = Site.download
+def wrap_download(self, step):
+	traced_downloads.append(step.url)
+	return orig_download(self, step)
+Site.download = wrap_download
+
 class TestDownload(BaseTest):
 	def setUp(self):
 		BaseTest.setUp(self)
@@ -162,6 +171,9 @@ class TestDownload(BaseTest):
 
 		global ran_gui
 		ran_gui = False
+
+		global traced_downloads
+		traced_downloads = []
 
 	def tearDown(self):
 		# Wait for all downloads to finish, otherwise they may interfere with other tests
@@ -582,6 +594,40 @@ class TestDownload(BaseTest):
 			tasks.wait_for_blocker(downloaded)
 			path = self.config.stores.lookup_any(driver.solver.selections.selections['http://example.com:8000/Hello.xml'].digests)
 			assert os.path.exists(os.path.join(path, 'HelloWorld', 'main'))
+
+	def testImplMirrorFails(self):
+		with resourcewarnings_suppressed():
+			trust.trust_db.trust_key('DE937DD411906ACF7C263B396FCF121BE2390E0B', 'example.com:8000')
+			run_server('/Hello.xml',
+					'/6FCF121BE2390E0B.gpg',
+					server.Give404('/HelloWorld.tgz'),
+					server.Give404('/0mirror/archive/http%3A%23%23example.com%3A8000%23HelloWorld.tgz'),
+					server.Give404('/0mirror/feeds/http/example.com:8000/Hello.xml/impl/sha1=3ce644dc725f1d21cfcf02562c76f375944b266a'))
+			driver = Driver(requirements = Requirements('http://example.com:8000/Hello.xml'), config = self.config)
+			self.config.mirror = 'http://example.com:8000/0mirror'
+
+			refreshed = driver.solve_with_downloads()
+			tasks.wait_for_blocker(refreshed)
+			assert driver.solver.ready
+
+			getLogger().setLevel(logging.ERROR)
+			try:
+				downloaded = driver.download_uncached_implementations()
+				tasks.wait_for_blocker(downloaded)
+				assert 0
+			except download.DownloadError as ex:
+				assert 'Missing: HelloWorld.tgz' in str(ex), ex
+
+			self.assertEqual([
+				'http://example.com:8000/Hello.xml',
+				'http://example.com:8000/6FCF121BE2390E0B.gpg',
+				# The original archive:
+				'http://example.com:8000/HelloWorld.tgz',
+				# Mirror of original archive:
+				'http://example.com:8000/0mirror/archive/http%3A%23%23example.com%3A8000%23HelloWorld.tgz',
+				# Mirror of implementation:
+				'http://example.com:8000/0mirror/feeds/http/example.com:8000/Hello.xml/impl/sha1=3ce644dc725f1d21cfcf02562c76f375944b266a'
+				], traced_downloads)
 
 	def testLocalFeedMirror(self):
 		with resourcewarnings_suppressed():