avsm/unpac

commit 027e2a9e8d64c2eceaa8c48485355f7b29d8d053

Clean-ups in the switch code.
This can help #542

We now export and read OPAMSWITCH to be able to run OPAM itself inside a different switch.

vendor/opam/opam-core/src/client/opamState.ml
@@ -881,13 +881,17 @@ let env_updates t =
   let man_path =
     "MANPATH", ":=", OpamFilename.Dir.to_string (OpamPath.Switch.man_dir t.root t.switch) in
   let comp_env = OpamFile.Comp.env comp in
+  let switch =
+    match !OpamGlobals.switch with
+    | None   -> []
+    | Some s -> [ "OPAMSWITCH", "=", s ] in
   let root =
     if !OpamGlobals.root_dir <> OpamGlobals.default_opam_dir then
       [ "OPAMROOT", "=", !OpamGlobals.root_dir ]
     else
       [] in
 
-  new_path :: man_path :: toplevel_dir :: (root @ comp_env)
+  new_path :: man_path :: toplevel_dir :: (switch @ root @ comp_env)
 
 let get_opam_env t =
   add_to_env t [] (env_updates t)
@@ -1063,9 +1067,6 @@ let status_of_init_file t init_sh =
       None
   ) else
     None
-
-let update_env_variables t =
-  update_init_scripts t ~global:None
 
 let dot_profile_needs_update t dot_profile =
   if OpamFilename.exists dot_profile then (
@@ -1397,9 +1398,7 @@ let install_compiler t ~quiet switch compiler =
       end;
     end;
 
-    (* write the new version in the configuration file *)
-    let config = OpamFile.Config.with_switch t.config switch in
-    OpamFile.Config.write (OpamPath.config t.root) config;
+    (* Update ~/.opam/aliases *)
     add_switch t.root switch compiler
 
   with e ->
@@ -1407,6 +1406,12 @@ let install_compiler t ~quiet switch compiler =
       OpamFilename.rmdir switch_dir;
     raise e
   end
+
+(* write the new version in the configuration file *)
+let update_switch_config t switch =
+  let config = OpamFile.Config.with_switch t.config switch in
+  OpamFile.Config.write (OpamPath.config t.root) config;
+  update_init_scripts t ~global:None
 
 let update_pinned_package t n =
   if OpamPackage.Name.Map.mem n t.pinned then

vendor/opam/opam-core/src/client/opamClient.ml
@@ -705,6 +705,7 @@ module API = struct
       let switch = OpamSwitch.of_string (OpamCompiler.to_string compiler) in
       let quiet = (compiler = OpamCompiler.system) in
       OpamState.install_compiler t ~quiet switch compiler;
+      OpamState.update_switch_config t switch;
       update_packages t ~show_packages:false t.repositories OpamPackage.Name.Set.empty;
 
       (* Finally, load the complete state and install the compiler packages *)
@@ -1075,8 +1076,8 @@ module SafeAPI = struct
     let switch ~quiet ~warning name =
       global_lock (fun () -> API.SWITCH.switch ~quiet ~warning name)
 
-    let install ~quiet ~warning switch ocaml_version =
-      global_lock (fun () -> API.SWITCH.install ~quiet ~warning switch ocaml_version)
+    let install ~quiet ~warning ~update_config switch ocaml_version =
+      global_lock (fun () -> API.SWITCH.install ~quiet ~warning ~update_config switch ocaml_version)
 
     let import filename =
       switch_lock (fun () -> API.SWITCH.import filename)

vendor/opam/opam-core/src/client/opamClient.mli
@@ -116,7 +116,7 @@ module API: sig
     val switch: quiet:bool -> warning:bool -> switch -> unit
 
     (** Install the given compiler. *)
-    val install: quiet:bool -> warning:bool -> switch -> compiler -> unit
+    val install: quiet:bool -> warning:bool -> update_config:bool -> switch -> compiler -> unit
 
     (** Import the packages from a file. If no filename is specified,
         read stdin. *)

vendor/opam/opam-core/src/client/opamMain.ml
@@ -911,9 +911,9 @@ let switch =
   ] @ mk_subdoc commands in
 
   let command, params = mk_subcommands_with_default commands
-    "If a compiler switch is given instead of an usual command, this command will switch to \
-     the given compiler. You will then need to run $(b,eval `opam config env`) to update your \
-     environment variables." in
+      "If a compiler switch is given instead of an usual command, this command will switch to \
+       the given compiler. You will then need to run $(b,eval `opam config env`) to update your \
+       environment variables." in
   let alias_of =
     mk_opt ["a";"alias-of"]
       "COMP" "The name of the compiler description which will be aliased."
@@ -924,7 +924,12 @@ let switch =
       Arg.(some filename) None in
   let no_warning =
     mk_flag ["no-warning"] "Do not display any warning related to environment variables." in
-  let switch global_options build_options command alias_of filename print_short installed_only no_warning params =
+  let no_switch =
+    mk_flag ["no-switch"]
+      "Only install the compiler switch, without switching to it. If the compiler switch \
+       is already installed, then do nothing." in
+
+  let switch global_options build_options command alias_of filename print_short installed_only no_warning no_switch params =
     set_global_options global_options;
     set_build_options build_options;
     let no_alias_of () =
@@ -937,37 +942,46 @@ let switch =
     match command, params with
     | None      , []
     | Some `list, [] ->
-        no_alias_of ();
-        Client.SWITCH.list ~print_short ~installed_only
+      no_alias_of ();
+      Client.SWITCH.list ~print_short ~installed_only
     | Some `install, [switch] ->
-        Client.SWITCH.install ~quiet:global_options.quiet ~warning (OpamSwitch.of_string switch) (mk_comp switch)
+      Client.SWITCH.install
+        ~quiet:global_options.quiet
+        ~warning
+        ~update_config:(not no_switch)
+        (OpamSwitch.of_string switch)
+        (mk_comp switch)
     | Some `export, [] ->
-        no_alias_of ();
-        Client.SWITCH.export filename
+      no_alias_of ();
+      Client.SWITCH.export filename
     | Some `import, [] ->
-        no_alias_of ();
-        Client.SWITCH.import filename
+      no_alias_of ();
+      Client.SWITCH.import filename
     | Some `remove, switches ->
-        no_alias_of ();
-        List.iter (fun switch -> Client.SWITCH.remove (OpamSwitch.of_string switch)) switches
+      no_alias_of ();
+      List.iter (fun switch -> Client.SWITCH.remove (OpamSwitch.of_string switch)) switches
     | Some `reinstall, [switch] ->
-        no_alias_of ();
-        Client.SWITCH.reinstall (OpamSwitch.of_string switch)
+      no_alias_of ();
+      Client.SWITCH.reinstall (OpamSwitch.of_string switch)
     | Some `current, [] ->
-        no_alias_of ();
-        Client.SWITCH.show ()
+      no_alias_of ();
+      Client.SWITCH.show ()
     | Some `default switch, [] ->
-        (match alias_of with
-        | None -> Client.SWITCH.switch ~quiet:global_options.quiet ~warning (OpamSwitch.of_string switch)
-        | _    ->
-          Client.SWITCH.install ~quiet:global_options.quiet ~warning
-            (OpamSwitch.of_string switch) (mk_comp switch))
+      (match alias_of with
+       | None -> Client.SWITCH.switch ~quiet:global_options.quiet ~warning (OpamSwitch.of_string switch)
+       | _    ->
+         Client.SWITCH.install
+           ~quiet:global_options.quiet
+           ~warning
+           ~update_config:(not no_switch)
+           (OpamSwitch.of_string switch)
+           (mk_comp switch))
     | _, l -> OpamGlobals.error_and_exit "too many arguments (%d)" (List.length l) in
 
   Term.(pure switch
     $global_options $build_options $command
     $alias_of $filename $print_short_flag
-    $installed_only_flag $no_warning $params),
+    $installed_only_flag $no_warning $no_switch $params),
   term_info "switch" ~doc ~man
 
 (* PIN *)

vendor/opam/opam-core/src/client/opamState.mli
@@ -122,9 +122,6 @@ val display_setup: state -> shell -> filename -> unit
 (** Update the user configuration. *)
 val update_setup: state -> user_config option -> global_config option -> unit
 
-(** Update the global environment variables. *)
-val update_env_variables: state -> unit
-
 (** {2 Substitutions} *)
 
 (** Compute the value of a variable *)
@@ -182,6 +179,9 @@ val compilers: root:dirname -> compiler_set
 
 (** Install the given compiler *)
 val install_compiler: state -> quiet:bool -> switch -> compiler -> unit
+
+(** Write the right compiler switch in ~/.opam/config *)
+val update_switch_config: state -> switch -> unit
 
 (** Get the packages associated with the given compiler *)
 val get_compiler_packages: state -> compiler -> atom list

vendor/opam/opam-core/src/client/opamSwitchCommand.ml
@@ -130,28 +130,27 @@ let remove switch =
   OpamFile.Aliases.write (OpamPath.aliases t.root) aliases;
   OpamFilename.rmdir comp_dir
 
-let update_config t ~warning switch =
-  let config = OpamFile.Config.with_switch t.config switch in
-  OpamFile.Config.write (OpamPath.config t.root) config;
-  if warning then (
-    let t = OpamState.load_state "switch-update-config" in
+let update_global_config t ~warning switch =
+  OpamState.update_switch_config t switch;
+  let t = OpamState.load_state "switch-update-config" in
+  if warning then
     OpamState.print_env_warning t None
-  )
 
-let install_with_packages ~quiet ~warning ~packages switch compiler =
+let install_with_packages ~quiet ~packages switch compiler =
   log "install %b %s %s" quiet
     (OpamSwitch.to_string switch)
     (OpamCompiler.to_string compiler);
 
   (* Remember the current switch to be able to roll-back *)
   let t = OpamState.load_state "switch-install-with-packages-1" in
-  let old_switch = t.switch in
 
   (* install the new OCaml version *)
   OpamState.install_compiler t ~quiet switch compiler;
 
   (* install the compiler packages *)
+  OpamGlobals.switch := Some (OpamSwitch.to_string switch);
   let t = OpamState.load_state "switch-install-with-packages-2" in
+
   let to_install, roots = match packages with
     | Some (p, r)  -> (OpamSolution.eq_atoms_of_packages p, OpamPackage.names_of_packages r)
     | None         ->
@@ -180,7 +179,6 @@ let install_with_packages ~quiet ~warning ~packages switch compiler =
         (OpamPackage.Version.to_string v) in
 
   let uninstall_compiler () =
-    update_config t ~warning old_switch;
     remove switch in
 
   match bad_packages with
@@ -191,7 +189,6 @@ let install_with_packages ~quiet ~warning ~packages switch compiler =
         wish_upgrade = to_install } in
     begin try
       OpamSolution.check_solution solution;
-      if warning then OpamState.print_env_warning t None
     with e ->
       uninstall_compiler ();
       raise e
@@ -200,25 +197,35 @@ let install_with_packages ~quiet ~warning ~packages switch compiler =
     uninstall_compiler ();
     package_error p
 
-let install ~quiet ~warning switch compiler =
-  install_with_packages ~quiet ~warning ~packages:None switch compiler
+let install ~quiet ~warning ~update_config switch compiler =
+  let t = OpamState.load_state "install" in
+  let comp_dir = OpamPath.Switch.root t.root switch in
+  let comp_f = OpamPath.compiler t.root compiler in
+  if not (OpamFilename.exists_dir comp_dir)
+  && not (OpamFilename.exists comp_f) then
+    OpamCompiler.unknown compiler;
+  if not (OpamSwitch.Map.mem switch t.aliases) then
+    install_with_packages ~quiet ~packages:None switch compiler
+  else (
+    let a = OpamSwitch.Map.find switch t.aliases in
+    if a <> compiler then
+      OpamGlobals.error_and_exit
+        "The compiler switch %s is already installed as an alias for %s."
+        (OpamSwitch.to_string switch)
+        (OpamCompiler.to_string a)
+  );
+  if update_config then
+    update_global_config ~warning t switch
 
 let switch ~quiet ~warning switch =
   log "switch switch=%s" (OpamSwitch.to_string switch);
   let t = OpamState.load_state "switch-1" in
-  let comp_dir = OpamPath.Switch.root t.root switch in
-  let compiler = OpamCompiler.of_string (OpamSwitch.to_string switch) in
-  let comp_f = OpamPath.compiler t.root compiler in
-  if not (OpamFilename.exists_dir comp_dir) && not (OpamFilename.exists comp_f) then
-    OpamCompiler.unknown compiler;
-  if not (OpamSwitch.Map.mem switch t.aliases) then
-    install ~quiet ~warning switch compiler
-  else
-    update_config t ~warning switch;
-  (* Check the consistency of the new switch *)
-  let new_state = OpamState.load_state "switch-2" in
-  OpamState.update_env_variables t;
-  OpamState.check_base_packages new_state
+  if not (OpamSwitch.Map.mem switch t.aliases) then (
+    let compiler = OpamCompiler.of_string (OpamSwitch.to_string switch) in
+    install ~quiet ~warning ~update_config:true switch compiler
+  ) else
+    update_global_config ~warning t switch;
+  OpamState.check_base_packages t
 
 (* Remove from [set] all the packages whose names appear in
    [filter]. *)
@@ -285,4 +292,4 @@ let reinstall switch =
   OpamFilename.rmdir (OpamPath.Switch.root t.root switch);
 
   (* Install the compiler *)
-  install_with_packages ~quiet:false ~packages ~warning:false switch ocaml_version
+  install_with_packages ~quiet:false ~packages switch ocaml_version

vendor/opam/opam-core/src/client/opamSwitchCommand.mli
@@ -18,7 +18,7 @@
 open OpamTypes
 
 (** Install a new switch. *)
-val install: quiet:bool -> warning:bool -> switch -> compiler -> unit
+val install: quiet:bool -> warning:bool -> update_config:bool -> switch -> compiler -> unit
 
 (** Import a file which contains the packages to install. *)
 val import: filename option -> unit

vendor/opam/opam-core/src/core/opamGlobals.ml
@@ -61,7 +61,10 @@ let default_package = "conf-ocaml"
 
 let system = "system"
 
-let switch : string option ref = ref None
+let switch = ref (
+    try Some (OpamMisc.getenv "OPAMSWITCH")
+    with _ -> None
+  )
 
 let opam_version = "1"
 

commit 41a395b48b36fbff74218458452b6d35b906821f

Ignore OPAMSWITCH when displaying environment varilables (eg. on 'opam config env' or 'opam switch foo').
This fixes a regression introduced by 027e2a9e8d64c2eceaa8c48485355f7b29d8d053 where you cannot switch anymore when you have OPAMSWITCH set.

vendor/opam/opam-core/src/client/opamState.ml
@@ -900,10 +900,7 @@ let env_updates t =
   let man_path =
     "MANPATH", ":=", OpamFilename.Dir.to_string (OpamPath.Switch.man_dir t.root t.switch) in
   let comp_env = OpamFile.Comp.env comp in
-  let switch =
-    match !OpamGlobals.switch with
-    | None   -> []
-    | Some s -> [ "OPAMSWITCH", "=", s ] in
+  let switch = [ "OPAMSWITCH", "=", OpamSwitch.to_string t.switch ] in
   let root =
     if !OpamGlobals.root_dir <> OpamGlobals.default_opam_dir then
       [ "OPAMROOT", "=", !OpamGlobals.root_dir ]
@@ -912,7 +909,13 @@ let env_updates t =
 
   new_path :: man_path :: toplevel_dir :: (switch @ root @ comp_env)
 
+(* This function is used by 'opam config env' and 'opam switch' to
+   display the environment variables. We have to make sure that
+   OPAMSWITCH is always the one being reported in '~/.opa/config'
+   otherwise we can have very weird results (as the inability to switch
+   between compilers). *)
 let get_opam_env t =
+  let t = { t with switch = OpamFile.Config.switch t.config } in
   add_to_env t [] (env_updates t)
 
 let get_full_env t =