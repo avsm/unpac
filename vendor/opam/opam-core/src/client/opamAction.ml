avsm/unpac

commit 5c73ac447cda60d691e8d80a3840f53df7f472e3

If an `opam` file is located at the root of a dev package, use its build instructions instead of the one stored in OPAM global state.
Fix #671

vendor/opam/opam-core/src/client/opamAction.ml
@@ -297,6 +297,21 @@ let update_metadata t ~installed ~installed_roots ~reinstall =
     (OpamPath.Switch.reinstall t.root t.switch)
     reinstall
 
+let dev_opam_opt t nv build_dir =
+  let opam () = OpamState.opam_opt t nv in
+  if OpamState.is_dev_package t nv then
+    let overlay = build_dir // "opam" in
+    if OpamFilename.exists overlay then
+      try Some (OpamFile.OPAM.read overlay)
+      with _ -> opam ()
+    else opam ()
+  else opam ()
+
+let dev_opam t nv build_dir =
+  match dev_opam_opt t nv build_dir with
+  | None    -> OpamPackage.unknown (OpamPackage.name nv) (Some (OpamPackage.version nv))
+  | Some nv -> nv
+
 (* Remove a given package *)
 (* This will be done by the parent process, so theoritically we are
    allowed to modify the global state of OPAM here. However, for
@@ -306,9 +321,7 @@ let remove_package_aux t ~metadata ~rm_build nv =
   let name = OpamPackage.name nv in
 
   (* Run the remove script *)
-  let opam =
-    try Some (OpamState.opam t nv)
-    with _ -> None in
+  let opam = OpamState.opam_opt t nv in
 
   OpamGlobals.msg "Removing %s.\n" (OpamPackage.to_string nv);
 
@@ -339,6 +352,9 @@ let remove_package_aux t ~metadata ~rm_build nv =
           try let _ = extract_package t nv in ()
           with _ -> ()
         );
+        let opam = dev_opam t nv p_build in
+        let remove = OpamState.filter_commands t
+            OpamVariable.Map.empty (OpamFile.OPAM.build opam) in
         let name = OpamPackage.Name.to_string name in
         let exec_dir, name =
           if OpamFilename.exists_dir p_build
@@ -359,7 +375,6 @@ let remove_package_aux t ~metadata ~rm_build nv =
 
   (* Remove the documentation *)
   OpamFilename.rmdir (OpamPath.Switch.doc t.root t.switch name);
-  (* XXX: remove the man pages *)
 
   (* Remove build/<package> if requested *)
   if not !OpamGlobals.keep_build_dir && rm_build then
@@ -481,16 +496,18 @@ let build_and_install_package_aux t ~metadata nv =
 
   OpamGlobals.msg "\n%s Installing %s %s\n" left (OpamPackage.to_string nv) right;
 
-  let opam = OpamState.opam t nv in
-
-  (* Get the env variables set up in the compiler description file *)
-  let env = compilation_env t opam in
-
   try
 
     (* This one can raises an exception (for insance an user's CTRL-C
        when the sync takes too long. *)
     let p_build = extract_package t nv in
+
+    (* For dev packages, we look for any opam file at the root of the build
+     directory *)
+    let opam = dev_opam t nv p_build in
+
+    (* Get the env variables set up in the compiler description file *)
+    let env = compilation_env t opam in
 
     (* Exec the given commands. *)
     let exec name f =

vendor/opam/opam-core/src/client/opamState.ml
@@ -581,14 +581,18 @@ let real_package t nv =
   if is_locally_pinned t name then OpamPackage.pinned name
   else nv
 
-let opam t nv =
+let opam_opt t nv =
   let nv = real_package t nv in
   let overlay = OpamPath.Switch.opam t.root t.switch nv in
-  if OpamFilename.exists overlay then OpamFile.OPAM.read overlay
+  if OpamFilename.exists overlay then Some (OpamFile.OPAM.read overlay)
   else
-    try OpamPackage.Map.find nv t.opams
-    with Not_found ->
-      OpamPackage.unknown (OpamPackage.name nv) (Some (OpamPackage.version nv))
+    try Some (OpamPackage.Map.find nv t.opams)
+    with Not_found -> None
+
+let opam t nv =
+  match opam_opt t nv with
+  | None    -> OpamPackage.unknown (OpamPackage.name nv) (Some (OpamPackage.version nv))
+  | Some nv -> nv
 
 let overlay_of_name t name =
   let versions = OpamPackage.versions_of_name t.packages name in
@@ -673,6 +677,14 @@ let url t nv =
       Some (OpamFile.URL.read url)
     else
       None
+
+let is_dev_package t nv =
+  match url t nv with
+  | None     -> false
+  | Some url ->
+    match guess_repository_kind (OpamFile.URL.kind url) (OpamFile.URL.url url) with
+    | `http  -> false
+    | _      -> true
 
 let dev_package t nv =
   if has_url_overlay t nv then OpamPath.Switch.dev_package t.root t.switch nv
@@ -903,7 +915,7 @@ let clean_file file nv =
     OpamFilename.remove file
   )
 
-let dev_packages dir =
+let dev_packages_of_dir dir =
   let dirs = OpamFilename.dirs dir in
   List.fold_left (fun map dir ->
       match OpamPackage.of_dirname dir with
@@ -916,10 +928,10 @@ let dev_packages dir =
     ) OpamPackage.Map.empty dirs
 
 let global_dev_packages t =
-  dev_packages (OpamPath.dev_packages_dir t.root)
+  dev_packages_of_dir (OpamPath.dev_packages_dir t.root)
 
 let switch_dev_packages t =
-  dev_packages (OpamPath.Switch.dev_packages_dir t.root t.switch)
+  dev_packages_of_dir (OpamPath.Switch.dev_packages_dir t.root t.switch)
 
 let keys map =
   OpamPackage.Map.fold (fun nv _ set ->
@@ -929,7 +941,7 @@ let keys map =
 (* Check that the dev packages are installed -- if not, just remove
    the tempory files. *)
 let consistency_checks dir pinned installed all_installed =
-  let dev_packages = dev_packages dir in
+  let dev_packages = dev_packages_of_dir dir in
   OpamPackage.Map.iter (fun nv dir ->
       let name = OpamPackage.name nv in
       if OpamPackage.is_pinned nv then (
@@ -949,7 +961,8 @@ let consistency_checks dir pinned installed all_installed =
 let dev_packages t =
   let global = global_dev_packages t in
   let switch = switch_dev_packages t in
-  OpamPackage.Set.union (keys global) (keys switch)
+  let all = OpamPackage.Set.union (keys global) (keys switch) in
+  OpamPackage.Set.filter (is_dev_package t) all
 
 let global_consistency_checks t =
   consistency_checks (OpamPath.dev_packages_dir t.root)

vendor/opam/opam-core/src/client/opamState.mli
@@ -165,6 +165,9 @@ val filter_commands: state -> variable_map -> command list -> string list list
 (** Return the OPAM file for the given package *)
 val opam: state -> package -> OpamFile.OPAM.t
 
+(** Return the OPAM file for the given package *)
+val opam_opt: state -> package -> OpamFile.OPAM.t option
+
 (** Return the URL file for the given package *)
 val url: state -> package -> OpamFile.URL.t option
 
@@ -307,6 +310,9 @@ val dev_packages: state -> package_set
     packages whose contents have changed upstream. Side-effect: update
     the reinstall files. *)
 val update_dev_packages: state -> package_set
+
+(** Check whether a package is a development package *)
+val is_dev_package: state -> package -> bool
 
 (** {2 Configuration files} *)
 

commit 2e1f58e75369c9a99564764efa877204d111ef80

Fix regression introduced in 5c73ac447cda60d691e8d80a3840f53df7f472e3
(thx @AltGr for noticing)

vendor/opam/opam-core/src/client/opamAction.ml
@@ -354,7 +354,7 @@ let remove_package_aux t ~metadata ~rm_build nv =
         );
         let opam = dev_opam t nv p_build in
         let remove = OpamState.filter_commands t
-            OpamVariable.Map.empty (OpamFile.OPAM.build opam) in
+            OpamVariable.Map.empty (OpamFile.OPAM.remove opam) in
         let name = OpamPackage.Name.to_string name in
         let exec_dir, name =
           if OpamFilename.exists_dir p_build