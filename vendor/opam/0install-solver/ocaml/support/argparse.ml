avsm/unpac

commit a47610da27f0b011d1e99aab57aaf7af192453f3

List.iteri is only available in OCaml >= 4

vendor/opam/0install-solver/ocaml/support/argparse.ml
@@ -293,8 +293,13 @@ let format_options format_type opts =
   let set_tab_stop = ref false in
   set_tab();
 
-  ListLabels.iteri display_options ~f:(fun opti (syn, help) ->
-    if opti > 0 then print_cut ();
+  let first = ref true in
+
+  ListLabels.iter display_options ~f:(fun (syn, help) ->
+    if !first then (
+      print_cut ();
+      first := false
+    );
 
     print_string syn;
 

commit 7ee7b9c08471b4602ea4232ce1839e87e4a03b62

Fixed formatting of help output
Was broken by a47610da.

vendor/opam/0install-solver/ocaml/support/argparse.ml
@@ -297,8 +297,9 @@ let format_options format_type opts =
 
   ListLabels.iter display_options ~f:(fun (syn, help) ->
     if !first then (
-      print_cut ();
       first := false
+    ) else (
+      print_cut ()
     );
 
     print_string syn;