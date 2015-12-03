ocaml/ocaml

Tests for constants with modifiers
add some tests

more tests

testsuite/tests/parsing/int_and_float_with_modifier.ml
@@ -1,0 +1,12 @@
+let int_with_custom_modifier   = 1234567890_1234567890_1234567890_1234567890_1234567890z
+let float_with_custom_modifier = 1234567890_1234567890_1234567890_1234567890_1234567890.z
+
+let int32     = 1234l
+let int64     = 1234L
+let nativeint = 1234n
+
+let hex_without_modifier = 0x32f
+let hex_with_modifier    = 0x32g
+
+let float_without_modifer = 1.2e3
+let float_with_modifer    = 1.2e

avsm/unpac

vendor/opam/seq/testsuite/tests/parsing/int_and_float_with_modifier.ml
@@ -1,0 +1,12 @@
+let int_with_custom_modifier   = 1234567890_1234567890_1234567890_1234567890_1234567890z
+let float_with_custom_modifier = 1234567890_1234567890_1234567890_1234567890_1234567890.z
+
+let int32     = 1234l
+let int64     = 1234L
+let nativeint = 1234n
+
+let hex_without_modifier = 0x32f
+let hex_with_modifier    = 0x32g
+
+let float_without_modifer = 1.2e3
+let float_with_modifer    = 1.2g