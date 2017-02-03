avsm/unpac

commit 3a55205223799b3d89d813390fda66a5e95cbfe2

Better error reports on missing required positional arguments.
Closes #39.

vendor/opam/cmdliner/src/cmdliner.ml
@@ -416,6 +416,20 @@ module Err = struct
     strf "too many arguments, don't know what to do with %s"
       (String.concat ", " (List.map quote excess))
 
+  let pos_miss a =
+    if a.docv = "" then strf "a required argument is missing" else
+    strf "required argument %s is missing" a.docv
+
+  let pos_misses = function
+  | [] -> assert false
+  | [a] -> pos_miss a
+  | args ->
+      let add_arg acc a = (if a.docv = "" then "ARG" else a.docv) :: acc in
+      let rev_args = List.sort rev_pos_arg_cli_order args in
+      let args = List.fold_left add_arg [] rev_args in
+      let args = String.concat ", " args in
+      strf "required arguments %s are missing" args
+
   let flag_value f v =
     strf "option %s is a flag, it cannot take the argument %s"
       (quote f) (quote v)
@@ -441,8 +455,7 @@ module Err = struct
       in
       strf "required option %s is missing" (long_name a.o_names)
     else
-    if a.docv = "" then strf "a required argument is missing" else
-    strf "required argument %s is missing" a.docv
+    pos_miss a
 
   (* Error printers *)
 
@@ -598,11 +611,15 @@ end = struct
     (* returns an updated [cl] cmdline in which each positional arg mentioned
        in the list index posi, is given a value according the list
        of positional arguments values [pargs]. *)
-    if pargs = [] then cl else
+    if pargs = [] then
+      match List.filter (fun a -> a.absent = Error) posi with
+      | [] -> cl
+      | misses -> raise (Error (Err.pos_misses misses))
+    else
     let last = List.length pargs - 1 in
     let pos rev k = if rev then last - k else k in
-    let rec loop cl max_spec = function
-    | [] -> cl, max_spec
+    let rec loop misses cl max_spec = function
+    | [] -> misses, cl, max_spec
     | a :: al ->
         let rev = a.p_kind.pos_rev in
         let start = pos rev a.p_kind.pos_start in
@@ -611,12 +628,16 @@ end = struct
         | Some n -> pos rev (a.p_kind.pos_start + n - 1)
         in
         let start, stop = if rev then stop, start else start, stop in
-        let arg = P (take_range start stop pargs) in
+        let args = take_range start stop pargs in
         let max_spec = max stop max_spec in
-        let cl = Amap.add a arg cl in
-        loop cl max_spec al
+        let cl = Amap.add a (P args) cl in
+        let misses =
+          if a.absent = Error && args = [] then (a:: misses) else misses
+        in
+        loop misses cl max_spec al
     in
-    let cl, max_spec = loop cl (-1) posi in
+    let misses, cl, max_spec = loop [] cl (-1) posi in
+    if misses <> [] then raise (Error (Err.pos_misses misses)) else
     if last <= max_spec then cl else
     let excess = take_range (max_spec + 1) last pargs in
     raise (Error (Err.pos_excess excess))

vendor/opam/cmdliner/pkg/pkg.ml
@@ -14,8 +14,8 @@ let () =
        test "test/rm_ex";
        test "test/tail_ex";
        Pkg.test ~run:false "test/test_man";
-       Pkg.test ~run:false "test/test_err";
        Pkg.test ~run:false "test/test_pos";
        Pkg.test ~run:false "test/test_pos_rev";
        Pkg.test ~run:false "test/test_pos_all";
-       Pkg.test ~run:false "test/test_pos_left"; ]
+       Pkg.test ~run:false "test/test_pos_left";
+       Pkg.test ~run:false "test/test_pos_req"; ]

vendor/opam/cmdliner/test/test_err.ml
@@ -1,17 +1,0 @@
-open Cmdliner
-
-let err_test strict a0 a1 a2 = ()
-
-let err_test =
-  let req p docv = Arg.(required & pos p (some string) None & info [] ~docv) in
-  let pkg = req 0 "PKG" in
-  let version = req 1 "VERSION" in
-  let platform = req 2 "PLATFORM" in
-  let strict = Arg.(value & flag & info ["strict"]) in
-  let doc = "Test required positional argument error reporting" in
-  Term.(const err_test $ strict $ pkg $ version $ platform),
-  Term.info "err_test" ~doc
-
-let () = match Term.eval err_test with
-| `Error _ -> exit 1
-| _ -> exit 0

vendor/opam/cmdliner/test/test_pos_req.ml
@@ -1,0 +1,17 @@
+open Cmdliner
+
+let pos r a1 a0 a2  =
+  print_endline (String.concat "\n" ([a0; a1; a2; "--"] @ r))
+
+let test_pos =
+  let req p =
+    let docv = Printf.sprintf "ARG%d" p in
+    Arg.(required & pos p (some string) None & info [] ~docv)
+  in
+  let right = Arg.(non_empty & pos_right 2 string [] & info [] ~docv:"RIGHT") in
+  Term.(const pos $ right $ req 1 $ req 0 $ req 2),
+  Term.info "test_pos_req" ~doc:"Test pos req arguments"
+
+let () = match Term.eval test_pos with
+| `Error _ -> exit 1
+| _ -> exit 0

commit b8da67ffa755093c330d6f1082bc27dc40811c6c

Get rid of internal Cmdline.Error exception.
Two side effects:

- Fixes regression introduced by 3a55205223799b3d89d813 (closes #39).
- All unknown or ambiguous optional arguments are now reported
  rather than just the first one.

vendor/opam/cmdliner/src/cmdliner.ml
@@ -444,14 +444,12 @@ end
    and convert command line arguments (see the Arg module). *)
 
 module Cmdline :sig
-  exception Error of string
   type t
-  val create : ?peek_opts:bool -> arg_info list -> string list -> t
+  val create : ?peek_opts:bool -> arg_info list -> string list ->
+    (t, string * t) result
   val opt_arg : t -> arg_info -> (int * string * (string option)) list
   val pos_arg : t -> arg_info -> string list
 end = struct
-
-  exception Error of string
 
   module Arg_info = struct
     type t = arg_info
@@ -515,15 +513,15 @@ end = struct
     | true, [] -> [short_opt]
     | true, l -> if List.mem short_opt l then l else short_opt :: l
 
-  let parse_args ~peek_opts optidx cl args =
+  let parse_opt_args ~peek_opts optidx cl args =
     (* returns an updated [cl] cmdline according to the options found in [args]
        with the trie index [optidx]. Positional arguments are returned in order
        in a list. *)
-    let rec loop k cl pargs = function
-    | [] -> cl, (List.rev pargs)
-    | "--" :: args -> cl, (List.rev_append pargs args)
+    let rec loop errs k cl pargs = function
+    | [] -> List.rev errs, cl, List.rev pargs
+    | "--" :: args -> List.rev errs, cl, (List.rev_append pargs args)
     | s :: args ->
-        if not (is_opt s) then loop (k + 1) cl (s :: pargs) args else
+        if not (is_opt s) then loop errs (k + 1) cl (s :: pargs) args else
         let name, value = parse_opt_arg s in
         match Cmdliner_trie.find optidx name with
         | `Ok a ->
@@ -537,17 +535,22 @@ end = struct
                 | v :: rest -> if is_opt v then None, args else Some v, rest
             in
             let arg = O ((k, name, value) :: opt_arg cl a) in
-            loop (k + 1) (Amap.add a arg cl) pargs args
-        | `Not_found when peek_opts -> loop (k + 1) cl pargs args (* skip *)
+            loop errs (k + 1) (Amap.add a arg cl) pargs args
+        | `Not_found when peek_opts -> loop errs (k + 1) cl pargs args
         | `Not_found ->
             let hints = hint_matching_opt optidx s in
-            raise (Error (Err.unknown "option" ~hints name))
+            let err = Err.unknown "option" ~hints name in
+            loop (err :: errs) (k + 1) cl pargs args
         | `Ambiguous ->
             let ambs = Cmdliner_trie.ambiguities optidx name in
             let ambs = List.sort compare ambs in
-            raise (Error (Cmdliner_base.err_ambiguous "option" name ambs))
+            let err = Cmdliner_base.err_ambiguous "option" name ambs in
+            loop (err :: errs) (k + 1) cl pargs args
     in
-    loop 0 cl [] args
+    let errs, cl, pargs = loop [] 0 cl [] args in
+    if errs = [] then Ok (cl, pargs) else
+    let err = String.concat "\n" errs in
+    Error (err, cl, pargs)
 
   let take_range start stop l =
     let rec loop i acc = function
@@ -559,14 +562,13 @@ end = struct
     in
     loop 0 [] l
 
-  let process_pos_args posi cl pargs =
+  let process_pos_args posidx cl pargs =
     (* returns an updated [cl] cmdline in which each positional arg mentioned
-       in the list index posi, is given a value according the list
+       in the list index posidx, is given a value according the list
        of positional arguments values [pargs]. *)
     if pargs = [] then
-      match List.filter (fun a -> a.absent = Error) posi with
-      | [] -> cl
-      | misses -> raise (Error (Err.pos_misses misses))
+      let misses = List.filter (fun a -> a.absent = Error) posidx in
+      if misses = [] then Ok cl else Error (Err.pos_misses misses, cl)
     else
     let last = List.length pargs - 1 in
     let pos rev k = if rev then last - k else k in
@@ -588,20 +590,23 @@ end = struct
         in
         loop misses cl max_spec al
     in
-    let misses, cl, max_spec = loop [] cl (-1) posi in
-    if misses <> [] then raise (Error (Err.pos_misses misses)) else
-    if last <= max_spec then cl else
+    let misses, cl, max_spec = loop [] cl (-1) posidx in
+    if misses <> [] then Error (Err.pos_misses misses, cl) else
+    if last <= max_spec then Ok cl else
     let excess = take_range (max_spec + 1) last pargs in
-    raise (Error (Err.pos_excess excess))
+    Error (Err.pos_excess excess, cl)
 
   let create ?(peek_opts = false) al args =
-    let opti, posi, cl = arg_info_indexes al in
-    let cl, pargs = parse_args ~peek_opts opti cl args in
-    if peek_opts then cl (* skip positional arguments *) else
-    process_pos_args posi cl pargs
+    let optidx, posidx, cl = arg_info_indexes al in
+    match parse_opt_args ~peek_opts optidx cl args with
+    | Ok (cl, _) when peek_opts -> Ok cl
+    | Ok (cl, pargs) -> process_pos_args posidx cl pargs
+    | Error (errs, cl, _) -> Error (errs, cl)
 end
 
 module Arg = struct
+  exception Error of string
+
   type 'a parser = string -> [ `Ok of 'a | `Error of string ]
   type 'a printer = Format.formatter -> 'a -> unit
   type 'a converter = 'a parser * 'a printer
@@ -615,7 +620,7 @@ module Arg = struct
     { env_var = env_var; env_doc = doc; env_docs = docs }
 
   let ( & ) f x = f x
-  let parse_error e = raise (Cmdline.Error e)
+  let parse_error e = raise (Error e)
   let some ?(none = "") (parse, print) =
     (fun s -> match parse s with `Ok v -> `Ok (Some v) | `Error _ as e -> e),
     (fun ppf v -> match v with
@@ -936,14 +941,19 @@ module Term = struct
 
   let eval_term help err ei f args =
     let help_arg, vers_arg, ei = add_std_opts ei in
-    try
-      let cl = Cmdline.create (snd ei.term) args in
-      match help_arg ei cl, vers_arg with
-      | Some fmt, _ -> Help.print fmt help ei; `Help
-      | None, Some v_arg when v_arg ei cl -> Help.pp_version help ei; `Version
-      | _ -> `Ok (f ei cl)
+    try match Cmdline.create (snd ei.term) args with
+    | Error (e, cl) ->
+        begin match help_arg ei cl with
+        | Some fmt -> Help.print fmt help ei; `Help
+        | None -> Err.pp_usage err ei e; `Error `Parse
+        end
+    | Ok cl ->
+        match help_arg ei cl, vers_arg with
+        | Some fmt, _ -> Help.print fmt help ei; `Help
+        | None, Some v_arg when v_arg ei cl -> Help.pp_version help ei; `Version
+        | _ -> `Ok (f ei cl)
     with
-    | Cmdline.Error e -> Err.pp_usage err ei e; `Error `Parse
+    | Arg.Error e -> Err.pp_usage err ei e; `Error `Parse
     | Term (`Error (usage, e)) ->
         if usage then Err.pp_usage err ei e else Err.print err ei e;
         `Error `Term
@@ -1015,17 +1025,23 @@ module Term = struct
     let ei = { term = term; main = term; choices = []; env = env } in
     let help_arg, vers_arg, ei = add_std_opts ei in
     try
-      let cl = Cmdline.create ~peek_opts:true (snd ei.term) args in
-      match help_arg ei cl, vers_arg with
-      | Some fmt, _ ->
-          (try (Some (f ei cl), `Help) with e -> None, `Help)
-      | None, Some v_arg when v_arg ei cl ->
-          (try (Some (f ei cl), `Version) with e -> None, `Version)
-      | _ ->
-          let v = f ei cl in
-          Some v, `Ok v
+      match Cmdline.create ~peek_opts:true (snd ei.term) args with
+      | Error (err, cl) ->
+          begin match help_arg ei cl with
+          | Some fmt -> None, `Help
+          | None -> None, (`Error `Parse)
+          end
+      | Ok cl ->
+          match help_arg ei cl, vers_arg with
+          | Some fmt, _ ->
+              (try (Some (f ei cl), `Help) with e -> None, `Help)
+          | None, Some v_arg when v_arg ei cl ->
+              (try (Some (f ei cl), `Version) with e -> None, `Version)
+          | _ ->
+              let v = f ei cl in
+              Some v, `Ok v
     with
-    | Cmdline.Error _ -> None, (`Error `Parse)
+    | Arg.Error _ -> None, (`Error `Parse)
     | Term _ -> None, (`Error `Term)
     | e -> None, (`Error `Exn)
 end