dstrekelj/opam

Env variable names case-insensitive on Windows
Environment variable names are not case sensitive on Windows and PATH is
by default returned as Path. Fix OpamStd.Env.get to use a case insensitive
search on Windows (and similarly the internal function OpamSystem.env_var)
and ensure that code which manipulates a list of environment variables
allows for case-insensitive removal (opamState.add_to_env, etc.)

src/core/opamStd.ml
@@ -476,8 +476,13 @@ module Env = struct
     ) in
     fun () -> Lazy.force lazy_env
 
-  let get n =
-    List.assoc n (list ())
+  let get =
+    if Sys.os_type = "Win32" then
+      fun n ->
+        let n = String.uppercase n in
+        snd (List.find (fun (k,_) -> String.uppercase k = n) (list ()))
+    else
+      fun n -> List.assoc n (list ())
 
   let getopt n = try Some (get n) with Not_found -> None
 

src/core/opamSystem.ml
@@ -273,12 +273,13 @@ let default_env =
 
 let env_var env var =
   let len = Array.length env in
-  let prefix = var^"=" in
+  let f = if OpamStd.(Sys.os () = Sys.Win32) then String.uppercase else fun x -> x in
+  let prefix = f var^"=" in
   let pfxlen = String.length prefix in
   let rec aux i =
     if i >= len then "" else
     let s = env.(i) in
-    if OpamStd.String.starts_with ~prefix s then
+    if OpamStd.String.starts_with ~prefix (f s) then
       String.sub s pfxlen (String.length s - pfxlen)
     else aux (i+1)
   in

avsm/unpac

vendor/opam/opam-core/src/core/opamStd.ml
@@ -533,8 +533,13 @@ module Env = struct
     ) in
     fun () -> Lazy.force lazy_env
 
-  let get n =
-    List.assoc n (list ())
+  let get =
+    if Sys.os_type = "Win32" then
+      fun n ->
+        let n = String.uppercase_ascii n in
+        snd (List.find (fun (k,_) -> String.uppercase_ascii k = n) (list ()))
+    else
+      fun n -> List.assoc n (list ())
 
   let getopt n = try Some (get n) with Not_found -> None
 

vendor/opam/opam-core/src/core/opamSystem.ml
@@ -297,12 +297,13 @@ let default_env =
 
 let env_var env var =
   let len = Array.length env in
-  let prefix = var^"=" in
+  let f = if OpamStd.(Sys.os () = Sys.Win32) then String.uppercase_ascii else fun x -> x in
+  let prefix = f var^"=" in
   let pfxlen = String.length prefix in
   let rec aux i =
     if i >= len then "" else
     let s = env.(i) in
-    if OpamStd.String.starts_with ~prefix s then
+    if OpamStd.String.starts_with ~prefix (f s) then
       String.sub s pfxlen (String.length s - pfxlen)
     else aux (i+1)
   in