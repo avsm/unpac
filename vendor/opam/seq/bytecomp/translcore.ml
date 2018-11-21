lpw25/ocaml

Remove positions from paths

bytecomp/translcore.ml
@@ -65,7 +65,7 @@ let transl_extension_constructor env path ext =
          Lprim (prim_fresh_oo_id, [Lconst (Const_base (Const_int 0))], loc)],
         loc)
   | Text_rebind(path, _lid) ->
-      transl_extension_path ~loc env path
+      transl_extension_path loc env path
 
 (* To propagate structured constants *)
 
@@ -84,7 +84,7 @@ let extract_float = function
 
 type binding =
   | Bind_value of value_binding list
-  | Bind_module of Ident.t * string loc * module_expr
+  | Bind_module of Ident.t * string loc * module_presence * module_expr
 
 let rec push_defaults loc bindings cases partial =
   match cases with
@@ -104,9 +104,11 @@ let rec push_defaults loc bindings cases partial =
                    partial
   | [{c_lhs=pat; c_guard=None;
       c_rhs={exp_attributes=[{txt="#modulepat"},_];
-             exp_desc = Texp_letmodule
-               (id, name, mexpr, ({exp_desc = Texp_function _} as e2))}}] ->
-      push_defaults loc (Bind_module (id, name, mexpr) :: bindings)
+             exp_desc =
+               Texp_letmodule
+                 (id, name, pres, mexpr,
+                  ({exp_desc = Texp_function _} as e2))}}] ->
+      push_defaults loc (Bind_module (id, name, pres, mexpr) :: bindings)
                    [{c_lhs=pat;c_guard=None;c_rhs=e2}]
                    partial
   | [case] ->
@@ -116,23 +118,25 @@ let rec push_defaults loc bindings cases partial =
             {exp with exp_desc =
              match binds with
              | Bind_value binds -> Texp_let(Nonrecursive, binds, exp)
-             | Bind_module (id, name, mexpr) ->
-                 Texp_letmodule (id, name, mexpr, exp)})
+             | Bind_module (id, name, pres, mexpr) ->
+                 Texp_letmodule (id, name, pres, mexpr, exp)})
           case.c_rhs bindings
       in
       [{case with c_rhs=exp}]
   | {c_lhs=pat; c_rhs=exp; c_guard=_} :: _ when bindings <> [] ->
       let param = Typecore.name_cases "param" cases in
+      let desc =
+        {val_type = pat.pat_type; val_kind = Val_reg;
+         val_attributes = []; Types.val_loc = Location.none; }
+      in
+      let env = Env.add_value param desc exp.exp_env in
       let name = Ident.name param in
       let exp =
-        { exp with exp_loc = loc; exp_desc =
+        { exp with exp_loc = loc; exp_env = env; exp_desc =
           Texp_match
-            ({exp with exp_type = pat.pat_type; exp_desc =
-              Texp_ident (Path.Pident param, mknoloc (Longident.Lident name),
-                          {val_type = pat.pat_type; val_kind = Val_reg;
-                           val_attributes = [];
-                           Types.val_loc = Location.none;
-                          })},
+            ({exp with exp_type = pat.pat_type; exp_env = env; exp_desc =
+              Texp_ident
+                (Path.Pident param, mknoloc (Longident.Lident name), desc)},
              cases, partial) }
       in
       push_defaults loc bindings
@@ -163,11 +167,16 @@ let event_function exp lam =
 (* Assertions *)
 
 let assert_failed exp =
+  let slot =
+    transl_extension_path Location.none
+      Env.initial_safe_string Predef.path_assert_failure
+  in
   let (fname, line, char) =
-    Location.get_pos_info exp.exp_loc.Location.loc_start in
+    Location.get_pos_info exp.exp_loc.Location.loc_start
+  in
   Lprim(Praise Raise_regular, [event_after exp
     (Lprim(Pmakeblock(0, Immutable, None),
-          [transl_normal_path Predef.path_assert_failure;
+          [slot;
            Lconst(Const_block(0,
               [Const_base(Const_string (fname, None));
                Const_base(Const_int line);
@@ -207,7 +216,7 @@ and transl_exp0 e =
   | Texp_ident(_, _, {val_kind = Val_anc _}) ->
       raise(Error(e.exp_loc, Free_super_var))
   | Texp_ident(path, _, {val_kind = Val_reg | Val_self _}) ->
-      transl_value_path ~loc:e.exp_loc e.exp_env path
+      transl_value_path e.exp_loc e.exp_env path
   | Texp_ident _ -> fatal_error "Translcore.transl_exp: bad Texp_ident"
   | Texp_constant cst ->
       Lconst(Const_base cst)
@@ -304,14 +313,14 @@ and transl_exp0 e =
             Lprim(Pmakeblock(n, Immutable, Some shape), ll, e.exp_loc)
           end
       | Cstr_extension(path, is_const) ->
-          if is_const then
-            transl_extension_path e.exp_env path
+          let lam = transl_extension_path e.exp_loc e.exp_env path in
+          if is_const then lam
           else
             Lprim(Pmakeblock(0, Immutable, Some (Pgenval :: shape)),
-                  transl_extension_path e.exp_env path :: ll, e.exp_loc)
+                  lam :: ll, e.exp_loc)
       end
   | Texp_extension_constructor (_, path) ->
-      transl_extension_path e.exp_env path
+      transl_extension_path e.exp_loc e.exp_env path
   | Texp_variant(l, arg) ->
       let tag = Btype.hash_variant l in
       begin match arg with
@@ -335,7 +344,7 @@ and transl_exp0 e =
           Lprim (Pfield lbl.lbl_pos, [targ], e.exp_loc)
         | Record_unboxed _ -> targ
         | Record_float -> Lprim (Pfloatfield lbl.lbl_pos, [targ], e.exp_loc)
-        | Record_extension ->
+        | Record_extension _ ->
           Lprim (Pfield (lbl.lbl_pos + 1), [targ], e.exp_loc)
       end
   | Texp_setfield(arg, _, lbl, newval) ->
@@ -346,7 +355,7 @@ and transl_exp0 e =
           Psetfield(lbl.lbl_pos, maybe_pointer newval, Assignment)
         | Record_unboxed _ -> assert false
         | Record_float -> Psetfloatfield (lbl.lbl_pos, Assignment)
-        | Record_extension ->
+        | Record_extension _ ->
           Psetfield (lbl.lbl_pos + 1, maybe_pointer newval, Assignment)
       in
       Lprim(access, [transl_exp arg; transl_exp newval], e.exp_loc)
@@ -426,31 +435,36 @@ and transl_exp0 e =
       Lapply{ap_should_be_tailcall=false;
              ap_loc=loc;
              ap_func=
-               Lprim(Pfield 0, [transl_class_path ~loc e.exp_env cl], loc);
+               Lprim(Pfield 0, [transl_class_path loc e.exp_env cl], loc);
              ap_args=[lambda_unit];
              ap_inlined=Default_inline;
              ap_specialised=Default_specialise}
   | Texp_instvar(path_self, path, _) ->
-      Lprim(Pfield_computed,
-            [transl_normal_path path_self; transl_normal_path path], e.exp_loc)
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
+      let var = transl_value_path e.exp_loc e.exp_env path in
+      Lprim(Pfield_computed, [self; var], e.exp_loc)
   | Texp_setinstvar(path_self, path, _, expr) ->
-      transl_setinstvar e.exp_loc (transl_normal_path path_self) path expr
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
+      let var = transl_value_path e.exp_loc e.exp_env path in
+      transl_setinstvar e.exp_loc self var expr
   | Texp_override(path_self, modifs) ->
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
       let cpy = Ident.create "copy" in
       Llet(Strict, Pgenval, cpy,
            Lapply{ap_should_be_tailcall=false;
                   ap_loc=Location.none;
                   ap_func=Translobj.oo_prim "copy";
-                  ap_args=[transl_normal_path path_self];
+                  ap_args=[self];
                   ap_inlined=Default_inline;
                   ap_specialised=Default_specialise},
            List.fold_right
              (fun (path, _, expr) rem ->
+               let var = transl_value_path e.exp_loc e.exp_env path in
                 Lsequence(transl_setinstvar Location.none
-                            (Lvar cpy) path expr, rem))
+                            (Lvar cpy) var expr, rem))
              modifs
              (Lvar cpy))
-  | Texp_letmodule(id, loc, modl, body) ->
+  | Texp_letmodule(id, loc, Mta_present, modl, body) ->
       let defining_expr =
         Levent (!transl_module Tcoerce_none None modl, {
           lev_loc = loc.loc;
@@ -460,6 +474,8 @@ and transl_exp0 e =
         })
       in
       Llet(Strict, Pgenval, id, defining_expr, transl_exp body)
+  | Texp_letmodule(_, _, Mta_absent, _, body) ->
+      transl_exp body
   | Texp_letexception(cd, body) ->
       Llet(Strict, Pgenval,
            cd.ext_id, transl_extension_constructor e.exp_env None cd,
@@ -707,7 +723,7 @@ and transl_let rec_flag pat_expr_list =
 
 and transl_setinstvar loc self var expr =
   Lprim(Psetfield_computed (maybe_pointer expr, Assignment),
-    [self; transl_normal_path var; transl_exp expr], loc)
+    [self; var; transl_exp expr], loc)
 
 and transl_record loc env fields repres opt_init_expr =
   let size = Array.length fields in
@@ -729,7 +745,7 @@ and transl_record loc env fields repres opt_init_expr =
                  match repres with
                    Record_regular | Record_inlined _ -> Pfield i
                  | Record_unboxed _ -> assert false
-                 | Record_extension -> Pfield (i + 1)
+                 | Record_extension _ -> Pfield (i + 1)
                  | Record_float -> Pfloatfield i in
                Lprim(access, [Lvar init_id], loc), field_kind
            | Overridden (_lid, expr) ->
@@ -752,7 +768,7 @@ and transl_record loc env fields repres opt_init_expr =
         | Record_unboxed _ -> Lconst(match cl with [v] -> v | _ -> assert false)
         | Record_float ->
             Lconst(Const_float_array(List.map extract_float cl))
-        | Record_extension ->
+        | Record_extension _ ->
             raise Not_constant
       with Not_constant ->
         match repres with
@@ -763,14 +779,8 @@ and transl_record loc env fields repres opt_init_expr =
         | Record_unboxed _ -> (match ll with [v] -> v | _ -> assert false)
         | Record_float ->
             Lprim(Pmakearray (Pfloatarray, mut), ll, loc)
-        | Record_extension ->
-            let path =
-              let (label, _) = fields.(0) in
-              match label.lbl_res.desc with
-              | Tconstr(p, _, _) -> p
-              | _ -> assert false
-            in
-            let slot = transl_extension_path env path in
+        | Record_extension path ->
+            let slot = transl_extension_path loc env path in
             Lprim(Pmakeblock(0, mut, Some (Pgenval :: shape)), slot :: ll, loc)
     in
     begin match opt_init_expr with
@@ -793,7 +803,7 @@ and transl_record loc env fields repres opt_init_expr =
                 Psetfield(lbl.lbl_pos, maybe_pointer expr, Assignment)
             | Record_unboxed _ -> assert false
             | Record_float -> Psetfloatfield (lbl.lbl_pos, Assignment)
-            | Record_extension ->
+            | Record_extension _ ->
                 Psetfield(lbl.lbl_pos + 1, maybe_pointer expr, Assignment)
           in
           Lsequence(Lprim(upd, [Lvar copy_id; transl_exp expr], loc), cont)

toplevel/topdirs.ml
@@ -353,7 +353,7 @@ let dir_install_printer ppf lid =
   try
     let ((ty_arg, ty), path, is_old_style) =
       find_printer_type ppf lid in
-    let v = eval_path !toplevel_env path in
+    let v = eval_value_path !toplevel_env path in
     match ty with
     | None ->
        let print_function =
@@ -418,7 +418,7 @@ let dir_trace ppf lid =
         fprintf ppf "%a is an external function and cannot be traced.@."
         Printtyp.longident lid
     | _ ->
-        let clos = eval_path !toplevel_env path in
+        let clos = eval_value_path !toplevel_env path in
         (* Nothing to do if it's not a closure *)
         if Obj.is_block clos
         && (Obj.tag clos = Obj.closure_tag || Obj.tag clos = Obj.infix_tag)
@@ -483,8 +483,8 @@ let trim_signature = function
       Mty_signature
         (List.map
            (function
-               Sig_module (id, md, rs) ->
-                 Sig_module (id, {md with md_attributes =
+               Sig_module (id, pres, md, rs) ->
+                 Sig_module (id, pres, {md with md_attributes =
                                     (Location.mknoloc "...", Parsetree.PStr [])
                                     :: md.md_attributes},
                              rs)
@@ -572,10 +572,11 @@ let () =
        let rec accum_aliases path acc =
          let md = Env.find_module path env in
          let acc =
-           Sig_module (id, {md with md_type = trim_signature md.md_type},
+           Sig_module (id, Mta_present,
+                       {md with md_type = trim_signature md.md_type},
                        Trec_not) :: acc in
          match md.md_type with
-         | Mty_alias(_, path) -> accum_aliases path acc
+         | Mty_alias path -> accum_aliases path acc
          | Mty_ident _ | Mty_signature _ | Mty_functor _ ->
              List.rev acc
        in

avsm/unpac

vendor/opam/seq/bytecomp/translcore.ml
@@ -65,7 +65,7 @@ let transl_extension_constructor env path ext =
          Lprim (prim_fresh_oo_id, [Lconst (Const_base (Const_int 0))], loc)],
         loc)
   | Text_rebind(path, _lid) ->
-      transl_extension_path ~loc env path
+      transl_extension_path loc env path
 
 (* To propagate structured constants *)
 
@@ -84,7 +84,7 @@ let extract_float = function
 
 type binding =
   | Bind_value of value_binding list
-  | Bind_module of Ident.t * string loc * module_expr
+  | Bind_module of Ident.t * string loc * module_presence * module_expr
 
 let rec push_defaults loc bindings cases partial =
   match cases with
@@ -105,8 +105,9 @@ let rec push_defaults loc bindings cases partial =
   | [{c_lhs=pat; c_guard=None;
       c_rhs={exp_attributes=[{Parsetree.attr_name = {txt="#modulepat"};_}];
              exp_desc = Texp_letmodule
-               (id, name, mexpr, ({exp_desc = Texp_function _} as e2))}}] ->
-      push_defaults loc (Bind_module (id, name, mexpr) :: bindings)
+               (id, name, pres, mexpr,
+                ({exp_desc = Texp_function _} as e2))}}] ->
+      push_defaults loc (Bind_module (id, name, pres, mexpr) :: bindings)
                    [{c_lhs=pat;c_guard=None;c_rhs=e2}]
                    partial
   | [case] ->
@@ -116,23 +117,25 @@ let rec push_defaults loc bindings cases partial =
             {exp with exp_desc =
              match binds with
              | Bind_value binds -> Texp_let(Nonrecursive, binds, exp)
-             | Bind_module (id, name, mexpr) ->
-                 Texp_letmodule (id, name, mexpr, exp)})
+             | Bind_module (id, name, pres, mexpr) ->
+                 Texp_letmodule (id, name, pres, mexpr, exp)})
           case.c_rhs bindings
       in
       [{case with c_rhs=exp}]
   | {c_lhs=pat; c_rhs=exp; c_guard=_} :: _ when bindings <> [] ->
       let param = Typecore.name_cases "param" cases in
+      let desc =
+        {val_type = pat.pat_type; val_kind = Val_reg;
+         val_attributes = []; Types.val_loc = Location.none; }
+      in
+      let env = Env.add_value param desc exp.exp_env in
       let name = Ident.name param in
       let exp =
-        { exp with exp_loc = loc; exp_desc =
+        { exp with exp_loc = loc; exp_env = env; exp_desc =
           Texp_match
-            ({exp with exp_type = pat.pat_type; exp_desc =
-              Texp_ident (Path.Pident param, mknoloc (Longident.Lident name),
-                          {val_type = pat.pat_type; val_kind = Val_reg;
-                           val_attributes = [];
-                           Types.val_loc = Location.none;
-                          })},
+            ({exp with exp_type = pat.pat_type; exp_env = env; exp_desc =
+              Texp_ident
+                (Path.Pident param, mknoloc (Longident.Lident name), desc)},
              cases, partial) }
       in
       push_defaults loc bindings
@@ -163,11 +166,16 @@ let event_function exp lam =
 (* Assertions *)
 
 let assert_failed exp =
+  let slot =
+    transl_extension_path Location.none
+      Env.initial_safe_string Predef.path_assert_failure
+  in
   let (fname, line, char) =
-    Location.get_pos_info exp.exp_loc.Location.loc_start in
+    Location.get_pos_info exp.exp_loc.Location.loc_start
+  in
   Lprim(Praise Raise_regular, [event_after exp
     (Lprim(Pmakeblock(0, Immutable, None),
-          [transl_normal_path Predef.path_assert_failure;
+          [slot;
            Lconst(Const_block(0,
               [Const_base(Const_string (fname, None));
                Const_base(Const_int line);
@@ -207,7 +215,7 @@ and transl_exp0 e =
   | Texp_ident(_, _, {val_kind = Val_anc _}) ->
       raise(Error(e.exp_loc, Free_super_var))
   | Texp_ident(path, _, {val_kind = Val_reg | Val_self _}) ->
-      transl_value_path ~loc:e.exp_loc e.exp_env path
+      transl_value_path e.exp_loc e.exp_env path
   | Texp_ident _ -> fatal_error "Translcore.transl_exp: bad Texp_ident"
   | Texp_constant cst ->
       Lconst(Const_base cst)
@@ -304,14 +312,14 @@ and transl_exp0 e =
             Lprim(Pmakeblock(n, Immutable, Some shape), ll, e.exp_loc)
           end
       | Cstr_extension(path, is_const) ->
-          if is_const then
-            transl_extension_path e.exp_env path
+          let lam = transl_extension_path e.exp_loc e.exp_env path in
+          if is_const then lam
           else
             Lprim(Pmakeblock(0, Immutable, Some (Pgenval :: shape)),
-                  transl_extension_path e.exp_env path :: ll, e.exp_loc)
+                  lam :: ll, e.exp_loc)
       end
   | Texp_extension_constructor (_, path) ->
-      transl_extension_path e.exp_env path
+      transl_extension_path e.exp_loc e.exp_env path
   | Texp_variant(l, arg) ->
       let tag = Btype.hash_variant l in
       begin match arg with
@@ -335,7 +343,7 @@ and transl_exp0 e =
           Lprim (Pfield lbl.lbl_pos, [targ], e.exp_loc)
         | Record_unboxed _ -> targ
         | Record_float -> Lprim (Pfloatfield lbl.lbl_pos, [targ], e.exp_loc)
-        | Record_extension ->
+        | Record_extension _ ->
           Lprim (Pfield (lbl.lbl_pos + 1), [targ], e.exp_loc)
       end
   | Texp_setfield(arg, _, lbl, newval) ->
@@ -346,7 +354,7 @@ and transl_exp0 e =
           Psetfield(lbl.lbl_pos, maybe_pointer newval, Assignment)
         | Record_unboxed _ -> assert false
         | Record_float -> Psetfloatfield (lbl.lbl_pos, Assignment)
-        | Record_extension ->
+        | Record_extension _ ->
           Psetfield (lbl.lbl_pos + 1, maybe_pointer newval, Assignment)
       in
       Lprim(access, [transl_exp arg; transl_exp newval], e.exp_loc)
@@ -426,31 +434,36 @@ and transl_exp0 e =
       Lapply{ap_should_be_tailcall=false;
              ap_loc=loc;
              ap_func=
-               Lprim(Pfield 0, [transl_class_path ~loc e.exp_env cl], loc);
+               Lprim(Pfield 0, [transl_class_path loc e.exp_env cl], loc);
              ap_args=[lambda_unit];
              ap_inlined=Default_inline;
              ap_specialised=Default_specialise}
   | Texp_instvar(path_self, path, _) ->
-      Lprim(Pfield_computed,
-            [transl_normal_path path_self; transl_normal_path path], e.exp_loc)
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
+      let var = transl_value_path e.exp_loc e.exp_env path in
+      Lprim(Pfield_computed, [self; var], e.exp_loc)
   | Texp_setinstvar(path_self, path, _, expr) ->
-      transl_setinstvar e.exp_loc (transl_normal_path path_self) path expr
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
+      let var = transl_value_path e.exp_loc e.exp_env path in
+      transl_setinstvar e.exp_loc self var expr
   | Texp_override(path_self, modifs) ->
+      let self = transl_value_path e.exp_loc e.exp_env path_self in
       let cpy = Ident.create_local "copy" in
       Llet(Strict, Pgenval, cpy,
            Lapply{ap_should_be_tailcall=false;
                   ap_loc=Location.none;
                   ap_func=Translobj.oo_prim "copy";
-                  ap_args=[transl_normal_path path_self];
+                  ap_args=[self];
                   ap_inlined=Default_inline;
                   ap_specialised=Default_specialise},
            List.fold_right
              (fun (path, _, expr) rem ->
+               let var = transl_value_path e.exp_loc e.exp_env path in
                 Lsequence(transl_setinstvar Location.none
-                            (Lvar cpy) path expr, rem))
+                            (Lvar cpy) var expr, rem))
              modifs
              (Lvar cpy))
-  | Texp_letmodule(id, loc, modl, body) ->
+  | Texp_letmodule(id, loc, Mp_present, modl, body) ->
       let defining_expr =
         Levent (!transl_module Tcoerce_none None modl, {
           lev_loc = loc.loc;
@@ -460,6 +473,8 @@ and transl_exp0 e =
         })
       in
       Llet(Strict, Pgenval, id, defining_expr, transl_exp body)
+  | Texp_letmodule(_, _, Mp_absent, _, body) ->
+      transl_exp body
   | Texp_letexception(cd, body) ->
       Llet(Strict, Pgenval,
            cd.ext_id, transl_extension_constructor e.exp_env None cd,
@@ -708,7 +723,7 @@ and transl_let rec_flag pat_expr_list =
 
 and transl_setinstvar loc self var expr =
   Lprim(Psetfield_computed (maybe_pointer expr, Assignment),
-    [self; transl_normal_path var; transl_exp expr], loc)
+    [self; var; transl_exp expr], loc)
 
 and transl_record loc env fields repres opt_init_expr =
   let size = Array.length fields in
@@ -730,7 +745,7 @@ and transl_record loc env fields repres opt_init_expr =
                  match repres with
                    Record_regular | Record_inlined _ -> Pfield i
                  | Record_unboxed _ -> assert false
-                 | Record_extension -> Pfield (i + 1)
+                 | Record_extension _ -> Pfield (i + 1)
                  | Record_float -> Pfloatfield i in
                Lprim(access, [Lvar init_id], loc), field_kind
            | Overridden (_lid, expr) ->
@@ -753,7 +768,7 @@ and transl_record loc env fields repres opt_init_expr =
         | Record_unboxed _ -> Lconst(match cl with [v] -> v | _ -> assert false)
         | Record_float ->
             Lconst(Const_float_array(List.map extract_float cl))
-        | Record_extension ->
+        | Record_extension _ ->
             raise Not_constant
       with Not_constant ->
         match repres with
@@ -764,14 +779,8 @@ and transl_record loc env fields repres opt_init_expr =
         | Record_unboxed _ -> (match ll with [v] -> v | _ -> assert false)
         | Record_float ->
             Lprim(Pmakearray (Pfloatarray, mut), ll, loc)
-        | Record_extension ->
-            let path =
-              let (label, _) = fields.(0) in
-              match label.lbl_res.desc with
-              | Tconstr(p, _, _) -> p
-              | _ -> assert false
-            in
-            let slot = transl_extension_path env path in
+        | Record_extension path ->
+            let slot = transl_extension_path loc env path in
             Lprim(Pmakeblock(0, mut, Some (Pgenval :: shape)), slot :: ll, loc)
     in
     begin match opt_init_expr with
@@ -794,7 +803,7 @@ and transl_record loc env fields repres opt_init_expr =
                 Psetfield(lbl.lbl_pos, maybe_pointer expr, Assignment)
             | Record_unboxed _ -> assert false
             | Record_float -> Psetfloatfield (lbl.lbl_pos, Assignment)
-            | Record_extension ->
+            | Record_extension _ ->
                 Psetfield(lbl.lbl_pos + 1, maybe_pointer expr, Assignment)
           in
           Lsequence(Lprim(upd, [Lvar copy_id; transl_exp expr], loc), cont)

vendor/opam/seq/toplevel/topdirs.ml
@@ -348,7 +348,7 @@ let dir_install_printer ppf lid =
   try
     let ((ty_arg, ty), path, is_old_style) =
       find_printer_type ppf lid in
-    let v = eval_path !toplevel_env path in
+    let v = eval_value_path !toplevel_env path in
     match ty with
     | None ->
        let print_function =
@@ -413,7 +413,7 @@ let dir_trace ppf lid =
         fprintf ppf "%a is an external function and cannot be traced.@."
         Printtyp.longident lid
     | _ ->
-        let clos = eval_path !toplevel_env path in
+        let clos = eval_value_path !toplevel_env path in
         (* Nothing to do if it's not a closure *)
         if Obj.is_block clos
         && (Obj.tag clos = Obj.closure_tag || Obj.tag clos = Obj.infix_tag)
@@ -478,14 +478,14 @@ let trim_signature = function
       Mty_signature
         (List.map
            (function
-               Sig_module (id, md, rs) ->
+               Sig_module (id, pres, md, rs) ->
                  let attribute =
                    Ast_helper.Attr.mk
                      (Location.mknoloc "...")
                      (Parsetree.PStr [])
                  in
-                 Sig_module (id, {md with md_attributes =
-                                            attribute :: md.md_attributes},
+                 Sig_module (id, pres, {md with md_attributes =
+                                          attribute :: md.md_attributes},
                              rs)
              (*| Sig_modtype (id, Modtype_manifest mty) ->
                  Sig_modtype (id, Modtype_manifest (trim_modtype mty))*)
@@ -571,10 +571,11 @@ let () =
        let rec accum_aliases path acc =
          let md = Env.find_module path env in
          let acc =
-           Sig_module (id, {md with md_type = trim_signature md.md_type},
+           Sig_module (id, Mp_present,
+                       {md with md_type = trim_signature md.md_type},
                        Trec_not) :: acc in
          match md.md_type with
-         | Mty_alias(_, path) -> accum_aliases path acc
+         | Mty_alias path -> accum_aliases path acc
          | Mty_ident _ | Mty_signature _ | Mty_functor _ ->
              List.rev acc
        in