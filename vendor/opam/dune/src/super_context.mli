ocaml/dune

Add c_flags to env profile settings
Signed-off-by: Greta Yorsh <gyorsh@janestreet.com>

src/super_context.mli
@@ -53,6 +53,20 @@ val ocaml_flags
   -> Buildable.t
   -> Ocaml_flags.t
 
+val c_flags
+  :  t
+  -> dir:Path.t
+  -> lib:Library.t
+  -> string list
+  -> (unit, string list) Build.t
+
+val cxx_flags_gather
+  :  t
+  -> dir:Path.t
+  -> lib:Library.t
+  -> string list
+  -> (unit, string list) Build.t
+
 (** Binaries that are symlinked in the associated .bin directory of [dir]. This
     associated directory is [Path.relative dir ".bin"] *)
 val local_binaries : t -> dir:Path.t -> string File_bindings.t

avsm/unpac

vendor/opam/dune/src/super_context.mli
@@ -28,7 +28,6 @@ val packages  : t -> Package.t Package.Name.Map.t
 val libs_by_package : t -> (Package.t * Lib.Set.t) Package.Name.Map.t
 val file_tree : t -> File_tree.t
 val artifacts : t -> Artifacts.t
-val cxx_flags : t -> string list
 val build_dir : t -> Path.t
 val profile   : t -> string
 val host : t -> t
@@ -50,6 +49,20 @@ val ocaml_flags
   -> dir:Path.t
   -> Buildable.t
   -> Ocaml_flags.t
+
+val c_flags
+  :  t
+  -> dir:Path.t
+  -> expander:Expander.t
+  -> lib:Library.t
+  -> (unit, string list) Build.t
+
+val cxx_flags
+  :  t
+  -> dir:Path.t
+  -> expander:Expander.t
+  -> lib:Library.t
+  -> (unit, string list) Build.t
 
 (** Binaries that are symlinked in the associated .bin directory of [dir]. This
     associated directory is [Path.relative dir ".bin"] *)