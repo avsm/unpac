stedolan/ocaml

Statistical memory profiling of blocks allocated in the major heap.

runtime/memory.c
@@ -32,6 +32,7 @@
 #include "caml/misc.h"
 #include "caml/mlvalues.h"
 #include "caml/signals.h"
+#include "caml/memprof.h"
 
 int caml_huge_fallback_count = 0;
 /* Number of times that mmapping big pages fails and we fell back to small
@@ -477,7 +478,7 @@ color_t caml_allocation_color (void *hp)
   }
 }
 
-static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag,
+static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag, int track,
                                         int raise_oom, uintnat profinfo)
 {
   header_t *hp;
@@ -532,12 +533,9 @@ static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag,
     }
   }
 #endif
+  if(track)
+    caml_memprof_track_alloc_shr(Val_hp (hp));
   return Val_hp (hp);
-}
-
-CAMLexport value caml_alloc_shr_no_raise (mlsize_t wosize, tag_t tag)
-{
-  return caml_alloc_shr_aux(wosize, tag, 0, 0);
 }
 
 #ifdef WITH_PROFINFO
@@ -548,17 +546,23 @@ CAMLexport value caml_alloc_shr_no_raise (mlsize_t wosize, tag_t tag)
 CAMLexport value caml_alloc_shr_with_profinfo (mlsize_t wosize, tag_t tag,
                                                intnat profinfo)
 {
-  return caml_alloc_shr_aux(wosize, tag, 1, profinfo);
+  return caml_alloc_shr_aux(wosize, tag, 1, 1, profinfo);
 }
 
-CAMLexport value caml_alloc_shr_preserving_profinfo (mlsize_t wosize,
-  tag_t tag, header_t old_header)
+CAMLexport value caml_alloc_shr_for_minor_gc (mlsize_t wosize,
+                                              tag_t tag, header_t old_header)
 {
-  return caml_alloc_shr_with_profinfo (wosize, tag, Profinfo_hd(old_header));
+  return caml_alloc_shr_aux (wosize, tag, 0, 1, Profinfo_hd(old_header));
 }
 
 #else
 #define NO_PROFINFO 0
+
+CAMLexport value caml_alloc_shr_for_minor_gc (mlsize_t wosize,
+                                              tag_t tag, header_t old_header)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 1, NO_PROFINFO);
+}
 #endif /* WITH_PROFINFO */
 
 #if defined(NATIVE_CODE) && defined(WITH_SPACETIME)
@@ -569,10 +573,21 @@ CAMLexport value caml_alloc_shr (mlsize_t wosize, tag_t tag)
   return caml_alloc_shr_with_profinfo (wosize, tag,
     caml_spacetime_my_profinfo (NULL, wosize));
 }
+
+CAMLexport value caml_alloc_shr_for_intern (mlsize_t wosize, tag_t tag)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 0,
+                             caml_spacetime_my_profinfo (NULL, wosize));
+}
 #else
 CAMLexport value caml_alloc_shr (mlsize_t wosize, tag_t tag)
 {
-  return caml_alloc_shr_aux (wosize, tag, 1, NO_PROFINFO);
+  return caml_alloc_shr_aux (wosize, tag, 1, 1, NO_PROFINFO);
+}
+
+CAMLexport value caml_alloc_shr_for_intern (mlsize_t wosize, tag_t tag)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 0, NO_PROFINFO);
 }
 #endif
 

avsm/unpac

vendor/opam/seq/runtime/memory.c
@@ -32,6 +32,7 @@
 #include "caml/misc.h"
 #include "caml/mlvalues.h"
 #include "caml/signals.h"
+#include "caml/memprof.h"
 
 int caml_huge_fallback_count = 0;
 /* Number of times that mmapping big pages fails and we fell back to small
@@ -477,7 +478,7 @@ color_t caml_allocation_color (void *hp)
   }
 }
 
-static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag,
+static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag, int track,
                                         int raise_oom, uintnat profinfo)
 {
   header_t *hp;
@@ -532,12 +533,9 @@ static inline value caml_alloc_shr_aux (mlsize_t wosize, tag_t tag,
     }
   }
 #endif
+  if(track)
+    caml_memprof_track_alloc_shr(Val_hp (hp));
   return Val_hp (hp);
-}
-
-CAMLexport value caml_alloc_shr_no_raise (mlsize_t wosize, tag_t tag)
-{
-  return caml_alloc_shr_aux(wosize, tag, 0, 0);
 }
 
 #ifdef WITH_PROFINFO
@@ -548,17 +546,23 @@ CAMLexport value caml_alloc_shr_no_raise (mlsize_t wosize, tag_t tag)
 CAMLexport value caml_alloc_shr_with_profinfo (mlsize_t wosize, tag_t tag,
                                                intnat profinfo)
 {
-  return caml_alloc_shr_aux(wosize, tag, 1, profinfo);
+  return caml_alloc_shr_aux(wosize, tag, 1, 1, profinfo);
 }
 
-CAMLexport value caml_alloc_shr_preserving_profinfo (mlsize_t wosize,
-  tag_t tag, header_t old_header)
+CAMLexport value caml_alloc_shr_for_minor_gc (mlsize_t wosize,
+                                              tag_t tag, header_t old_header)
 {
-  return caml_alloc_shr_with_profinfo (wosize, tag, Profinfo_hd(old_header));
+  return caml_alloc_shr_aux (wosize, tag, 0, 1, Profinfo_hd(old_header));
 }
 
 #else
 #define NO_PROFINFO 0
+
+CAMLexport value caml_alloc_shr_for_minor_gc (mlsize_t wosize,
+                                              tag_t tag, header_t old_header)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 1, NO_PROFINFO);
+}
 #endif /* WITH_PROFINFO */
 
 #if defined(NATIVE_CODE) && defined(WITH_SPACETIME)
@@ -569,10 +573,21 @@ CAMLexport value caml_alloc_shr (mlsize_t wosize, tag_t tag)
   return caml_alloc_shr_with_profinfo (wosize, tag,
     caml_spacetime_my_profinfo (NULL, wosize));
 }
+
+CAMLexport value caml_alloc_shr_no_track (mlsize_t wosize, tag_t tag)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 0,
+                             caml_spacetime_my_profinfo (NULL, wosize));
+}
 #else
 CAMLexport value caml_alloc_shr (mlsize_t wosize, tag_t tag)
 {
-  return caml_alloc_shr_aux (wosize, tag, 1, NO_PROFINFO);
+  return caml_alloc_shr_aux (wosize, tag, 1, 1, NO_PROFINFO);
+}
+
+CAMLexport value caml_alloc_shr_no_track (mlsize_t wosize, tag_t tag)
+{
+  return caml_alloc_shr_aux (wosize, tag, 0, 0, NO_PROFINFO);
 }
 #endif
 