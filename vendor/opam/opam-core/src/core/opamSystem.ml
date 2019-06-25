dra27/opam

Fix OpamSystem.install for Windows
The install command doesn't make sense for Windows. OpamSystem.install
altered to use copying instead and also various checks added to guard
against package errors. In particular, .exe is automatically added to
executables (with a warning) if exec is true and warnings are displayed
if Cygwin-linked files or Unix shell-scripts are installed.

src/core/opamSystem.ml
@@ -444,6 +444,12 @@ let read_command_output ?verbose ?env ?metadata ?allow_stdin cmd =
 let verbose_for_base_commands () =
   OpamCoreConfig.(!r.verbose_level) >= 3
 
+let cygify =
+  if OpamStd.Sys.(os () = Win32) then
+    List.map apply_cygpath
+  else
+    fun x -> x
+
 let copy src dst =
   if (try Sys.is_directory src
       with Sys_error _ -> raise (File_not_found src))
@@ -453,12 +459,12 @@ let copy src dst =
   if Sys.file_exists dst
   then remove_file dst;
   mkdir (Filename.dirname dst);
-  command ~verbose:(verbose_for_base_commands ()) ["cp"; src; dst ]
+  command ~verbose:(verbose_for_base_commands ()) ("cp"::(cygify [src; dst ]))
 
 let mv src dst =
   if Sys.file_exists dst then remove_file dst;
   mkdir (Filename.dirname dst);
-  command ~verbose:(verbose_for_base_commands ()) ["mv"; src; dst ]
+  command ~verbose:(verbose_for_base_commands ()) ("mv"::(cygify [src; dst ]))
 
 let is_exec file =
   let stat = Unix.stat file in
@@ -540,8 +546,42 @@ let install ?exec src dst =
   let exec = match exec with
     | Some e -> e
     | None -> is_exec src in
-  command ("install" :: "-m" :: (if exec then "0755" else "0644") ::
-     [ src; dst ])
+  begin
+    if OpamStd.Sys.(os () = Win32) then
+      if exec then begin
+        let (dst, cygcheck) =
+          match classify_executable src with
+            `Exe arch ->
+              if not (Filename.check_suffix dst ".exe") then begin
+                OpamConsole.warning "Automatically adding .exe to %s" dst;
+                (dst ^ ".exe", true)
+              end else
+                (dst, true)
+          | `Dll arch ->
+              (* TODO Installation of .dll to bin is unfortunate, but not sure if it should be a warning *)
+              (dst, true)
+          | `Script ->
+              (* TODO Generate a .cmd wrapper (and warn about it - they're not perfect) *)
+              OpamConsole.warning "%s is a script; the command won't be available" dst;
+              (dst, false)
+          | `Unknown ->
+              (* TODO Installation of a non-executable file is unexpected, but not sure if it should be a warning/error *)
+              (dst, false) in
+        copy src dst;
+        if cygcheck then
+          match OpamStd.Sys.is_cygwin_variant dst with
+            `Native ->
+              ()
+          | `Cygwin ->
+              OpamConsole.warning "%s is a Cygwin-linked executable" dst
+          | `CygLinked ->
+              OpamConsole.warning "%s links with a Cygwin-compiled DLL (almost certainly a packaging or environment error)" dst
+      end else
+        copy src dst
+    else
+      command ("install" :: "-m" :: (if exec then "0755" else "0644") ::
+         [ src; dst ])
+  end
 
 let cpu_count () =
   try

avsm/unpac

Signed-off-by: David Allsopp <david.allsopp@metastack.com>

vendor/opam/opam-core/src/core/opamSystem.ml
@@ -11,6 +11,10 @@
 
 open OpamCompat
 
+type install_warning =
+  [ `Add_exe | `Install_dll | `Install_script | `Install_unknown | `Cygwin | `Cygwin_libraries ]
+type install_warning_fn = string -> install_warning -> unit
+
 exception Process_error of OpamProcess.result
 exception Internal_error of string
 exception Command_not_found of string
@@ -510,7 +514,13 @@ let read_command_output ?verbose ?env ?metadata ?allow_stdin cmd =
 let verbose_for_base_commands () =
   OpamCoreConfig.(!r.verbose_level) >= 3
 
-let copy_file src dst =
+let cygify f =
+  if Sys.win32 then
+    List.map (Lazy.force f)
+  else
+    fun x -> x
+
+let copy_file_aux f src dst =
   if (try Sys.is_directory src
       with Sys_error _ -> raise (File_not_found src))
   then internal_error "Cannot copy %s: it is a directory." src;
@@ -519,7 +529,9 @@ let copy_file src dst =
   if file_or_symlink_exists dst
   then remove_file dst;
   mkdir (Filename.dirname dst);
-  command ~verbose:(verbose_for_base_commands ()) ["cp"; src; dst ]
+  command ~verbose:(verbose_for_base_commands ()) ("cp"::(cygify f [src; dst]))
+
+let copy_file = copy_file_aux (get_cygpath_function ~command:"cp")
 
 let copy_dir src dst =
   if Sys.file_exists dst then
@@ -536,10 +548,12 @@ let copy_dir src dst =
      command ~verbose:(verbose_for_base_commands ())
        [ "cp"; "-PRp"; src; dst ])
 
-let mv src dst =
+let mv_aux f src dst =
   if file_or_symlink_exists dst then remove_file dst;
   mkdir (Filename.dirname dst);
-  command ~verbose:(verbose_for_base_commands ()) ["mv"; src; dst ]
+  command ~verbose:(verbose_for_base_commands ()) ("mv"::(cygify f [src; dst]))
+
+let mv = mv_aux (get_cygpath_function ~command:"mv")
 
 let is_exec file =
   let stat = Unix.stat file in
@@ -614,7 +628,24 @@ let classify_executable file =
     close_in c;
     `Unknown
 
-let install ?exec src dst =
+let default_install_warning dst = function
+| `Add_exe ->
+    OpamConsole.warning "Automatically adding .exe to %s" dst
+| `Install_dll ->
+    (* TODO Installation of .dll to bin is unfortunate, but not sure if it should be a warning *)
+    ()
+| `Install_script ->
+    (* TODO Generate a .cmd wrapper (and warn about it - they're not perfect) *)
+    OpamConsole.warning "%s is a script; the command won't be available" dst;
+| `Install_unknown ->
+    (* TODO Installation of a non-executable file is unexpected, but not sure if it should be a warning/error *)
+    ()
+| `Cygwin ->
+    OpamConsole.warning "%s is a Cygwin-linked executable" dst
+| `Cygwin_libraries ->
+    OpamConsole.warning "%s links with a Cygwin-compiled DLL (almost certainly a packaging or environment error)" dst
+
+let install ?(warning=default_install_warning) ?exec src dst =
   if Sys.is_directory src then
     internal_error "Cannot install %s: it is a directory." src;
   if (try Sys.is_directory dst with Sys_error _ -> false) then
@@ -623,8 +654,42 @@ let install ?exec src dst =
   let exec = match exec with
     | Some e -> e
     | None -> is_exec src in
-  command ("install" :: "-m" :: (if exec then "0755" else "0644") ::
-     [ src; dst ])
+  begin
+    if Sys.win32 then
+      if exec then begin
+        let (dst, cygcheck) =
+          match classify_executable src with
+            `Exe _ ->
+              if not (Filename.check_suffix dst ".exe") && not (Filename.check_suffix dst ".dll") then begin
+                warning dst `Add_exe;
+                (dst ^ ".exe", true)
+              end else
+                (dst, true)
+          | `Dll _ ->
+              warning dst `Install_dll;
+              (dst, true)
+          | `Script ->
+              warning dst `Install_script;
+              (dst, false)
+          | `Unknown ->
+              warning dst `Install_unknown;
+              (dst, false)
+        in
+        copy_file src dst;
+        if cygcheck then
+          match OpamStd.Sys.is_cygwin_variant dst with
+            `Native ->
+              ()
+          | `Cygwin ->
+              warning dst `Cygwin
+          | `CygLinked ->
+              warning dst `Cygwin_libraries
+      end else
+        copy_file src dst
+    else
+      command ("install" :: "-m" :: (if exec then "0755" else "0644") ::
+         [ src; dst ])
+  end
 
 let cpu_count () =
   try