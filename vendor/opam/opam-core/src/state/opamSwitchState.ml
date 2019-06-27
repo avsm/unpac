rjbou/opam

switch load: check that it is > 2.0 not equal to latest

src/state/opamSwitchState.ml
@@ -101,15 +101,15 @@ let load lock_kind gt rt switch =
   let switch_config = load_switch_config gt switch in
   if OpamVersion.compare
       (OpamVersion.nopatch (switch_config.OpamFile.Switch_config.opam_version))
-      (OpamVersion.nopatch OpamFormatUpgrade.latest_version)
-     <> 0 then
+      (OpamVersion.nopatch OpamFormatUpgrade.v2_0)
+     < 0 then
     OpamConsole.error_and_exit `Configuration_error
-      "Could not load opam switch %s: it reports version %s while %s was \
+      "Could not load opam switch %s: it reports version %s while > %s was \
        expected"
       (OpamSwitch.to_string switch)
       (OpamVersion.to_string
          (switch_config.OpamFile.Switch_config.opam_version))
-      (OpamVersion.to_string OpamFormatUpgrade.latest_version);
+      (OpamVersion.to_string OpamFormatUpgrade.v2_0);
   let { sel_installed = installed; sel_roots = installed_roots;
         sel_pinned = pinned; sel_compiler = compiler_packages; } =
     load_selections gt switch

avsm/unpac

vendor/opam/opam-core/src/state/opamSwitchState.ml
@@ -101,15 +101,15 @@ let load lock_kind gt rt switch =
   let switch_config = load_switch_config gt switch in
   if OpamVersion.compare
       (OpamVersion.nopatch (switch_config.OpamFile.Switch_config.opam_version))
-      (OpamVersion.nopatch OpamFormatUpgrade.latest_version)
-     <> 0 then
+      (OpamVersion.nopatch OpamFormatUpgrade.lastest_compatible_switch_version)
+     < 0 then
     OpamConsole.error_and_exit `Configuration_error
-      "Could not load opam switch %s: it reports version %s while %s was \
+      "Could not load opam switch %s: it reports version %s while > %s was \
        expected"
       (OpamSwitch.to_string switch)
       (OpamVersion.to_string
          (switch_config.OpamFile.Switch_config.opam_version))
-      (OpamVersion.to_string OpamFormatUpgrade.latest_version);
+      (OpamVersion.to_string OpamFormatUpgrade.lastest_compatible_switch_version);
   let { sel_installed = installed; sel_roots = installed_roots;
         sel_pinned = pinned; sel_compiler = compiler_packages; } =
     load_selections gt switch