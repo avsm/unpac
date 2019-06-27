rjbou/opam

Update format upgrade latest version

src/state/opamFormatUpgrade.mli
@@ -21,6 +21,7 @@ exception Upgrade_done of OpamFile.Config.t
 (** The latest version of the opam root format, that normal operation of this
     instance of opam requires *)
 val latest_version: OpamVersion.t
+val v2_0: OpamVersion.t
 
 (** Runs the upgrade from its current format to the latest version for the opam
    root at the given directory. A global write lock must be supplied. If an

avsm/unpac

vendor/opam/opam-core/src/state/opamFormatUpgrade.mli
@@ -21,6 +21,7 @@ exception Upgrade_done of OpamFile.Config.t
 (** The latest version of the opam root format, that normal operation of this
     instance of opam requires *)
 val latest_version: OpamVersion.t
+val lastest_compatible_switch_version: OpamVersion.t
 
 (** Runs the upgrade from its current format to the latest version for the opam
    root at the given directory. A global write lock must be supplied. If an