ocaml/opam

Improve `VCS.is_dirty` when `subpath` is defined.

src/repository/opamHg.ml
@@ -99,7 +99,7 @@ module VCS = struct
     | { OpamProcess.r_code = 0; OpamProcess.r_stdout = [b]; _ } -> Done (Some b)
     | _ -> Done None
 
-  let is_dirty dir =
+  let is_dirty ?subpath:_ dir =
     hg dir [ "stat" ] @@> fun r ->
     OpamSystem.raise_on_process_error r;
     Done (r.OpamProcess.r_stdout = [])

avsm/unpac

vendor/opam/opam-core/src/repository/opamHg.ml
@@ -111,7 +111,7 @@ module VCS = struct
             | branch::_ when branch <> "default" -> Done (Some branch)
             | _ -> Done None
 
-  let is_dirty repo_root =
+  let is_dirty ?subpath:_ repo_root =
     hg repo_root [ "status"; "--subrepos" ] @@> fun r ->
     OpamSystem.raise_on_process_error r;
     Done (r.OpamProcess.r_stdout = [])