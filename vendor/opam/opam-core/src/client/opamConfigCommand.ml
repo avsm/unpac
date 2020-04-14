rjbou/opam

depext configuration: add depxt options to setopt modifiable fields

src/client/opamConfigCommand.ml
@@ -637,6 +637,22 @@ let set_opt_global_t ?inner gt field value =
       Config.with_eval_variables (InitConfig.eval_variables in_config);
       "repository-validation-command", Fixed,
       Config.with_validation_hook_opt (Config.validation_hook Config.empty);
+      "depext-bypass", OpamSysPkg.Set.Op.(Modifiable (
+          (fun nc c -> Config.with_depext_bypass
+              (Config.depext_bypass nc ++ Config.depext_bypass c) c),
+          (fun nc c -> Config.with_depext_bypass
+              (Config.depext_bypass c -- Config.depext_bypass nc) c)
+        )),
+      Config.with_depext_bypass (Config.depext_bypass Config.empty);
+      "depext-enable", Fixed,
+      Config.with_depext_enable (Config.depext_enable Config.empty);
+      "depext-no-consistency-checks", Fixed,
+      Config.with_depext_no_consistency_checks
+        (Config.depext_no_consistency_checks Config.empty);
+      "depext-no-root", Fixed,
+      Config.with_depext_no_root (Config.depext_no_root Config.empty);
+      "depext-print-only", Fixed,
+      Config.with_depext_print_only (Config.depext_print_only Config.empty);
     ] @ List.map (fun f ->
         f, Fixed, Config.with_criteria
           (Config.criteria Config.empty))

avsm/unpac

vendor/opam/opam-core/src/client/opamConfigCommand.ml
@@ -583,6 +583,22 @@ let global_allowed_fields, global_allowed_sections =
         Config.with_eval_variables (InitConfig.eval_variables in_config);
         "repository-validation-command", Atomic,
         Config.with_validation_hook_opt (Config.validation_hook Config.empty);
+        "depext-bypass", OpamSysPkg.Set.Op.(Modifiable (
+            (fun nc c -> Config.with_depext_bypass
+                (Config.depext_bypass nc ++ Config.depext_bypass c) c),
+            (fun nc c -> Config.with_depext_bypass
+                (Config.depext_bypass c -- Config.depext_bypass nc) c)
+          )),
+        Config.with_depext_bypass (Config.depext_bypass Config.empty);
+        "depext-enable", Atomic,
+        Config.with_depext_enable (Config.depext_enable Config.empty);
+        "depext-no-consistency-checks", Atomic,
+        Config.with_depext_no_consistency_checks
+          (Config.depext_no_consistency_checks Config.empty);
+        "depext-no-root", Atomic,
+        Config.with_depext_no_root (Config.depext_no_root Config.empty);
+        "depext-print-only", Atomic,
+        Config.with_depext_print_only (Config.depext_print_only Config.empty);
       ] @ List.map (fun f ->
         f, Atomic, Config.with_criteria
           (Config.criteria Config.empty))