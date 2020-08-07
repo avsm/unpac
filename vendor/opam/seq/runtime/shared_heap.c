ctk21/ocaml-multicore

move to a (work_todo, work_computed, gc_clock) state and functions (update, work_todo, commit) to operate on it

runtime/shared_heap.c
@@ -213,7 +213,7 @@ static pool* pool_find(struct caml_heap_state* local, sizeclass sz) {
   /* Otherwise, try to sweep until we find one */
   while (!local->avail_pools[sz] && local->unswept_avail_pools[sz]) {
     caml_domain_state* domain_state = caml_domain_self()->state;
-    domain_state->opportunistic_work +=
+    domain_state->major_work_todo -=
       pool_sweep(local, &local->unswept_avail_pools[sz], sz);
   }
 
@@ -280,7 +280,7 @@ static pool* pool_find(struct caml_heap_state* local, sizeclass sz) {
 
   if( !r && adopted_pool ) {
     caml_domain_state* domain_state = caml_domain_self()->state;
-    domain_state->opportunistic_work +=
+    domain_state->major_work_todo -=
       pool_sweep(local, &local->full_pools[sz], sz);
     r = local->avail_pools[sz];
   }

avsm/unpac

vendor/opam/seq/runtime/shared_heap.c
@@ -298,7 +298,7 @@ static pool* pool_global_adopt(struct caml_heap_state* local, sizeclass sz)
   caml_plat_unlock(&pool_freelist.lock);
 
   if( !r && adopted_pool ) {
-    Caml_state->opportunistic_work +=
+    Caml_state->major_work_todo -=
       pool_sweep(local, &local->full_pools[sz], sz, 0);
     r = local->avail_pools[sz];
   }
@@ -315,7 +315,7 @@ static pool* pool_find(struct caml_heap_state* local, sizeclass sz) {
 
   /* Otherwise, try to sweep until we find one */
   while (!local->avail_pools[sz] && local->unswept_avail_pools[sz]) {
-    Caml_state->opportunistic_work +=
+    Caml_state->major_work_todo -=
       pool_sweep(local, &local->unswept_avail_pools[sz], sz, 0);
   }
 