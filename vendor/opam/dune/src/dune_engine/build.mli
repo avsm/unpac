ocaml/dune

Inject fibers into build
Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

src/dune/build.mli
@@ -192,11 +192,13 @@ val static_deps : _ t -> Static_deps.t
 (** Compute static library dependencies of a build description. *)
 val lib_deps : _ t -> Lib_deps_info.t
 
+val fiber : 'a Fiber.t -> 'a t
+
 (** {1 Execution} *)
 
 (** Execute a build description. Returns the result and the set of dynamic
     dependencies discovered during execution. *)
-val exec : 'a t -> 'a * Dep.Set.t
+val exec : 'a t -> ('a * Dep.Set.t) Fiber.t
 
 (**/**)
 

avsm/unpac

vendor/opam/dune/src/dune_engine/build.mli
@@ -216,7 +216,7 @@ module Make_exec (Build_deps:sig val build_deps: Dep.Set.t -> unit Fiber.t end) 
   val exec : 'a t -> ('a * Dep.Set.t) Fiber.t
 end
 
-val do_not_use_stage_fiber: 'a t Fiber.t -> 'a t
+val do_not_use_stage_fiber: 'a Fiber.t -> 'a t
 val do_not_use_stage_build: 'a t t -> 'a t
 
 