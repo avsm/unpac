gasche/ocaml

TMC: error if several different calls could be optimized
One reason to error in these ambiguous situations, instead of making
a choice for the user, is that the TMC transformation changes the
evaluation order, delaying the call that is made tailrec to after the
evaluation of all other arguments.

Example:

```
type 'a tree =
| Leaf of 'a
| Node of 'a tree * 'a tree

let[@tail_mod_cons] rec correct_map f = function
| Leaf v -> Leaf (f v)
| Node (left, right) ->
  Node (correct_map f left, (correct_map[@tailcall]) f right)

let[@tail_mod_cons] rec incorrect_map f = function
| Leaf v -> Leaf (f v)
| Node (left, right) ->
  Node (incorrect_map f left, incorrect_map f right)
  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
Error: [@tail_mod_cons]: this constructor application may be optimized in several different ways, please disambiguate by adding a [@tailcall] attribute to the call that should be made tail-recursive.
```

testsuite/tests/trmc/ambiguities.ml
@@ -1,0 +1,47 @@
+(* TEST
+   * expect *)
+type 'a tree =
+| Leaf of 'a
+| Node of 'a tree * 'a tree
+[%%expect{|
+type 'a tree = Leaf of 'a | Node of 'a tree * 'a tree
+|}]
+
+module Ambiguous = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node (map f left, map f right)
+end
+[%%expect{|
+Line 5, characters 4-34:
+5 |     Node (map f left, map f right)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: [@tail_mod_cons]: this constructor application may be trmc-transformed
+       in several different ways. Please disambiguate by adding an explicit
+       [@tailcall] attribute to the call that should be made tail-recursive,
+       or a [@tailcall false] attribute on calls that should not be
+       transformed.
+|}]
+
+module Positive_disambiguation = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node (map f left, (map [@tailcall]) f right)
+end
+[%%expect{|
+module Positive_disambiguation :
+  sig val map : ('a -> 'b) -> 'a tree -> 'b tree end
+|}]
+
+module Negative_disambiguation = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node ((map [@tailcall false]) f left, map f right)
+end
+[%%expect{|
+module Negative_disambiguation :
+  sig val map : ('a -> 'b) -> 'a tree -> 'b tree end
+|}]

avsm/unpac

vendor/opam/seq/testsuite/tests/trmc/ambiguities.ml
@@ -1,0 +1,47 @@
+(* TEST
+   * expect *)
+type 'a tree =
+| Leaf of 'a
+| Node of 'a tree * 'a tree
+[%%expect{|
+type 'a tree = Leaf of 'a | Node of 'a tree * 'a tree
+|}]
+
+module Ambiguous = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node (map f left, map f right)
+end
+[%%expect{|
+Line 5, characters 4-34:
+5 |     Node (map f left, map f right)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: [@tail_mod_cons]: this constructor application may be TMC-transformed
+       in several different ways. Please disambiguate by adding an explicit
+       [@tailcall] attribute to the call that should be made tail-recursive,
+       or a [@tailcall false] attribute on calls that should not be
+       transformed.
+|}]
+
+module Positive_disambiguation = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node (map f left, (map [@tailcall]) f right)
+end
+[%%expect{|
+module Positive_disambiguation :
+  sig val map : ('a -> 'b) -> 'a tree -> 'b tree end
+|}]
+
+module Negative_disambiguation = struct
+  let[@tail_mod_cons] rec map f = function
+  | Leaf v -> Leaf (f v)
+  | Node (left, right) ->
+    Node ((map [@tailcall false]) f left, map f right)
+end
+[%%expect{|
+module Negative_disambiguation :
+  sig val map : ('a -> 'b) -> 'a tree -> 'b tree end
+|}]