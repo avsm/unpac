avsm/unpac

commit de67bdcd2e841aff0324f35e5ab9e3d4bb782a66

Change default rendering of `Arg.doc_alts[_enum]`.

vendor/opam/cmdliner/src/cmdliner_base.ml
@@ -59,8 +59,11 @@ let pp_tokens ~spaces ppf s = (* collapse white and hint spaces (maybe) *)
 (* Converter (end-user) error messages *)
 
 let quote s = strf "`%s'" s
-let alts_str ?(quoted = true) alts =
-  let quote = if quoted then quote else (fun s -> s) in
+let alts_str ?quoted alts =
+  let quote = match quoted with
+  | None -> strf "$(b,%s)"
+  | Some quoted -> if quoted then quote else (fun s -> s)
+  in
   match alts with
   | [] -> invalid_arg err_empty_list
   | [a] -> (quote a)

vendor/opam/cmdliner/src/cmdliner.mli
@@ -802,12 +802,17 @@ module Arg : sig
   (** [doc_quote s] quotes the string [s]. *)
 
   val doc_alts : ?quoted:bool -> string list -> string
-  (** [doc_alts alts] documents the alternative tokens [alts] according
-      the number of alternatives. If [quoted] is [true] (default)
-      the tokens are quoted. The resulting string can be used in
-      sentences of the form ["$(docv) must be %s"].
+  (** [doc_alts alts] documents the alternative tokens [alts]
+      according the number of alternatives. If [quoted] is:
+      {ul
+      {- [None], the tokens are enclosed in manpage markup directives
+         to render them in bold (manpage convention).}
+      {- [Some true], the tokens are quoted with {!doc_quote}.}
+      {- [Some false], the tokens are written as is}}
+      The resulting string can be used in sentences of
+      the form ["$(docv) must be %s"].
 
-      @raise Invalid_argument if [alts] is the empty string.  *)
+      @raise Invalid_argument if [alts] is the empty list.  *)
 
   val doc_alts_enum : ?quoted:bool -> (string * 'a) list -> string
   (** [doc_alts_enum quoted alts] is [doc_alts quoted (List.map fst alts)]. *)

commit fb47f4c5df5820af0b1c9b6972606900d5383eea

Fix error rendering regression introduced by de67bdcd2e841.

vendor/opam/cmdliner/src/cmdliner_base.ml
@@ -81,11 +81,15 @@ let err_multi_def ~kind name doc v v' =
     kind name (doc v) (doc v')
 
 let err_ambiguous ~kind s ~ambs =
-    strf "%s %s ambiguous and could be %s" kind (quote s) (alts_str ambs)
+  strf "%s %s ambiguous and could be %s" kind (quote s)
+    (alts_str ~quoted:true ambs)
 
 let err_unknown ?(hints = []) ~kind v =
   let did_you_mean s = strf ", did you mean %s ?" s in
-  let hints = match hints with [] -> "." | hs -> did_you_mean (alts_str hs) in
+  let hints = match hints with
+  | [] -> "."
+  | hs -> did_you_mean (alts_str ~quoted:true hs)
+  in
   strf "unknown %s %s%s" kind (quote v) hints
 
 let err_no kind s = strf "no %s %s" (quote s) kind