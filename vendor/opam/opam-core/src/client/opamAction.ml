rjbou/opam

Download function: rewrite to factorise downloading: one archive downloaded for several directories

src/client/opamAction.ml
@@ -237,33 +237,55 @@ let preprocess_dot_install st nv build_dir =
   in
   files, really_process_dot_install, config
 
-let download_package st nv =
-  log "download_package: %a" (slog OpamPackage.to_string) nv;
+let download_same_source_packages st url nvs =
+  let labelise pkg_str = OpamStd.List.concat_map "+" pkg_str nvs in
+  log "download_package: %a%a"
+    (slog (fun _ -> labelise OpamPackage.to_string)) ()
+    (slog (fun url -> match url, nvs with
+         | None, _ | _, [_] -> ""
+         | Some url, _ -> " " ^ OpamUrl.to_string (OpamFile.URL.url url))) url;
   if OpamStateConfig.(!r.dryrun) || OpamClientConfig.(!r.fake)
-  then Done None
+  then Done None else
+  let nvs =
+    (* we pinned remove package that already has their source *)
+    List.filter (fun nv ->
+        let dir = OpamSwitchState.source_dir st nv in
+        not (OpamPackage.Set.mem nv st.pinned &&
+             OpamFilename.exists_dir dir &&
+             OpamStd.Option.Op.(
+               OpamPinned.find_opam_file_in_source nv.name dir >>|
+               OpamFile.OPAM.safe_read >>=
+               OpamFile.OPAM.version_opt) = Some nv.version))
+      nvs
+  in
+  if nvs = [] then Done None
   else
-  let dir = OpamSwitchState.source_dir st nv in
-  if OpamPackage.Set.mem nv st.pinned &&
-     OpamFilename.exists_dir dir &&
-     OpamStd.Option.Op.(
-       OpamPinned.find_opam_file_in_source nv.name dir >>|
-       OpamFile.OPAM.safe_read >>=
-       OpamFile.OPAM.version_opt)
-     = Some nv.version
-  then Done None
-  else
-  let print_action msg =
-    OpamConsole.msg "%s retrieved %s.%s  (%s)\n"
+  let print_action =
+    OpamConsole.msg "%s retrieved %s  (%s)\n"
       (if not (OpamConsole.utf8 ()) then "->"
        else OpamActionGraph.
               (action_color (`Fetch ()) (action_strings (`Fetch ()))))
-      (OpamConsole.colorise `bold (OpamPackage.name_to_string nv))
-      (OpamPackage.version_to_string nv)
-      msg;
   in
-  OpamUpdate.cleanup_source st
-    (OpamPackage.Map.find_opt nv st.installed_opams)
-    (OpamSwitchState.opam st nv);
+  let print_single_actions msgs =
+    List.iter (fun (nv, msg) ->
+        print_action
+          (OpamConsole.colorise `bold (OpamPackage.name_to_string nv)
+           ^ "." ^ (OpamPackage.version_to_string nv))
+          msg)
+      msgs
+  in
+  let print_full_action msg =
+    print_action
+      (labelise @@ fun nv ->
+       (OpamConsole.colorise `bold (OpamPackage.name_to_string nv))
+       ^ "." ^ (OpamPackage.version_to_string nv))
+      msg
+  in
+  List.iter (fun nv ->
+      OpamUpdate.cleanup_source st
+        (OpamPackage.Map.find_opt nv st.installed_opams)
+        (OpamSwitchState.opam st nv))
+    nvs;
   OpamProcess.Job.catch (fun e ->
       let na =
         match e with
@@ -272,30 +294,48 @@ let download_package st nv =
       in
       Done (Some na))
   @@ fun () ->
-  OpamUpdate.download_package_source st nv dir @@| function
+  OpamUpdate.download_same_packages_source st url nvs @@| function
   | Some (Not_available (s, l)), _ ->
-    let msg = match s with None -> l | Some s -> s in
-    OpamConsole.error "Failed to get sources of %s: %s"
-      (OpamPackage.to_string nv) msg;
+    let msg = OpamStd.Option.default l s in
+    OpamConsole.error "Failed to get sources of %s%s: %s"
+      (labelise OpamPackage.to_string)
+      (match url, nvs with
+       | None, _ | _, [_] -> ""
+       | Some url, _ ->
+         Printf.sprintf " (%s)" (OpamUrl.to_string (OpamFile.URL.url url)))
+      msg;
     Some (s, l)
-  | _, ((name, Not_available (s, l)) :: _) ->
+  | _, ((nv, name, Not_available (s, l)) :: _) ->
     let msg = match s with None -> l | Some s -> s in
     OpamConsole.error "Failed to get extra source \"%s\" of %s: %s"
       name (OpamPackage.to_string nv) msg;
     Some (s, l)
   | Some (Result msg), _ ->
-    print_action msg; None
+    print_full_action msg; None
   | Some (Up_to_date msg), _ ->
-    print_action msg; None
+    print_full_action msg; None
   | None, [] -> None
   | None, (e :: es as extras) ->
-    if List.for_all (function _, Up_to_date _ -> true | _ -> false) extras then
-      print_action "cached"
-    else begin match e, es with
-      | (_, Result msg), [] -> print_action msg
-      | _, _ -> print_action (Printf.sprintf "%d extra sources" (List.length extras))
-    end;
+    if List.for_all (function _, _, Up_to_date _ -> true | _ -> false) extras then
+      print_full_action "cached"
+    else
+      (match e, es with
+       | (_, _, Result msg), [] -> print_full_action msg
+       | _, _ ->
+         print_single_actions
+           (List.map (fun (nv, _, _) ->
+                nv,
+                (Printf.sprintf "%d extra sources"
+                   (List.length
+                      (List.filter (fun (nv',_,_) ->
+                           OpamPackage.compare nv nv' = 0)
+                          extras))))
+               extras));
     None
+
+let download_package st nv =
+  download_same_source_packages st
+    (OpamFile.OPAM.url (OpamSwitchState.opam st nv)) [nv]
 
 (* Prepare the package build:
    * apply the patches

src/repository/opamRepository.ml
@@ -243,20 +243,66 @@ let pull_from_mirrors label ?working_dir ?subpath cache_dir destdir checksums ur
     url, Not_available (Some m, m)
   | ret -> ret
 
-let pull_tree
-    label ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
-    local_dirname checksums remote_urls =
-  let extract_archive f s =
-    OpamFilename.cleandir local_dirname;
-    OpamFilename.extract_job f local_dirname @@+ function
-    | None -> Done (Up_to_date s)
-    | Some (Failure s) ->
-      Done (Not_available (Some s, "Could not extract archive:\n"^s))
-    | Some (OpamSystem.Process_error pe) ->
-      Done (Not_available (Some (OpamProcess.result_summary pe),
-                           OpamProcess.string_of_result pe))
-    | Some e -> Done (Not_available (None, Printexc.to_string e))
+(* handle subpathes *)
+let pulls_tree_t
+    ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
+    dirnames checksums remote_urls =
+  let extract_archive =
+    let fallback success = function
+      | None -> success ()
+      | Some (Failure s) ->
+        Done (Not_available (Some s, "Could not extract archive:\n"^s))
+      | Some (OpamSystem.Process_error pe) ->
+        Done (Not_available (Some (OpamProcess.result_summary pe),
+                             OpamProcess.string_of_result pe))
+      | Some e -> Done (Not_available (None, Printexc.to_string e))
+    in
+    match dirnames with
+    | [ _label, local_dirname ] ->
+      (fun archive msg ->
+         OpamFilename.cleandir local_dirname;
+         OpamFilename.extract_job archive local_dirname
+         @@+ fallback (fun () ->  Done (Up_to_date msg)))
+    | _ ->
+      fun archive msg ->
+        OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
+        let copies () =
+          OpamParallel.map ~jobs:3
+            ~command:(fun (label, local_dirname) ->
+                let text = OpamProcess.make_command_text label label in
+                OpamProcess.Job.with_text text @@
+                (try
+                   OpamFilename.cleandir local_dirname;
+                   OpamFilename.copy_dir ~src:tmpdir ~dst:local_dirname;
+                   Done (Up_to_date label)
+                 with OpamSystem.Process_error r ->
+                   Done (Not_available
+                           (Some label, OpamProcess.result_summary r))))
+            dirnames
+        in
+        OpamFilename.extract_job archive tmpdir
+        @@+ fallback (fun () ->
+            let failing =
+              OpamStd.List.filter_map (function
+                  | Result _ | Up_to_date _ -> None
+                  | Not_available (Some s,l) -> Some (s,l)
+                  | Not_available (None, _) -> assert false
+                ) (copies ())
+            in
+            if failing = [] then Done (Up_to_date msg) else
+            let simple =
+              Printf.sprintf "Failed to copy source of %s"
+                (OpamStd.Format.pretty_list (List.map fst failing))
+            in
+            let long =
+              Printf.sprintf "Failed to copy source of:\n%s"
+                (OpamStd.Format.itemize (fun (nv, msg) ->
+                     Printf.sprintf "%s: %s" nv msg)
+                    failing)
+            in
+            Done (Not_available (Some simple, long)))
   in
+  let label = OpamStd.List.concat_map "+" fst dirnames in
   (match cache_dir with
    | Some cache_dir ->
      let text = OpamProcess.make_command_text label "dl" in
@@ -283,17 +329,38 @@ let pull_tree
           Some ("missing checksum"),
           label ^ ": Missing checksum, and `--require-checksums` was set."))
     else
-      pull_from_mirrors label ?working_dir ?subpath cache_dir local_dirname checksums
-        remote_urls
+      OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
+      let extract url archive =
+        match dirnames with
+        | [_] ->
+          let tmp_archive = OpamFilename.(create tmpdir (basename archive)) in
+          OpamFilename.move ~src:archive ~dst:tmp_archive;
+          extract_archive tmp_archive (OpamUrl.to_string url)
+        | _ -> extract_archive archive (OpamUrl.to_string url)
+      in
+      let pull label =
+        match dirnames with
+        | [ label, local_dirname ] ->
+          pull_from_mirrors label ?working_dir ?subpath cache_dir local_dirname
+        | _ ->
+          pull_from_mirrors label cache_dir tmpdir
+      in
+      pull label checksums remote_urls
       @@+ function
       | _, Up_to_date None -> Done (Up_to_date "no changes")
       | url, (Up_to_date (Some archive) | Result (Some archive)) ->
-        OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
-        let tmp_archive = OpamFilename.(create tmpdir (basename archive)) in
-        OpamFilename.move ~src:archive ~dst:tmp_archive;
-        extract_archive tmp_archive (OpamUrl.to_string url)
+        extract url archive
       | url, Result None -> Done (Result (OpamUrl.to_string url))
       | _, (Not_available _ as na) -> Done na
+
+
+let pull_tree label ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
+    local_dirname  =
+  pulls_tree_t ?cache_dir ~cache_urls ?working_dir ?subpath
+  [label, local_dirname]
+
+let pulls_tree ?cache_dir ?(cache_urls=[]) dirnames checksums remote_urls =
+  pulls_tree_t ?cache_dir ~cache_urls dirnames checksums remote_urls
 
 let revision dirname url =
   let kind = url.OpamUrl.backend in

avsm/unpac

vendor/opam/opam-core/src/client/opamAction.ml
@@ -237,33 +237,55 @@ let preprocess_dot_install st nv build_dir =
   in
   files, really_process_dot_install, config
 
-let download_package st nv =
-  log "download_package: %a" (slog OpamPackage.to_string) nv;
+let download_shared_source st url nvs =
+  let labelise pkg_str = OpamStd.List.concat_map ", " pkg_str nvs in
+  log "download_package: %a%a"
+    (slog (fun _ -> labelise OpamPackage.to_string)) ()
+    (slog (fun url -> match url, nvs with
+         | None, _ | _, [_] -> ""
+         | Some url, _ -> " " ^ OpamUrl.to_string (OpamFile.URL.url url))) url;
   if OpamStateConfig.(!r.dryrun) || OpamClientConfig.(!r.fake)
-  then Done None
+  then Done None else
+  let nvs =
+    (* filter out version-pinned packages since we already have their source *)
+    List.filter (fun nv ->
+        let dir = OpamSwitchState.source_dir st nv in
+        not (OpamPackage.Set.mem nv st.pinned &&
+             OpamFilename.exists_dir dir &&
+             OpamStd.Option.Op.(
+               OpamPinned.find_opam_file_in_source nv.name dir >>|
+               OpamFile.OPAM.safe_read >>=
+               OpamFile.OPAM.version_opt) = Some nv.version))
+      nvs
+  in
+  if nvs = [] then Done None
   else
-  let dir = OpamSwitchState.source_dir st nv in
-  if OpamPackage.Set.mem nv st.pinned &&
-     OpamFilename.exists_dir dir &&
-     OpamStd.Option.Op.(
-       OpamPinned.find_opam_file_in_source nv.name dir >>|
-       OpamFile.OPAM.safe_read >>=
-       OpamFile.OPAM.version_opt)
-     = Some nv.version
-  then Done None
-  else
-  let print_action msg =
-    OpamConsole.msg "%s retrieved %s.%s  (%s)\n"
+  let print_action =
+    OpamConsole.msg "%s retrieved %s  (%s)\n"
       (if not (OpamConsole.utf8 ()) then "->"
        else OpamActionGraph.
               (action_color (`Fetch []) (action_strings (`Fetch []))))
-      (OpamConsole.colorise `bold (OpamPackage.name_to_string nv))
-      (OpamPackage.version_to_string nv)
-      msg;
   in
-  OpamUpdate.cleanup_source st
-    (OpamPackage.Map.find_opt nv st.installed_opams)
-    (OpamSwitchState.opam st nv);
+  let print_single_actions msgs =
+    List.iter (fun (nv, msg) ->
+        print_action
+          (OpamConsole.colorise `bold (OpamPackage.name_to_string nv)
+           ^ "." ^ (OpamPackage.version_to_string nv))
+          msg)
+      msgs
+  in
+  let print_full_action msg =
+    print_action
+      (labelise @@ fun nv ->
+       (OpamConsole.colorise `bold (OpamPackage.name_to_string nv))
+       ^ "." ^ (OpamPackage.version_to_string nv))
+      msg
+  in
+  List.iter (fun nv ->
+      OpamUpdate.cleanup_source st
+        (OpamPackage.Map.find_opt nv st.installed_opams)
+        (OpamSwitchState.opam st nv))
+    nvs;
   OpamProcess.Job.catch (fun e ->
       let na =
         match e with
@@ -272,30 +294,48 @@ let download_package st nv =
       in
       Done (Some na))
   @@ fun () ->
-  OpamUpdate.download_package_source st nv dir @@| function
+  OpamUpdate.download_shared_package_source st url nvs @@| function
   | Some (Not_available (s, l)), _ ->
-    let msg = match s with None -> l | Some s -> s in
-    OpamConsole.error "Failed to get sources of %s: %s"
-      (OpamPackage.to_string nv) msg;
+    let msg = OpamStd.Option.default l s in
+    OpamConsole.error "Failed to get sources of %s%s: %s"
+      (labelise OpamPackage.to_string)
+      (match url, nvs with
+       | None, _ | _, [_] -> ""
+       | Some url, _ ->
+         Printf.sprintf " (%s)" (OpamUrl.to_string (OpamFile.URL.url url)))
+      msg;
     Some (s, l)
-  | _, ((name, Not_available (s, l)) :: _) ->
+  | _, ((nv, name, Not_available (s, l)) :: _) ->
     let msg = match s with None -> l | Some s -> s in
     OpamConsole.error "Failed to get extra source \"%s\" of %s: %s"
       name (OpamPackage.to_string nv) msg;
     Some (s, l)
   | Some (Result msg), _ ->
-    print_action msg; None
+    print_full_action msg; None
   | Some (Up_to_date msg), _ ->
-    print_action msg; None
+    print_full_action msg; None
   | None, [] -> None
   | None, (e :: es as extras) ->
-    if List.for_all (function _, Up_to_date _ -> true | _ -> false) extras then
-      print_action "cached"
-    else begin match e, es with
-      | (_, Result msg), [] -> print_action msg
-      | _, _ -> print_action (Printf.sprintf "%d extra sources" (List.length extras))
-    end;
+    if List.for_all (function _, _, Up_to_date _ -> true | _ -> false) extras then
+      print_full_action "cached"
+    else
+      (match e, es with
+       | (_, _, Result msg), [] -> print_full_action msg
+       | _, _ ->
+         print_single_actions
+           (List.map (fun (nv, _, _) ->
+                nv,
+                (Printf.sprintf "%d extra sources"
+                   (List.length
+                      (List.filter (fun (nv',_,_) ->
+                           OpamPackage.compare nv nv' = 0)
+                          extras))))
+               extras));
     None
+
+let download_package st nv =
+  download_shared_source st
+    (OpamFile.OPAM.url (OpamSwitchState.opam st nv)) [nv]
 
 (* Prepare the package build:
    * apply the patches

vendor/opam/opam-core/src/repository/opamRepository.ml
@@ -245,20 +245,66 @@ let pull_from_mirrors label ?working_dir ?subpath cache_dir destdir checksums ur
     url, Not_available (Some m, m)
   | ret -> ret
 
-let pull_tree
-    label ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
-    local_dirname checksums remote_urls =
-  let extract_archive f s =
-    OpamFilename.cleandir local_dirname;
-    OpamFilename.extract_job f local_dirname @@+ function
-    | None -> Done (Up_to_date s)
-    | Some (Failure s) ->
-      Done (Not_available (Some s, "Could not extract archive:\n"^s))
-    | Some (OpamSystem.Process_error pe) ->
-      Done (Not_available (Some (OpamProcess.result_summary pe),
-                           OpamProcess.string_of_result pe))
-    | Some e -> Done (Not_available (None, Printexc.to_string e))
+(* handle subpathes *)
+let pull_tree_t
+    ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
+    dirnames checksums remote_urls =
+  let extract_archive =
+    let fallback success = function
+      | None -> success ()
+      | Some (Failure s) ->
+        Done (Not_available (Some s, "Could not extract archive:\n"^s))
+      | Some (OpamSystem.Process_error pe) ->
+        Done (Not_available (Some (OpamProcess.result_summary pe),
+                             OpamProcess.string_of_result pe))
+      | Some e -> Done (Not_available (None, Printexc.to_string e))
+    in
+    match dirnames with
+    | [ _label, local_dirname ] ->
+      (fun archive msg ->
+         OpamFilename.cleandir local_dirname;
+         OpamFilename.extract_job archive local_dirname
+         @@+ fallback (fun () ->  Done (Up_to_date msg)))
+    | _ ->
+      fun archive msg ->
+        OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
+        let copies () =
+          OpamParallel.map ~jobs:3
+            ~command:(fun (label, local_dirname) ->
+                let text = OpamProcess.make_command_text label label in
+                OpamProcess.Job.with_text text @@
+                (try
+                   OpamFilename.cleandir local_dirname;
+                   OpamFilename.copy_dir ~src:tmpdir ~dst:local_dirname;
+                   Done (Up_to_date label)
+                 with OpamSystem.Process_error r ->
+                   Done (Not_available
+                           (Some label, OpamProcess.result_summary r))))
+            dirnames
+        in
+        OpamFilename.extract_job archive tmpdir
+        @@+ fallback (fun () ->
+            let failing =
+              OpamStd.List.filter_map (function
+                  | Result _ | Up_to_date _ -> None
+                  | Not_available (Some s,l) -> Some (s,l)
+                  | Not_available (None, _) -> assert false
+                ) (copies ())
+            in
+            if failing = [] then Done (Up_to_date msg) else
+            let simple =
+              Printf.sprintf "Failed to copy source of %s"
+                (OpamStd.Format.pretty_list (List.map fst failing))
+            in
+            let long =
+              Printf.sprintf "Failed to copy source of:\n%s"
+                (OpamStd.Format.itemize (fun (nv, msg) ->
+                     Printf.sprintf "%s: %s" nv msg)
+                    failing)
+            in
+            Done (Not_available (Some simple, long)))
   in
+  let label = OpamStd.List.concat_map ", " fst dirnames in
   (match cache_dir with
    | Some cache_dir ->
      let text = OpamProcess.make_command_text label "dl" in
@@ -285,17 +331,38 @@ let pull_tree
           Some ("missing checksum"),
           label ^ ": Missing checksum, and `--require-checksums` was set."))
     else
-      pull_from_mirrors label ?working_dir ?subpath cache_dir local_dirname checksums
-        remote_urls
+      OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
+      let extract url archive =
+        match dirnames with
+        | [_] ->
+          let tmp_archive = OpamFilename.(create tmpdir (basename archive)) in
+          OpamFilename.move ~src:archive ~dst:tmp_archive;
+          extract_archive tmp_archive (OpamUrl.to_string url)
+        | _ -> extract_archive archive (OpamUrl.to_string url)
+      in
+      let pull label =
+        match dirnames with
+        | [ label, local_dirname ] ->
+          pull_from_mirrors label ?working_dir ?subpath cache_dir local_dirname
+        | _ ->
+          pull_from_mirrors label cache_dir tmpdir
+      in
+      pull label checksums remote_urls
       @@+ function
       | _, Up_to_date None -> Done (Up_to_date "no changes")
       | url, (Up_to_date (Some archive) | Result (Some archive)) ->
-        OpamFilename.with_tmp_dir_job @@ fun tmpdir ->
-        let tmp_archive = OpamFilename.(create tmpdir (basename archive)) in
-        OpamFilename.move ~src:archive ~dst:tmp_archive;
-        extract_archive tmp_archive (OpamUrl.to_string url)
+        extract url archive
       | url, Result None -> Done (Result (OpamUrl.to_string url))
       | _, (Not_available _ as na) -> Done na
+
+
+let pull_tree label ?cache_dir ?(cache_urls=[]) ?working_dir ?subpath
+    local_dirname  =
+  pull_tree_t ?cache_dir ~cache_urls ?working_dir ?subpath
+  [label, local_dirname]
+
+let pull_shared_tree ?cache_dir ?(cache_urls=[]) dirnames checksums remote_urls =
+  pull_tree_t ?cache_dir ~cache_urls dirnames checksums remote_urls
 
 let revision dirname url =
   let kind = url.OpamUrl.backend in