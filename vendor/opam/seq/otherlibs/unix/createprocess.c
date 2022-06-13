dra27/ocaml

Rename the two Windows unicode C functions

otherlibs/unix/createprocess.c
@@ -120,11 +120,11 @@ value win_create_process_native(value cmd, value cmdline, value env,
   if (Is_some(env)) {
     env = Some_val(env);
     size =
-      win_multi_byte_to_wide_char(String_val(env),
-                                  caml_string_length(env), NULL, 0);
+      caml_multi_byte_to_wide_char(String_val(env),
+                                   caml_string_length(env), NULL, 0);
     wenv = caml_stat_alloc((size + 1)*sizeof(wchar_t));
-    win_multi_byte_to_wide_char(String_val(env),
-                                caml_string_length(env), wenv, size);
+    caml_multi_byte_to_wide_char(String_val(env),
+                                 caml_string_length(env), wenv, size);
     wenv[size] = 0;
   } else {
     wenv = NULL;

runtime/caml/osdeps.h
@@ -121,14 +121,18 @@ extern wchar_t *caml_win32_getenv(wchar_t const *);
 
 /* Windows Unicode support */
 
-CAMLextern int win_multi_byte_to_wide_char(const char* s,
-                                       int slen,
-                                       wchar_t *out,
-                                       int outlen);
-CAMLextern int win_wide_char_to_multi_byte(const wchar_t* s,
-                                       int slen,
-                                       char *out,
-                                       int outlen);
+CAMLextern int caml_multi_byte_to_wide_char(const char* s,
+                                            int slen,
+                                            wchar_t *out,
+                                            int outlen);
+CAMLextern int caml_wide_char_to_multi_byte(const wchar_t* s,
+                                            int slen,
+                                            char *out,
+                                            int outlen);
+
+/* Legacy names */
+#define win_multi_byte_to_wide_char caml_multi_byte_to_wide_char
+#define win_wide_char_to_multi_byte caml_wide_char_to_multi_byte
 
 CAMLextern int caml_win32_isatty(int fd);
 

runtime/startup_byt.c
@@ -235,9 +235,9 @@ static char_os * read_section_to_os(int fd, struct exec_trailer *trail,
   if (read(fd, data, len) != len)
     caml_fatal_error("error reading section %s", name);
   data[len] = 0;
-  wlen = win_multi_byte_to_wide_char(data, len, NULL, 0);
+  wlen = caml_multi_byte_to_wide_char(data, len, NULL, 0);
   wdata = caml_stat_alloc((wlen + 1)*sizeof(wchar_t));
-  win_multi_byte_to_wide_char(data, len, wdata, wlen);
+  caml_multi_byte_to_wide_char(data, len, wdata, wlen);
   wdata[wlen] = 0;
   caml_stat_free(data);
   return wdata;

runtime/win32.c
@@ -864,8 +864,8 @@ static uintnat windows_unicode_strict = 1;
    the argument string is encoded in the local codepage. */
 static uintnat windows_unicode_fallback = 1;
 
-CAMLexport int win_multi_byte_to_wide_char(const char *s, int slen,
-                                           wchar_t *out, int outlen)
+CAMLexport int caml_multi_byte_to_wide_char(const char *s, int slen,
+                                            wchar_t *out, int outlen)
 {
   int retcode;
 
@@ -896,8 +896,8 @@ CAMLexport int win_multi_byte_to_wide_char(const char *s, int slen,
 #define WC_ERR_INVALID_CHARS 0
 #endif
 
-CAMLexport int win_wide_char_to_multi_byte(const wchar_t *s, int slen,
-                                           char *out, int outlen)
+CAMLexport int caml_wide_char_to_multi_byte(const wchar_t *s, int slen,
+                                            char *out, int outlen)
 {
   int retcode;
 
@@ -928,9 +928,9 @@ CAMLexport value caml_copy_string_of_utf16(const wchar_t *s)
 
   slen = wcslen(s);
   /* Do not include final NULL */
-  retcode = win_wide_char_to_multi_byte(s, slen, NULL, 0);
+  retcode = caml_wide_char_to_multi_byte(s, slen, NULL, 0);
   v = caml_alloc_string(retcode);
-  win_wide_char_to_multi_byte(s, slen, (char *)String_val(v), retcode);
+  caml_wide_char_to_multi_byte(s, slen, (char *)String_val(v), retcode);
 
   return v;
 }
@@ -940,9 +940,9 @@ CAMLexport wchar_t* caml_stat_strdup_to_utf16(const char *s)
   wchar_t * ws;
   int retcode;
 
-  retcode = win_multi_byte_to_wide_char(s, -1, NULL, 0);
+  retcode = caml_multi_byte_to_wide_char(s, -1, NULL, 0);
   ws = caml_stat_alloc_noexc(retcode * sizeof(*ws));
-  win_multi_byte_to_wide_char(s, -1, ws, retcode);
+  caml_multi_byte_to_wide_char(s, -1, ws, retcode);
 
   return ws;
 }
@@ -952,9 +952,9 @@ CAMLexport caml_stat_string caml_stat_strdup_of_utf16(const wchar_t *s)
   caml_stat_string out;
   int retcode;
 
-  retcode = win_wide_char_to_multi_byte(s, -1, NULL, 0);
+  retcode = caml_wide_char_to_multi_byte(s, -1, NULL, 0);
   out = caml_stat_alloc(retcode);
-  win_wide_char_to_multi_byte(s, -1, out, retcode);
+  caml_wide_char_to_multi_byte(s, -1, out, retcode);
 
   return out;
 }

yacc/wstr.c
@@ -22,8 +22,8 @@ static int windows_unicode_enabled = WINDOWS_UNICODE;
 static int windows_unicode_strict = 1;
 
 /* Adapted from runtime/win32.c */
-int win_wide_char_to_multi_byte(const wchar_t *s, int slen,
-                                char *out, int outlen)
+int caml_wide_char_to_multi_byte(const wchar_t *s, int slen,
+                                 char *out, int outlen)
 {
   int retcode;
 
@@ -50,10 +50,10 @@ char* caml_stat_strdup_of_utf16(const wchar_t *s)
   char *out = NULL;
   int retcode;
 
-  retcode = win_wide_char_to_multi_byte(s, -1, NULL, 0);
+  retcode = caml_wide_char_to_multi_byte(s, -1, NULL, 0);
   if (retcode >= 0) {
     out = (char *)malloc(retcode);
-    win_wide_char_to_multi_byte(s, -1, out, retcode);
+    caml_wide_char_to_multi_byte(s, -1, out, retcode);
   }
 
   return out;

avsm/unpac

vendor/opam/seq/otherlibs/unix/createprocess.c
@@ -120,11 +120,11 @@ value win_create_process_native(value cmd, value cmdline, value env,
   if (Is_some(env)) {
     env = Some_val(env);
     size =
-      win_multi_byte_to_wide_char(String_val(env),
-                                  caml_string_length(env), NULL, 0);
+      caml_win32_multi_byte_to_wide_char(String_val(env),
+                                         caml_string_length(env), NULL, 0);
     wenv = caml_stat_alloc((size + 1)*sizeof(wchar_t));
-    win_multi_byte_to_wide_char(String_val(env),
-                                caml_string_length(env), wenv, size);
+    caml_win32_multi_byte_to_wide_char(String_val(env),
+                                       caml_string_length(env), wenv, size);
     wenv[size] = 0;
   } else {
     wenv = NULL;

vendor/opam/seq/runtime/caml/osdeps.h
@@ -121,14 +121,18 @@ extern wchar_t *caml_win32_getenv(wchar_t const *);
 
 /* Windows Unicode support */
 
-CAMLextern int win_multi_byte_to_wide_char(const char* s,
-                                       int slen,
-                                       wchar_t *out,
-                                       int outlen);
-CAMLextern int win_wide_char_to_multi_byte(const wchar_t* s,
-                                       int slen,
-                                       char *out,
-                                       int outlen);
+CAMLextern int caml_win32_multi_byte_to_wide_char(const char* s,
+                                                  int slen,
+                                                  wchar_t *out,
+                                                  int outlen);
+CAMLextern int caml_win32_wide_char_to_multi_byte(const wchar_t* s,
+                                                  int slen,
+                                                  char *out,
+                                                  int outlen);
+
+/* Legacy names */
+#define win_multi_byte_to_wide_char caml_win32_multi_byte_to_wide_char
+#define win_wide_char_to_multi_byte caml_win32_wide_char_to_multi_byte
 
 CAMLextern int caml_win32_isatty(int fd);
 

vendor/opam/seq/runtime/startup_byt.c
@@ -235,9 +235,9 @@ static char_os * read_section_to_os(int fd, struct exec_trailer *trail,
   if (read(fd, data, len) != len)
     caml_fatal_error("error reading section %s", name);
   data[len] = 0;
-  wlen = win_multi_byte_to_wide_char(data, len, NULL, 0);
+  wlen = caml_win32_multi_byte_to_wide_char(data, len, NULL, 0);
   wdata = caml_stat_alloc((wlen + 1)*sizeof(wchar_t));
-  win_multi_byte_to_wide_char(data, len, wdata, wlen);
+  caml_win32_multi_byte_to_wide_char(data, len, wdata, wlen);
   wdata[wlen] = 0;
   caml_stat_free(data);
   return wdata;

vendor/opam/seq/runtime/win32.c
@@ -864,8 +864,8 @@ static uintnat windows_unicode_strict = 1;
    the argument string is encoded in the local codepage. */
 static uintnat windows_unicode_fallback = 1;
 
-CAMLexport int win_multi_byte_to_wide_char(const char *s, int slen,
-                                           wchar_t *out, int outlen)
+CAMLexport int caml_win32_multi_byte_to_wide_char(const char *s, int slen,
+                                                  wchar_t *out, int outlen)
 {
   int retcode;
 
@@ -896,8 +896,8 @@ CAMLexport int win_multi_byte_to_wide_char(const char *s, int slen,
 #define WC_ERR_INVALID_CHARS 0
 #endif
 
-CAMLexport int win_wide_char_to_multi_byte(const wchar_t *s, int slen,
-                                           char *out, int outlen)
+CAMLexport int caml_win32_wide_char_to_multi_byte(const wchar_t *s, int slen,
+                                                  char *out, int outlen)
 {
   int retcode;
 
@@ -928,9 +928,9 @@ CAMLexport value caml_copy_string_of_utf16(const wchar_t *s)
 
   slen = wcslen(s);
   /* Do not include final NULL */
-  retcode = win_wide_char_to_multi_byte(s, slen, NULL, 0);
+  retcode = caml_win32_wide_char_to_multi_byte(s, slen, NULL, 0);
   v = caml_alloc_string(retcode);
-  win_wide_char_to_multi_byte(s, slen, (char *)String_val(v), retcode);
+  caml_win32_wide_char_to_multi_byte(s, slen, (char *)String_val(v), retcode);
 
   return v;
 }
@@ -940,9 +940,9 @@ CAMLexport wchar_t* caml_stat_strdup_to_utf16(const char *s)
   wchar_t * ws;
   int retcode;
 
-  retcode = win_multi_byte_to_wide_char(s, -1, NULL, 0);
+  retcode = caml_win32_multi_byte_to_wide_char(s, -1, NULL, 0);
   ws = caml_stat_alloc_noexc(retcode * sizeof(*ws));
-  win_multi_byte_to_wide_char(s, -1, ws, retcode);
+  caml_win32_multi_byte_to_wide_char(s, -1, ws, retcode);
 
   return ws;
 }
@@ -952,9 +952,9 @@ CAMLexport caml_stat_string caml_stat_strdup_of_utf16(const wchar_t *s)
   caml_stat_string out;
   int retcode;
 
-  retcode = win_wide_char_to_multi_byte(s, -1, NULL, 0);
+  retcode = caml_win32_wide_char_to_multi_byte(s, -1, NULL, 0);
   out = caml_stat_alloc(retcode);
-  win_wide_char_to_multi_byte(s, -1, out, retcode);
+  caml_win32_wide_char_to_multi_byte(s, -1, out, retcode);
 
   return out;
 }

vendor/opam/seq/yacc/wstr.c
@@ -22,8 +22,8 @@ static int windows_unicode_enabled = WINDOWS_UNICODE;
 static int windows_unicode_strict = 1;
 
 /* Adapted from runtime/win32.c */
-int win_wide_char_to_multi_byte(const wchar_t *s, int slen,
-                                char *out, int outlen)
+int caml_win32_wide_char_to_multi_byte(const wchar_t *s, int slen,
+                                       char *out, int outlen)
 {
   int retcode;
 
@@ -50,10 +50,10 @@ char* caml_stat_strdup_of_utf16(const wchar_t *s)
   char *out = NULL;
   int retcode;
 
-  retcode = win_wide_char_to_multi_byte(s, -1, NULL, 0);
+  retcode = caml_win32_wide_char_to_multi_byte(s, -1, NULL, 0);
   if (retcode >= 0) {
     out = (char *)malloc(retcode);
-    win_wide_char_to_multi_byte(s, -1, out, retcode);
+    caml_win32_wide_char_to_multi_byte(s, -1, out, retcode);
   }
 
   return out;