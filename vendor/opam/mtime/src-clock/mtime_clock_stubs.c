avsm/unpac

commit 9b16da554e162f55b0e530a5c86aee116f7ccf50

Use CLOCK_BOOTTIME on linux and mach_continuous_time on macOS.
Closes #10.

vendor/opam/mtime/src-clock/mtime_clock_stubs.c
@@ -22,6 +22,9 @@
 
 #elif defined(__unix__) || defined(__unix)
  #include <unistd.h>
+ #if defined(__linux__)
+   #define OCAML_MTIME_LINUX
+ #endif
  #if defined(_POSIX_VERSION)
    #define OCAML_MTIME_POSIX
  #endif
@@ -51,14 +54,14 @@ CAMLprim value ocaml_mtime_clock_elapsed_ns (value unit)
   static uint64_t start = 0L;
   if (start == 0L) { start = mach_absolute_time (); }
   if (scale.denom == 0) { ocaml_mtime_clock_init_scale (); }
-  uint64_t now = mach_absolute_time ();
+  uint64_t now = mach_continuous_time ();
   return caml_copy_int64 (((now - start) * scale.numer) / scale.denom);
 }
 
 CAMLprim value ocaml_mtime_clock_now_ns (value unit)
 {
   if (scale.denom == 0) { ocaml_mtime_clock_init_scale (); }
-  uint64_t now = mach_absolute_time ();
+  uint64_t now = mach_continuous_time ();
   return caml_copy_int64 ((now * scale.numer) / scale.denom);
 }
 
@@ -75,14 +78,21 @@ CAMLprim value ocaml_mtime_clock_elapsed_ns (value unit)
 {
   static struct timespec start = {0};
   struct timespec now;
+  clockid_t clockid;
+
+#if defined(OCAML_MTIME_LINUX)
+  clockid = CLOCK_BOOTTIME;
+#else
+  clockid = CLOCK_MONOTONIC;
+#endif
 
   if (start.tv_sec == 0)
   {
-    if (clock_gettime (CLOCK_MONOTONIC, &start))
+    if (clock_gettime (clockid, &start))
       OCAML_MTIME_RAISE_SYS_ERROR ("clock_gettime () failed");
   }
 
-  if (clock_gettime (CLOCK_MONOTONIC, &now))
+  if (clock_gettime (clockid, &now))
     OCAML_MTIME_RAISE_SYS_ERROR ("clock_gettime () failed");
 
   return caml_copy_int64 ((uint64_t)(now.tv_sec - start.tv_sec) *

vendor/opam/mtime/src/mtime_clock.mli
@@ -83,11 +83,15 @@ val period_ns : unit -> int64 option
     {1:platform_support Platform support}
 
     {ul
-    {- Platforms with a POSIX clock (includes Linux) use
+    {- Linux uses {{:http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_gettime.html}[clock_gettime]}
+       with {{:https://www.man7.org/linux/man-pages/man3/clock_settime.3.html}
+       CLOCK_BOOTTIME}. This means that sleep time is taken into account.}
+    {- Platforms with a POSIX clock use
        {{:http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_gettime.html}[clock_gettime]}
        with CLOCK_MONOTONIC.}
     {- Darwin uses
-       {{:https://developer.apple.com/library/mac/qa/qa1398/_index.html}[mach_absolute_time]}.}
+       {{:https://developer.apple.com/documentation/kernel/1646199-mach_continuous_time}[mach_continous_time]}.
+       This means that sleep time is taken into account.}
     {- Windows uses
        {{:https://msdn.microsoft.com/en-us/library/windows/desktop/aa373083%28v=vs.85%29.aspx}Performance counters}. }
     {- JavaScript uses

commit b301584bad4b49a866e57869951fce27d7eaf5f5

Fix MacOS regression introduced by 9b16da5 (#44).

vendor/opam/mtime/src-clock/mtime_clock_stubs.c
@@ -52,7 +52,7 @@ void ocaml_mtime_clock_init_scale (void)
 CAMLprim value ocaml_mtime_clock_elapsed_ns (value unit)
 {
   static uint64_t start = 0L;
-  if (start == 0L) { start = mach_absolute_time (); }
+  if (start == 0L) { start = mach_continuous_time (); }
   if (scale.denom == 0) { ocaml_mtime_clock_init_scale (); }
   uint64_t now = mach_continuous_time ();
   return caml_copy_int64 (((now - start) * scale.numer) / scale.denom);