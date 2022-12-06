dra27/ocaml

Introduce caml_sys_mmap_alignment
On Unix platforms, equal caml_sys_pagesize but on Windows reflects the
fact that VirtualAlloc has a stricter alignment guarantee.

runtime/domain.c
@@ -724,8 +724,8 @@ static void reserve_minor_heaps() {
   minor_heap_reservation_bsize = minor_heap_max_bsz * Max_domains;
 
   /* reserve memory space for minor heaps */
-  heaps_base = caml_mem_map(minor_heap_reservation_bsize,
-                            caml_plat_mmap_granularity, 1 /* reserve_only */);
+  heaps_base = caml_mem_map(minor_heap_reservation_bsize, caml_sys_pagesize,
+                1 /* reserve_only */);
   if (heaps_base == NULL)
     caml_fatal_error("Not enough heap memory to reserve minor heaps");
 

runtime/platform.c
@@ -138,14 +138,15 @@ static uintnat round_up(uintnat size, uintnat align) {
   return (size + align - 1) & ~(align - 1);
 }
 
-intnat caml_plat_mmap_granularity = 0;
+intnat caml_sys_pagesize = 0;
+intnat caml_sys_mmap_alignment = 0;
 
 uintnat caml_mem_round_up_pages(uintnat size)
 {
-  return round_up(size, caml_plat_mmap_granularity);
+  return round_up(size, caml_sys_pagesize);
 }
 
-#define Is_page_aligned(size) ((size & (caml_plat_mmap_granularity - 1)) == 0)
+#define Is_page_aligned(size) ((size & (caml_sys_pagesize - 1)) == 0)
 
 #ifdef DEBUG
 static struct lf_skiplist mmap_blocks = {NULL};
@@ -155,7 +156,7 @@ void* caml_mem_map(uintnat size, uintnat alignment, int reserve_only)
 {
   CAMLassert(Is_power_of_2(alignment));
   CAMLassert(Is_page_aligned(size));
-  alignment = caml_mem_round_up_pages(alignment);
+  alignment = round_up(alignment, caml_sys_mmap_alignment);
 
 #ifdef DEBUG
   if (mmap_blocks.head == NULL) {

runtime/win32.c
@@ -1091,15 +1091,11 @@ void caml_init_os_params(void)
   SYSTEM_INFO si;
   LARGE_INTEGER frequency;
 
-  /* Get the system page size */
+  /* Get the system page size and allocation granularity. */
   GetSystemInfo(&si);
-  /* Use the Allocation Granularity rather than the Page Size. Page Size only
-     applies when committing a previous reservation, which for OCaml only occurs
-     when committing a minor heap, which will necessarily be larger than both
-     page size and granularity. This simplifies caml_mem_map, since we can
-     guarantee that trimming will not be required. */
   CAMLassert(si.dwAllocationGranularity >= si.dwPageSize);
-  caml_plat_mmap_granularity = si.dwAllocationGranularity;
+  caml_sys_pagesize = si.dwPageSize;
+  caml_sys_mmap_alignment = si.dwAllocationGranularity;
 
   /* Get the number of nanoseconds for each tick in QueryPerformanceCounter */
   QueryPerformanceFrequency(&frequency);
@@ -1116,9 +1112,9 @@ int64_t caml_time_counter(void)
 
 void *caml_plat_mem_map(uintnat size, uintnat alignment, int reserve_only)
 {
-  /* VirtualAlloc returns an address aligned to caml_plat_mmap_granularity, so
+  /* VirtualAlloc returns an address aligned to caml_sys_mmap_alignment, so
      trimming will not be required. VirtualAlloc returns 0 on error. */
-  if (alignment > caml_plat_mmap_granularity)
+  if (alignment > caml_sys_mmap_alignment)
     caml_fatal_error("Cannot align memory to %" ARCH_INTNAT_PRINTF_FORMAT "x"
                      " on this platform", alignment);
   return

avsm/unpac

vendor/opam/seq/runtime/domain.c
@@ -724,8 +724,8 @@ static void reserve_minor_heaps(void) {
   minor_heap_reservation_bsize = minor_heap_max_bsz * Max_domains;
 
   /* reserve memory space for minor heaps */
-  heaps_base = caml_mem_map(minor_heap_reservation_bsize,
-                            caml_plat_mmap_granularity, 1 /* reserve_only */);
+  heaps_base = caml_mem_map(minor_heap_reservation_bsize, caml_plat_pagesize,
+                1 /* reserve_only */);
   if (heaps_base == NULL)
     caml_fatal_error("Not enough heap memory to reserve minor heaps");
 

vendor/opam/seq/runtime/platform.c
@@ -138,14 +138,15 @@ static uintnat round_up(uintnat size, uintnat align) {
   return (size + align - 1) & ~(align - 1);
 }
 
-intnat caml_plat_mmap_granularity = 0;
+intnat caml_plat_pagesize = 0;
+intnat caml_plat_mmap_alignment = 0;
 
 uintnat caml_mem_round_up_pages(uintnat size)
 {
-  return round_up(size, caml_plat_mmap_granularity);
+  return round_up(size, caml_plat_pagesize);
 }
 
-#define Is_page_aligned(size) ((size & (caml_plat_mmap_granularity - 1)) == 0)
+#define Is_page_aligned(size) ((size & (caml_plat_pagesize - 1)) == 0)
 
 #ifdef DEBUG
 static struct lf_skiplist mmap_blocks = {NULL};
@@ -158,7 +159,7 @@ void* caml_mem_map(uintnat size, uintnat alignment, int reserve_only)
 {
   CAMLassert(Is_power_of_2(alignment));
   CAMLassert(Is_page_aligned(size));
-  alignment = caml_mem_round_up_pages(alignment);
+  alignment = round_up(alignment, caml_plat_mmap_alignment);
 
 #ifdef DEBUG
   if (mmap_blocks.head == NULL) {

vendor/opam/seq/runtime/win32.c
@@ -1092,15 +1092,11 @@ void caml_init_os_params(void)
   SYSTEM_INFO si;
   LARGE_INTEGER frequency;
 
-  /* Get the system page size */
+  /* Get the system page size and allocation granularity. */
   GetSystemInfo(&si);
-  /* Use the Allocation Granularity rather than the Page Size. Page Size only
-     applies when committing a previous reservation, which for OCaml only occurs
-     when committing a minor heap, which will necessarily be larger than both
-     page size and granularity. This simplifies caml_mem_map, since we can
-     guarantee that trimming will not be required. */
   CAMLassert(si.dwAllocationGranularity >= si.dwPageSize);
-  caml_plat_mmap_granularity = si.dwAllocationGranularity;
+  caml_plat_pagesize = si.dwPageSize;
+  caml_plat_mmap_alignment = si.dwAllocationGranularity;
 
   /* Get the number of nanoseconds for each tick in QueryPerformanceCounter */
   QueryPerformanceFrequency(&frequency);
@@ -1117,9 +1113,9 @@ int64_t caml_time_counter(void)
 
 void *caml_plat_mem_map(uintnat size, uintnat alignment, int reserve_only)
 {
-  /* VirtualAlloc returns an address aligned to caml_plat_mmap_granularity, so
+  /* VirtualAlloc returns an address aligned to caml_plat_mmap_alignment, so
      trimming will not be required. VirtualAlloc returns 0 on error. */
-  if (alignment > caml_plat_mmap_granularity)
+  if (alignment > caml_plat_mmap_alignment)
     caml_fatal_error("Cannot align memory to %" ARCH_INTNAT_PRINTF_FORMAT "x"
                      " on this platform", alignment);
   return