gasche/ocaml

matching debug: totality information on all COMBINE calls

lambda/matching.ml
@@ -2799,6 +2799,12 @@ let complete_pats_constrs = function
 *)
 
 let mk_failaction_neg partial ctx def =
+  debugf
+    "@,@[<v 2>COMBINE (mk_failaction_neg %s)@]"
+    ( match partial with
+    | Partial -> "Partial"
+    | Total -> "Total"
+    );
   match partial with
   | Partial -> (
       match Default_environment.pop def with
@@ -2847,13 +2853,17 @@ let mk_failaction_pos partial seen ctx defs =
         defs
     in
     debugf
-      "@,@[<v 2>COMBINE (mk_failaction_pos)@,\
+      "@,@[<v 2>COMBINE (mk_failaction_pos %s)@,\
            %a@,\
            @[<v 2>FAIL PATTERNS:@,\
              %a@]@,\
            @[<v 2>POSITIVE JUMPS:@,\
              %a@]\
            @]"
+      ( match partial with
+      | Partial -> "Partial"
+      | Total -> "Total"
+      )
       Default_environment.pp defs
       (Format.pp_print_list ~pp_sep:Format.pp_print_cut
          Printpat.top_pretty) fail_pats

avsm/unpac

vendor/opam/seq/lambda/matching.ml
@@ -2735,6 +2735,10 @@ let complete_pats_constrs = function
 *)
 
 let mk_failaction_neg partial ctx def =
+  debugf
+    "@,@[<v 2>COMBINE (mk_failaction_neg %a)@]"
+    pp_partial partial
+  ;
   match partial with
   | Partial -> (
       match Default_environment.pop def with
@@ -2783,13 +2787,14 @@ let mk_failaction_pos partial seen ctx defs =
         defs
     in
     debugf
-      "@,@[<v 2>COMBINE (mk_failaction_pos)@,\
+      "@,@[<v 2>COMBINE (mk_failaction_pos %a)@,\
            %a@,\
            @[<v 2>FAIL PATTERNS:@,\
              %a@]@,\
            @[<v 2>POSITIVE JUMPS:@,\
              %a@]\
            @]"
+      pp_partial partial
       Default_environment.pp defs
       (Format.pp_print_list ~pp_sep:Format.pp_print_cut
          Printpat.pretty_pat) fail_pats