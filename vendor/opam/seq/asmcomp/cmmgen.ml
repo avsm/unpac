lthls/ocaml

Add Constant and Class classifications
Use that to assert that Static bindings always have a known size

asmcomp/cmmgen.ml
@@ -224,8 +224,17 @@ let rec expr_size env = function
 
 let expr_size_of_binding (clas : Lambda.rec_check_classification) expr =
   match clas with
-  | Dynamic -> RHS_nonrec
-  | Static -> expr_size V.empty expr
+  | Dynamic | Constant -> RHS_nonrec
+  | Class ->
+       (* Actual size is always 4, but [transl_class] only generates
+          explicit allocations when the classes are actually recursive.
+          Computing the size means that we don't go through pre-allocation
+          when the classes are not recursive. *)
+      expr_size V.empty expr
+  | Static ->
+      let result = expr_size V.empty expr in
+      assert (result <> RHS_nonrec);
+      result
 
 (* Translate structured constants to Cmm data items *)
 

bytecomp/bytegen.ml
@@ -312,8 +312,17 @@ let rec size_of_lambda env = function
 
 let size_of_rec_binding clas expr =
   match (clas : Lambda.rec_check_classification) with
-  | Dynamic -> RHS_nonrec
-  | Static -> size_of_lambda Ident.empty expr
+  | Dynamic | Constant -> RHS_nonrec
+  | Class ->
+       (* Actual size is always 4, but [transl_class] only generates
+          explicit allocations when the classes are actually recursive.
+          Computing the size means that we don't go through pre-allocation
+          when the classes are not recursive. *)
+      size_of_lambda Ident.empty expr
+  | Static ->
+      let result = size_of_lambda Ident.empty expr in
+      assert (result <> RHS_nonrec);
+      result
 
 (**** Merging consecutive events ****)
 

avsm/unpac

vendor/opam/seq/asmcomp/cmmgen.ml
@@ -224,8 +224,22 @@ let rec expr_size env = function
 
 let expr_size_of_binding (clas : Typedtree.recursive_binding_kind) expr =
   match clas with
-  | Not_recursive -> RHS_nonrec
-  | Static -> expr_size V.empty expr
+  | Not_recursive | Constant -> RHS_nonrec
+  | Class ->
+       (* Actual size is always 4, but [transl_class] only generates
+          explicit allocations when the classes are actually recursive.
+          Computing the size means that we don't go through pre-allocation
+          when the classes are not recursive. *)
+      expr_size V.empty expr
+  | Static ->
+      let result = expr_size V.empty expr in
+      (* Patching Closure to properly propagate Constant kinds is too complex;
+         for now, just live with the fact that Static expressions may not always
+         be statically allocated with Closure.
+         Forthcoming patches will remove all this logic anyway. *)
+      if Config.flambda then
+        assert (result <> RHS_nonrec);
+      result
 
 (* Translate structured constants to Cmm data items *)
 

vendor/opam/seq/bytecomp/bytegen.ml
@@ -313,8 +313,17 @@ let rec size_of_lambda env = function
 
 let size_of_rec_binding clas expr =
   match (clas : Typedtree.recursive_binding_kind) with
-  | Not_recursive -> RHS_nonrec
-  | Static -> size_of_lambda Ident.empty expr
+  | Not_recursive | Constant -> RHS_nonrec
+  | Class ->
+       (* Actual size is always 4, but [transl_class] only generates
+          explicit allocations when the classes are actually recursive.
+          Computing the size means that we don't go through pre-allocation
+          when the classes are not recursive. *)
+      size_of_lambda Ident.empty expr
+  | Static ->
+      let result = size_of_lambda Ident.empty expr in
+      assert (result <> RHS_nonrec);
+      result
 
 (**** Merging consecutive events ****)
 