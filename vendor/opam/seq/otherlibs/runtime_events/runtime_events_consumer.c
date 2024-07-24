edwintorok/ocaml

runtime_events_consumer: copy the metadata
We'll want to validate the offsets in the metadata.
But if the metadata is writable by another process,
then it could get corrupted after we've already validated it.

Make a private copy of the metadata that will be useful for bounds
checking.

No functional change yet.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

otherlibs/runtime_events/runtime_events_consumer.c
@@ -53,7 +53,8 @@
 struct caml_runtime_events_cursor {
   int cursor_open;                  /* has this cursor been opened? */
   atomic_uintnat cursor_in_poll;    /* cursor is inside a read_poll() */
-  struct runtime_events_metadata_header *metadata; /* ptr to ring metadata */
+  void *map;
+  struct runtime_events_metadata_header metadata; /* ptr to ring metadata */
   uint64_t *current_positions;      /* positions in the rings for each domain */
   size_t ring_file_size_bytes; /* size of the runtime_events file in bytes */
   int next_read_domain;        /* the next domain to read from */
@@ -171,7 +172,7 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
     return E_MAP_FAILURE;
   }
 
-  cursor->metadata = MapViewOfFile(
+  cursor->map = MapViewOfFile(
     cursor->ring_handle,
     FILE_MAP_ALL_ACCESS,
     0,
@@ -179,7 +180,7 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
     0
   );
 
-  if( cursor->metadata == NULL ) {
+  if( cursor->map == NULL ) {
     caml_stat_free(cursor);
     caml_stat_free(runtime_events_loc);
     return E_MAP_FAILURE;
@@ -216,21 +217,22 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
 
   /* This cast is necessary for compatibility with Illumos' non-POSIX
     mmap/munmap */
-  cursor->metadata = (struct runtime_events_metadata_header *)
-                      mmap(NULL, cursor->ring_file_size_bytes, mmap_prot,
+  cursor->map = (void*) mmap(NULL, cursor->ring_file_size_bytes, mmap_prot,
                           MAP_SHARED, ring_fd, 0);
 
-  if( cursor->metadata == MAP_FAILED ) {
+  if( cursor->map == MAP_FAILED ) {
     caml_stat_free(cursor);
     caml_stat_free(runtime_events_loc);
     return E_MAP_FAILURE;
   }
 #endif
 
-  cursor->current_positions =
-      caml_stat_alloc(cursor->metadata->max_domains * sizeof(uint64_t));
+  cursor->metadata = *(struct runtime_events_metadata_header*)cursor->map;
 
-  for (int j = 0; j < cursor->metadata->max_domains; j++) {
+  cursor->current_positions =
+      caml_stat_alloc(cursor->metadata.max_domains * sizeof(uint64_t));
+
+  for (int j = 0; j < cursor->metadata.max_domains; j++) {
     cursor->current_positions[j] = 0;
   }
   cursor->cursor_open = 1;
@@ -345,13 +347,13 @@ void caml_runtime_events_free_cursor(struct caml_runtime_events_cursor *cursor){
   if (cursor->cursor_open) {
     cursor->cursor_open = 0;
 #ifdef _WIN32
-    UnmapViewOfFile(cursor->metadata);
+    UnmapViewOfFile(cursor->map);
     CloseHandle(cursor->ring_file_handle);
     CloseHandle(cursor->ring_handle);
 #else
     /* This cast is necessary for compatibility with Illumos' non-POSIX
       mmap/munmap */
-    munmap((void*)cursor->metadata, cursor->ring_file_size_bytes);
+    munmap((void*)cursor->map, cursor->ring_file_size_bytes);
 #endif
     caml_stat_free(cursor->current_positions);
     caml_stat_free(cursor);
@@ -384,19 +386,19 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
      This is necessary because in the case where the consumer can't keep up
      with message production (i.e max_events is hit each time) it ensures that
      messages are read from all domains, rather than just the first. */
-  for (int i = 0; i < cursor->metadata->max_domains && !early_exit; i++) {
-    int domain_num = (start_domain + i) % cursor->metadata->max_domains;
+  for (int i = 0; i < cursor->metadata.max_domains && !early_exit; i++) {
+    int domain_num = (start_domain + i) % cursor->metadata.max_domains;
 
     struct runtime_events_buffer_header *runtime_events_buffer_header =
         (struct runtime_events_buffer_header *)(
-          (char*)cursor->metadata +
-          cursor->metadata->headers_offset +
-          domain_num * cursor->metadata->ring_header_size_bytes
+          (char*)cursor->map +
+          cursor->metadata.headers_offset +
+          domain_num * cursor->metadata.ring_header_size_bytes
         );
 
-    uint64_t *ring_ptr = (uint64_t *)((char*)cursor->metadata +
-                                      cursor->metadata->data_offset +
-                                domain_num * cursor->metadata->ring_size_bytes);
+    uint64_t *ring_ptr = (uint64_t *)((char*)cursor->map +
+                                      cursor->metadata.data_offset +
+                                domain_num * cursor->metadata.ring_size_bytes);
 
     do {
       uint64_t buf[RUNTIME_EVENTS_MAX_MSG_LENGTH];
@@ -420,7 +422,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
         break;
       }
 
-      ring_mask = cursor->metadata->ring_size_elements - 1;
+      ring_mask = cursor->metadata.ring_size_elements - 1;
       header = ring_ptr[cursor->current_positions[domain_num] & ring_mask];
       msg_length = RUNTIME_EVENTS_ITEM_LENGTH(header);
 
@@ -506,7 +508,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
 
         struct runtime_events_custom_event *custom_event =
           &((struct runtime_events_custom_event *)
-            ((char *)cursor->metadata + cursor->metadata->custom_events_offset))
+            ((char *)cursor->map + cursor->metadata.custom_events_offset))
             [event_id];
         char* event_name = custom_event->name;
         ev_user_message_type event_type = RUNTIME_EVENTS_ITEM_TYPE(header);
@@ -573,7 +575,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
     /* next domain to read from (saved in the cursor so we can resume from it
        if need be in the next poll of the cursor). */
     cursor->next_read_domain =
-      (domain_num + 1 == cursor->metadata->max_domains) ? 0 : domain_num + 1;
+      (domain_num + 1 == cursor->metadata.max_domains) ? 0 : domain_num + 1;
   }
 
   if (events_consumed != NULL) {

avsm/unpac

Co-authored-by: Miod Vallat <118974489+dustanddreams@users.noreply.github.com>

vendor/opam/seq/otherlibs/runtime_events/runtime_events_consumer.c
@@ -53,7 +53,8 @@
 struct caml_runtime_events_cursor {
   int cursor_open;                  /* has this cursor been opened? */
   atomic_uintnat cursor_in_poll;    /* cursor is inside a read_poll() */
-  struct runtime_events_metadata_header *metadata; /* ptr to ring metadata */
+  void *map;
+  struct runtime_events_metadata_header metadata; /* copy of ring metadata */
   uint64_t *current_positions;      /* positions in the rings for each domain */
   size_t ring_file_size_bytes; /* size of the runtime_events file in bytes */
   int next_read_domain;        /* the next domain to read from */
@@ -171,7 +172,7 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
     return E_MAP_FAILURE;
   }
 
-  cursor->metadata = MapViewOfFile(
+  cursor->map = MapViewOfFile(
     cursor->ring_handle,
     FILE_MAP_ALL_ACCESS,
     0,
@@ -179,7 +180,7 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
     0
   );
 
-  if( cursor->metadata == NULL ) {
+  if( cursor->map == NULL ) {
     caml_stat_free(cursor);
     caml_stat_free(runtime_events_loc);
     return E_MAP_FAILURE;
@@ -216,21 +217,22 @@ caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
 
   /* This cast is necessary for compatibility with Illumos' non-POSIX
     mmap/munmap */
-  cursor->metadata = (struct runtime_events_metadata_header *)
-                      mmap(NULL, cursor->ring_file_size_bytes, mmap_prot,
+  cursor->map = (void*) mmap(NULL, cursor->ring_file_size_bytes, mmap_prot,
                           MAP_SHARED, ring_fd, 0);
 
-  if( cursor->metadata == MAP_FAILED ) {
+  if( cursor->map == MAP_FAILED ) {
     caml_stat_free(cursor);
     caml_stat_free(runtime_events_loc);
     return E_MAP_FAILURE;
   }
 #endif
 
-  cursor->current_positions =
-      caml_stat_alloc(cursor->metadata->max_domains * sizeof(uint64_t));
+  cursor->metadata = *(struct runtime_events_metadata_header*)cursor->map;
 
-  for (int j = 0; j < cursor->metadata->max_domains; j++) {
+  cursor->current_positions =
+      caml_stat_alloc(cursor->metadata.max_domains * sizeof(uint64_t));
+
+  for (int j = 0; j < cursor->metadata.max_domains; j++) {
     cursor->current_positions[j] = 0;
   }
   cursor->cursor_open = 1;
@@ -345,13 +347,13 @@ void caml_runtime_events_free_cursor(struct caml_runtime_events_cursor *cursor){
   if (cursor->cursor_open) {
     cursor->cursor_open = 0;
 #ifdef _WIN32
-    UnmapViewOfFile(cursor->metadata);
+    UnmapViewOfFile(cursor->map);
     CloseHandle(cursor->ring_file_handle);
     CloseHandle(cursor->ring_handle);
 #else
     /* This cast is necessary for compatibility with Illumos' non-POSIX
       mmap/munmap */
-    munmap((void*)cursor->metadata, cursor->ring_file_size_bytes);
+    munmap((void*)cursor->map, cursor->ring_file_size_bytes);
 #endif
     caml_stat_free(cursor->current_positions);
     caml_stat_free(cursor);
@@ -384,19 +386,19 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
      This is necessary because in the case where the consumer can't keep up
      with message production (i.e max_events is hit each time) it ensures that
      messages are read from all domains, rather than just the first. */
-  for (int i = 0; i < cursor->metadata->max_domains && !early_exit; i++) {
-    int domain_num = (start_domain + i) % cursor->metadata->max_domains;
+  for (int i = 0; i < cursor->metadata.max_domains && !early_exit; i++) {
+    int domain_num = (start_domain + i) % cursor->metadata.max_domains;
 
     struct runtime_events_buffer_header *runtime_events_buffer_header =
         (struct runtime_events_buffer_header *)(
-          (char*)cursor->metadata +
-          cursor->metadata->headers_offset +
-          domain_num * cursor->metadata->ring_header_size_bytes
+          (char*)cursor->map +
+          cursor->metadata.headers_offset +
+          domain_num * cursor->metadata.ring_header_size_bytes
         );
 
-    uint64_t *ring_ptr = (uint64_t *)((char*)cursor->metadata +
-                                      cursor->metadata->data_offset +
-                                domain_num * cursor->metadata->ring_size_bytes);
+    uint64_t *ring_ptr = (uint64_t *)((char*)cursor->map +
+                                      cursor->metadata.data_offset +
+                                domain_num * cursor->metadata.ring_size_bytes);
 
     do {
       uint64_t buf[RUNTIME_EVENTS_MAX_MSG_LENGTH];
@@ -420,7 +422,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
         break;
       }
 
-      ring_mask = cursor->metadata->ring_size_elements - 1;
+      ring_mask = cursor->metadata.ring_size_elements - 1;
       header = ring_ptr[cursor->current_positions[domain_num] & ring_mask];
       msg_length = RUNTIME_EVENTS_ITEM_LENGTH(header);
 
@@ -506,7 +508,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
 
         struct runtime_events_custom_event *custom_event =
           &((struct runtime_events_custom_event *)
-            ((char *)cursor->metadata + cursor->metadata->custom_events_offset))
+            ((char *)cursor->map + cursor->metadata.custom_events_offset))
             [event_id];
         char* event_name = custom_event->name;
         ev_user_message_type event_type = RUNTIME_EVENTS_ITEM_TYPE(header);
@@ -573,7 +575,7 @@ caml_runtime_events_read_poll(struct caml_runtime_events_cursor *cursor,
     /* next domain to read from (saved in the cursor so we can resume from it
        if need be in the next poll of the cursor). */
     cursor->next_read_domain =
-      (domain_num + 1 == cursor->metadata->max_domains) ? 0 : domain_num + 1;
+      (domain_num + 1 == cursor->metadata.max_domains) ? 0 : domain_num + 1;
   }
 
   if (events_consumed != NULL) {