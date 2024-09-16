ocaml/opam

Improve the error message when a directory is not available while fetching using rsync

src/repository/opamLocal.ml
@@ -168,9 +168,9 @@ module B = struct
          else
            OpamFilename.mkdir quarantine;
          pull_dir_quiet quarantine url) @@+ function
-    | Not_available _ ->
+    | Not_available (_, msg) ->
       finalise ();
-      Done (OpamRepositoryBackend.Update_err (Failure "rsync failed"))
+      Done (OpamRepositoryBackend.Update_err (Failure ("Not available: " ^ msg)))
     | Up_to_date _ ->
       finalise (); Done OpamRepositoryBackend.Update_empty
     | Result _ ->

avsm/unpac

vendor/opam/opam-core/src/repository/opamLocal.ml
@@ -168,9 +168,9 @@ module B = struct
          else
            OpamFilename.mkdir quarantine;
          pull_dir_quiet quarantine url) @@+ function
-    | Not_available _ ->
+    | Not_available (_, msg) ->
       finalise ();
-      Done (OpamRepositoryBackend.Update_err (Failure "rsync failed"))
+      Done (OpamRepositoryBackend.Update_err (Failure ("rsync error: " ^ msg)))
     | Up_to_date _ ->
       finalise (); Done OpamRepositoryBackend.Update_empty
     | Result _ ->