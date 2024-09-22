ocaml/ocaml

Add align_double and align_int64 to `ocamlc -config`

utils/config.mli
@@ -223,6 +223,13 @@ val flat_float_array : bool
 (** Whether the compiler and runtime automagically flatten float
     arrays *)
 
+val align_double : bool
+(** Wether the compiler and runtime needs to align double values.
+    If [false], a [floatarray] value can be casted into a C array of double. *)
+
+val align_int64 : bool
+(** Wether the compiler and runtime needs to align int64 values *)
+
 val function_sections : bool
 (** Whether the compiler was configured to generate
     each function in a separate section *)

avsm/unpac

vendor/opam/seq/utils/config.mli
@@ -222,6 +222,13 @@ val flat_float_array : bool
 (** Whether the compiler and runtime automagically flatten float
     arrays *)
 
+val align_double : bool
+(** Whether the compiler and runtime need to align double values.
+    If [false], a [floatarray] value can be cast to a C array of doubles. *)
+
+val align_int64 : bool
+(** Whether the compiler and runtime need to align int64 values *)
+
 val function_sections : bool
 (** Whether the compiler was configured to generate
     each function in a separate section *)