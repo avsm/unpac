avsm/unpac

commit d5a86debd169f48faa61b374e6b3b64964d1504a

Use the default criteria during reinstall/upgrade when requesting at least one non-installed package

vendor/opam/opam-core/src/client/opamClient.ml
@@ -113,6 +113,7 @@ let compute_upgrade_t
         | Some nv -> not (OpamPackage.Set.mem nv (Lazy.force t.available_packages)))
       atoms
   in
+  let criteria = if to_install = [] then `Upgrade else `Default in
   if all then
     names,
     OpamSolution.resolve t Upgrade
@@ -123,7 +124,7 @@ let compute_upgrade_t
          ~upgrade:to_upgrade
          ~deprequest:(OpamFormula.to_atom_formula formula)
          ~all:[]
-         ~criteria:`Upgrade ())
+         ~criteria ())
   else
   names,
   OpamSolution.resolve t Upgrade
@@ -132,6 +133,7 @@ let compute_upgrade_t
        ~install:to_install
        ~upgrade:to_upgrade
        ~deprequest:(OpamFormula.to_atom_formula formula)
+       ~criteria
        ())
 
 let print_requested requested formula =
@@ -1337,7 +1339,10 @@ let reinstall_t t ?ask ?(force=false) ~assume_built atoms =
     else t, atoms
   in
 
-  let request = OpamSolver.request ~install:atoms ~criteria:`Fixup () in
+  let request =
+    let criteria = if to_install = [] then `Fixup else `Default in
+    OpamSolver.request ~install:atoms ~criteria ()
+  in
 
   let t, solution =
     OpamSolution.resolve_and_apply ?ask t Reinstall

vendor/opam/opam-core/tests/reftests/reinstall.test
@@ -86,7 +86,6 @@ The following actions would be performed:
 === install 1 package
   - install   c 1
 ### :::::: Check that reinstalling a non-installed package is equivalent to installing it
-### :::::: TODO: This is currently broken
 ### <pkg:d.1>
 opam-version: "2.0"
 ### opam reinstall d --cudf=reinstall --show --yes
@@ -99,15 +98,10 @@ The following actions would be performed:
 === install 1 package
   - install d 1
 ### diff install-1.cudf reinstall-1.cudf
-2c2
-< # Criteria: -removed,-count[avoid-version,changed],-count[version-lag,request],-count[version-lag,changed],-count[missing-depexts,changed],-changed
----
-> # Criteria: -changed,-count[avoid-version:,true],-count[version-lag:,false],-count[missing-depexts:,true]
 48d47
 < opam-query: 1
 # Return code 1 #
 ### :::::: Check again with one install and one non-installed
-### :::::: TODO: This is currently broken
 ### opam reinstall d a --cudf=reinstall --show --yes
 d is not installed. Install it? [y/n] y
 The following actions would be performed:
@@ -127,10 +121,6 @@ The following actions would be performed:
   - install   c 1
   - install   d 1
 ### diff install-1.cudf reinstall-1.cudf
-2c2
-< # Criteria: -removed,-count[avoid-version,changed],-count[version-lag,request],-count[version-lag,changed],-count[missing-depexts,changed],-changed
----
-> # Criteria: -changed,-count[avoid-version:,true],-count[version-lag:,false],-count[missing-depexts:,true]
 20a21
 > reinstall: true
 49d49

commit a6681b46d66750214ef31a8706c6d06bb4b711f1

Fix regression on 'opam upgrade <package>' upgrading unrelated things
Regression introduced in d5a86debd169f48faa61b374e6b3b64964d1504a, which is
dedicated to a different case (functionality of this patch is preserved)

vendor/opam/opam-core/src/client/opamClient.ml
@@ -113,7 +113,7 @@ let compute_upgrade_t
         | Some nv -> not (OpamPackage.Set.mem nv (Lazy.force t.available_packages)))
       atoms
   in
-  let criteria = if to_install = [] then `Upgrade else `Default in
+  let criteria = if to_install = [] && all then `Upgrade else `Default in
   if all then
     names,
     OpamSolution.resolve t Upgrade

vendor/opam/opam-core/tests/reftests/upgrade.test
@@ -432,7 +432,6 @@ Done.
 => Note: This package is deprecated.
 ### :::::::::::::::::::::::
 ### : Make sure opam upgrade <pkg> only tries to upgrade <pkg> and not anything else
-### : TODO: broken
 ### :::::::::::::::::::::::
 ### opam upgrade
 Already up-to-date.
@@ -448,8 +447,7 @@ depends: "foo"
 opam-version: "2.0"
 ### opam upgrade --show foo
 The following actions would be performed:
-=== upgrade 2 packages
-  - upgrade baz 1 to 2 [uses foo]
+=== upgrade 1 package
   - upgrade foo 3 to 4
 ### opam upgrade --show -a foo
 The following actions would be performed:
@@ -469,9 +467,8 @@ The following actions would be performed:
   - upgrade foo 3 to 4
 ### opam upgrade --show baz
 The following actions would be performed:
-=== upgrade 2 packages
+=== upgrade 1 package
   - upgrade baz 1 to 2
-  - upgrade foo 3 to 4 [required by baz]
 ### opam upgrade --show -a baz
 The following actions would be performed:
 === upgrade 3 packages
@@ -496,16 +493,16 @@ The following actions would be performed:
   - upgrade foo 3 to 4
 ### opam upgrade baz
 The following actions will be performed:
-=== upgrade 2 packages
+=== upgrade 1 package
   - upgrade baz 1 to 2
-  - upgrade foo 3 to 4 [required by baz]
 
 <><> Processing actions <><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 -> removed   baz.1
--> removed   foo.3
--> installed foo.4
 -> installed baz.2
 Done.
 ### opam upgrade --show foo
-Already up-to-date.
-Nothing to do.
+The following actions would be performed:
+=== recompile 1 package
+  - recompile baz 2      [uses foo]
+=== upgrade 1 package
+  - upgrade   foo 3 to 4