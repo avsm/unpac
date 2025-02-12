ocaml/ocaml

a more versatile API
functor Make provides a simple API (with priorities apart
from values) and functor MakePoly provides a generic API
where the type of elements is user-provided

stdlib/pqueue.mli
@@ -15,8 +15,8 @@
 (** Priority queues.
 
     The {!Pqueue} module implements a data structure of priority queues,
-    given a total ordering function over the elements. This is a mutable
-    data structure. Smaller elements are considered to have a higher
+    given a totally ordered type for priorities. This is a mutable
+    data structure. Smaller values are considered to have a higher
     priority (i.e., these are min-priority queues).
 
     The implementation uses a heap stored in a dynamic array, and is
@@ -26,14 +26,7 @@
     [of_array] runs in linear time (and thus must be preferred to
     repeated insertions with [add]).
 
-    It is fine to have several elements that are considered equal by
-    the ordering function and which are then considered as having the
-    same priority. Said otherwise, the priority queue must be seen as
-    a bag, not a set.  In particular, it is fine (and even expected)
-    to have elements of a tuple type such as [priority * whatever]
-    where the ordering function is only comparing the first component
-    of the elements.
-
+    It is fine to have several elements with the same priority.
     It is guaranteed that the element returned by [min_elt] (or
     [min_elt_opt]) is the one that is removed from the priority queue
     by [pop_min] (or [pop_min_opt] or [remove_min]). This is important
@@ -46,22 +39,20 @@
 module type OrderedType =
   sig
     type t
-      (** The type of the priority queue elements. *)
+      (** The type of priorities. *)
 
     val compare : t -> t -> int
-      (** A total ordering function over the elements.
+      (** A total ordering function to compare priorities.
 
-          This is a two-argument function [f] such that [f e1 e2] is
-          zero if the elements [e1] and [e2] are equal (i.e.  having
-          the same priority), [f e1 e2] is strictly negative if [e1]
-          is smaller than [e2] (i.e.  [e1] has higher priority than
-          [e2]), and [f e1 e2] is strictly positive if [e1] is greater
-          than [e2] (i.e. [e1] has lower priority than [e2]).
+          This is a two-argument function [f] such that [f p1 p2] is
+          zero if the priorities [p1] and [p2] are equal,
+          [f p1 p2] is strictly negative if [p1] has higher priority than
+          [p2], and [f p1 p2] is strictly positive if [p1]
+          has lower priority than [p2].
 
           The generic structural comparison function {!Stdlib.compare}
-          is a suitable ordering function when the elements are
-          identified with their priority (such as integers, strings,
-          etc.) and when the smaller the better. *)
+          is a suitable ordering function to get min-priority queues
+          with priority types such as [int] or [string] for instance. *)
   end
 (** Input signature of the functor {!Make}. *)
 
@@ -70,71 +61,72 @@ module type S =
 
     (** {1:pqueue Priority queues} *)
 
-    type elt
-    (** The type of the priority queue elements. *)
+    type prio
+    (** The type of priorities. *)
 
-    type t
+    type 'a t
     (** The type of priority queues. *)
 
-    val create: unit -> t
+    val create: unit -> 'a t
     (** Return a new priority queue, initially empty. *)
 
-    val length: t -> int
+    val length: 'a t -> int
     (** Return the number of elements in a priority queue. *)
 
-    val is_empty: t -> bool
+    val is_empty: 'a t -> bool
     (** [is_empty q] is [true] iff [q] is empty, that is, iff [length q = 0]. *)
 
-    val add: t -> elt -> unit
-    (** [add q x] adds the element [x] in the priority queue [q]. *)
+    val add: 'a t -> prio -> 'a -> unit
+    (** [add q p x] adds the element [x] with priority [p]
+        in the priority queue [q]. *)
 
-    val add_seq: t -> elt Seq.t -> unit
+    val add_seq: 'a t -> (prio * 'a) Seq.t -> unit
     (** [add q s] adds the elements of [s] in the priority queue [q]. *)
 
     exception Empty
     (** Raised when {!min_elt}, {!pop_min} or {!remove_min} is applied
         to an empty queue. *)
 
-    val min_elt: t -> elt
+    val min_elt: 'a t -> prio * 'a
     (** [min_elt q] returns an element in queue [q] with minimal priority,
         without removing it from the queue,
         or raises {!Empty} if the queue is empty. *)
 
-    val min_elt_opt: t -> elt option
+    val min_elt_opt: 'a t -> (prio * 'a) option
     (** [min_elt_opt q] returns an element in queue [q] with minimal priority,
         without removing it from the queue, or returns [None] if the queue
         is empty. *)
 
-    val pop_min: t -> elt
+    val pop_min: 'a t -> prio * 'a
     (** [pop_min q] removes and returns an element in queue [q] with
         minimal priority, or raises {!Empty} if the queue is empty. *)
 
-    val pop_min_opt: t -> elt option
+    val pop_min_opt: 'a t -> (prio * 'a) option
     (** [pop_min_opt q] removes and returns an element in queue [q] with
         minimal priority, or returns [None] if the queue is empty. *)
 
-    val remove_min: t -> unit
+    val remove_min: 'a t -> unit
     (** [pop_min_opt q] removes an element in queue [q] with
         minimal priority, or raises {!Empty} if the queue is empty. *)
 
-    val clear: t -> unit
+    val clear: 'a t -> unit
     (** Discard all elements from a priority queue. *)
 
-    val copy: t -> t
+    val copy: 'a t -> 'a t
     (** [copy q] is a shallow copy of [q] (a new priority queue
         containing the same elements as [q]). *)
 
     (** {1:conversions Conversions from other data structures} *)
 
-    val of_array: elt array -> t
+    val of_array: (prio * 'a) array -> 'a t
     (** [of_array a] returns a new priority queue containing the
         elements of array [a]. Runs in linear time. *)
 
-    val of_list: elt list -> t
+    val of_list: (prio * 'a) list -> 'a t
     (** [of_list l] returns a new priority queue containing the
         elements of list [l]. Runs in linear time. *)
 
-    val of_seq: elt Seq.t -> t
+    val of_seq: (prio * 'a) Seq.t -> 'a t
     (** [of_seq s] returns a new priority queue containing the
         elements of sequence [s]. Runs in linear time. *)
 
@@ -148,22 +140,69 @@ module type S =
         Such an error may be detected and signaled by the backing dynamic
         array implementation, but this is not guaranteed. *)
 
-    val iter: (elt -> unit) -> t -> unit
+    val iter: (prio -> 'a -> unit) -> 'a t -> unit
     (** [iter f q] calls [f] on each element of [q] in some
         unspecified order. *)
 
-    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    val fold: ('acc -> prio -> 'a -> 'acc) -> 'acc -> 'a t -> 'acc
     (** [fold f accu q] is [(f (... (f (f accu x1) x2) ...) xn)]
         where [x1,x2,...,xn] are the elements of [q] in some
         unspecified order. *)
 
-    val to_seq: t -> elt Seq.t
+    val to_seq: 'a t -> (prio * 'a) Seq.t
     (** [to_seq q] is the sequence of elements of [q] in some
         unspecified order. *)
 
   end
 (** Output signature of the functor {!Make}. *)
 
-module Make (Ord : OrderedType) : S with type elt = Ord.t
+module Make (Prio : OrderedType) : S with type prio = Prio.t
 (** Functor building an implementation of the priority queue structure
     given a totally ordered type. *)
+
+module MinQueue : S with type prio = int
+(** Min-priority queues with integer priorities. *)
+
+module MaxQueue : S with type prio = int
+(** Max-priority queues with integer priorities. *)
+
+module MakePoly (E : sig
+    type 'a t
+    val compare: 'a t -> 'a t -> int
+  end) : sig
+    type 'a elt = 'a E.t
+    type 'a t
+    val create: unit -> 'a t
+    val length: 'a t -> int
+    val is_empty: 'a t -> bool
+    val add: 'a t -> 'a elt -> unit
+    val add_seq: 'a t -> 'a elt Seq.t -> unit
+    exception Empty
+    val min_elt: 'a t -> 'a elt
+    val min_elt_opt: 'a t -> 'a elt option
+    val pop_min: 'a t -> 'a elt
+    val pop_min_opt: 'a t -> 'a elt option
+    val remove_min: 'a t -> unit
+    val clear: 'a t -> unit
+    val copy: 'a t -> 'a t
+    val of_array: 'a elt array -> 'a t
+    val of_list: 'a elt list -> 'a t
+    val of_seq: 'a elt Seq.t -> 'a t
+    val iter: ('a elt -> unit) -> 'a t -> unit
+    val fold: ('acc -> 'a elt -> 'acc) -> 'acc -> 'a t -> 'acc
+    val to_seq: 'a t -> 'a elt Seq.t
+  end
+(** Functor building an implementation of priority queues
+    given a totally ordered type of elements. This is convenient when
+    the type of elements is already equipped with a comparison function.
+    For instance, we get priority queues with string elements as follows:
+
+{[
+  module IntPQ = Pqueue.MakePoly(struct
+    type _ t = string
+    let compare = String.compare
+  end)
+]}
+
+    Note that we cannot apply functor [MakePoly] to module [Int] directly,
+    as the type of elements is polymorphic. *)

testsuite/tests/lib-pqueue/test.ml
@@ -1,6 +1,6 @@
 (* TEST *)
 
-module Q = Pqueue.Make(Int)
+module Q = Pqueue.MinQueue
 
 let does_raise f q =
   try
@@ -18,25 +18,25 @@ let check_is_empty q =
 let () =
   let q = Q.create () in
   check_is_empty q;
-  Q.add q 1;
+  Q.add q 1 "a";
   assert (Q.length q = 1); assert (not (Q.is_empty q));
-  assert (Q.min_elt q = 1); assert (Q.min_elt_opt q = Some 1);
+  assert (Q.min_elt q = (1, "a")); assert (Q.min_elt_opt q = Some (1, "a"));
   assert (Q.length q = 1);
-  assert (Q.pop_min q = 1); check_is_empty q;
-  Q.add q 2;
-  Q.add q 1;
-  assert (Q.min_elt q = 1); assert (Q.min_elt_opt q = Some 1);
-  assert (Q.pop_min_opt q = Some 1); assert (Q.length q = 1);
-  assert (Q.min_elt q = 2); assert (Q.min_elt_opt q = Some 2);
+  assert (Q.pop_min q = (1, "a")); check_is_empty q;
+  Q.add q 2 "b";
+  Q.add q 1 "a";
+  assert (Q.min_elt q = (1, "a")); assert (Q.min_elt_opt q = Some (1, "a"));
+  assert (Q.pop_min_opt q = Some (1, "a")); assert (Q.length q = 1);
+  assert (Q.min_elt q = (2, "b")); assert (Q.min_elt_opt q = Some (2, "b"));
   Q.remove_min q; check_is_empty q;
-  Q.add q 2;
-  Q.add q 1;
+  Q.add q 2 "b";
+  Q.add q 1 "a";
   Q.clear q; check_is_empty q;
   ()
 
 let () =
   let q = Q.create () in
-  Q.add q 1; Q.add q 2;
+  Q.add q 1 "a"; Q.add q 2 "b";
   let q' = Q.copy q in
   Q.clear q; check_is_empty q;
   assert (Q.length q' = 2)
@@ -44,48 +44,76 @@ let () =
 let () =
   let q = Q.create () in
   for n = 0 to 10 do
-    for x = n-1 downto 0 do Q.add q x done;
-    for x = 0 to n-1 do assert (Q.pop_min q = x) done;
+    for x = n-1 downto 0 do Q.add q x () done;
+    for x = 0 to n-1 do assert (Q.pop_min q = (x, ())) done;
     check_is_empty q
   done
 
 let () =
   let q = Q.create () in
   for n = 0 to 10 do
-    for x = n-1 downto 0 do Q.add q x done;
-    for x = 0 to n-1 do assert (Q.pop_min q = x) done;
+    for x = n-1 downto 0 do Q.add q x () done;
+    for x = 0 to n-1 do assert (Q.pop_min q = (x, ())) done;
     check_is_empty q
   done
 
 let () =
   for n = 0 to 10 do
-    let a = Array.init n (fun i -> i/3) in
+    let a = Array.init n (fun i -> (i/3, string_of_int i)) in
     let q = Q.of_array a in
     assert (Q.length q = n);
-    for i = 0 to n - 1 do assert (Q.pop_min_opt q = Some a.(i)) done;
+    for i = 0 to n - 1 do match Q.pop_min_opt q with
+                          | None -> assert false
+                          | Some (x, _) -> assert (x = fst a.(i))
+    done;
     check_is_empty q
   done
 
 let () =
   let q = Q.create () in
-  let s = List.to_seq [2;3;1;4;0] in
+  let s = List.to_seq [2, "b"; 3, "c"; 1, "a"; 4, "d"; 0, ""] in
   Q.add_seq q s;
-  assert (Q.min_elt q = 0);
-  assert (Q.fold (+) 0 q = 10)
+  assert (Q.min_elt q = (0, ""));
+  assert (Q.fold (fun acc x _ -> acc+x) 0 q = 10)
 
-(* check that min_elt and pop_min are consistent
-   (we cannot do that with integers) *)
-module Qs = Pqueue.Make(struct
-  type t = int * string
-  let compare x y = Int.compare (fst x) (fst y)
-end)
-
+(* check that min_elt and pop_min are consistent when several elements
+   have the same priority *)
 let () =
-  let q = Qs.create () in
-  Qs.add q (1, "b"); Qs.add q (1, "a"); Qs.add q (1, "d"); Qs.add q (1, "c");
+  let q = Q.create () in
+  Q.add q 1 "b"; Q.add q 1 "a"; Q.add q 1 "d"; Q.add q 1 "c";
   for _ = 1 to 4 do
-    let x = Qs.min_elt q in assert (x = Qs.pop_min q)
+    let x = Q.min_elt q in assert (x = Q.pop_min q)
   done;
   ()
+
+(* check that MaxQueue is indeed a max-pqueue *)
+let () = Pqueue.MaxQueue.(
+  let q = create () in
+  add q 2 "b"; add q 1 "a"; add q 4 "d"; add q 3 "c";
+  for i = 4 downto 1 do let x = pop_min q in assert (fst x = i) done
+  )
+
+let () =
+  let module E = struct type _ t = string let compare = String.compare end in
+  let module PQ = Pqueue.MakePoly(E) in
+  PQ.(
+  let a = [| "b"; "bar"; "a"; "aa"; "foo"; "ocaml" |] in
+  let n = Array.length a in
+  let q = create () in
+  for i = 0 to n - 1 do add q a.(i) done;
+  assert (length q = n);
+  Array.sort String.compare a;
+  for i = 0 to n - 1 do match pop_min_opt q with
+                          | None -> assert false
+                          | Some x -> assert (x = a.(i))
+  done;
+  assert (is_empty q)
+  )
+
+(* check that the code snippet in the documentation type-checks *)
+module IntPQ = Pqueue.MakePoly(struct
+    type _ t = string
+    let compare = String.compare
+  end)
 
 let () = print_endline "OK"

avsm/unpac

vendor/opam/seq/stdlib/pqueue.mli
@@ -15,8 +15,8 @@
 (** Priority queues.
 
     The {!Pqueue} module implements a data structure of priority queues,
-    given a total ordering function over the elements. This is a mutable
-    data structure. Smaller elements are considered to have a higher
+    given a totally ordered type for priorities. This is a mutable
+    data structure. Smaller values are considered to have a higher
     priority (i.e., these are min-priority queues).
 
     The implementation uses a heap stored in a dynamic array, and is
@@ -26,14 +26,7 @@
     [of_array] runs in linear time (and thus must be preferred to
     repeated insertions with [add]).
 
-    It is fine to have several elements that are considered equal by
-    the ordering function and which are then considered as having the
-    same priority. Said otherwise, the priority queue must be seen as
-    a bag, not a set.  In particular, it is fine (and even expected)
-    to have elements of a tuple type such as [priority * whatever]
-    where the ordering function is only comparing the first component
-    of the elements.
-
+    It is fine to have several elements with the same priority.
     It is guaranteed that the element returned by [min_elt] (or
     [min_elt_opt]) is the one that is removed from the priority queue
     by [pop_min] (or [pop_min_opt] or [remove_min]). This is important
@@ -46,22 +39,20 @@
 module type OrderedType =
   sig
     type t
-      (** The type of the priority queue elements. *)
+      (** The type of priorities. *)
 
     val compare : t -> t -> int
-      (** A total ordering function over the elements.
+      (** A total ordering function to compare priorities.
 
-          This is a two-argument function [f] such that [f e1 e2] is
-          zero if the elements [e1] and [e2] are equal (i.e.  having
-          the same priority), [f e1 e2] is strictly negative if [e1]
-          is smaller than [e2] (i.e.  [e1] has higher priority than
-          [e2]), and [f e1 e2] is strictly positive if [e1] is greater
-          than [e2] (i.e. [e1] has lower priority than [e2]).
+          This is a two-argument function [f] such that [f p1 p2] is
+          zero if the priorities [p1] and [p2] are equal,
+          [f p1 p2] is strictly negative if [p1] has higher priority than
+          [p2], and [f p1 p2] is strictly positive if [p1]
+          has lower priority than [p2].
 
           The generic structural comparison function {!Stdlib.compare}
-          is a suitable ordering function when the elements are
-          identified with their priority (such as integers, strings,
-          etc.) and when the smaller the better. *)
+          is a suitable ordering function to get min-priority queues
+          with priority types such as [int] or [string] for instance. *)
   end
 (** Input signature of the functor {!Make}. *)
 
@@ -70,71 +61,72 @@ module type S =
 
     (** {1:pqueue Priority queues} *)
 
-    type elt
-    (** The type of the priority queue elements. *)
+    type prio
+    (** The type of priorities. *)
 
-    type t
+    type 'a t
     (** The type of priority queues. *)
 
-    val create: unit -> t
+    val create: unit -> 'a t
     (** Return a new priority queue, initially empty. *)
 
-    val length: t -> int
+    val length: 'a t -> int
     (** Return the number of elements in a priority queue. *)
 
-    val is_empty: t -> bool
+    val is_empty: 'a t -> bool
     (** [is_empty q] is [true] iff [q] is empty, that is, iff [length q = 0]. *)
 
-    val add: t -> elt -> unit
-    (** [add q x] adds the element [x] in the priority queue [q]. *)
+    val add: 'a t -> prio -> 'a -> unit
+    (** [add q p x] adds the element [x] with priority [p]
+        in the priority queue [q]. *)
 
-    val add_seq: t -> elt Seq.t -> unit
+    val add_seq: 'a t -> (prio * 'a) Seq.t -> unit
     (** [add q s] adds the elements of [s] in the priority queue [q]. *)
 
     exception Empty
     (** Raised when {!min_elt}, {!pop_min} or {!remove_min} is applied
         to an empty queue. *)
 
-    val min_elt: t -> elt
+    val min_elt: 'a t -> prio * 'a
     (** [min_elt q] returns an element in queue [q] with minimal priority,
         without removing it from the queue,
         or raises {!Empty} if the queue is empty. *)
 
-    val min_elt_opt: t -> elt option
+    val min_elt_opt: 'a t -> (prio * 'a) option
     (** [min_elt_opt q] returns an element in queue [q] with minimal priority,
         without removing it from the queue, or returns [None] if the queue
         is empty. *)
 
-    val pop_min: t -> elt
+    val pop_min: 'a t -> prio * 'a
     (** [pop_min q] removes and returns an element in queue [q] with
         minimal priority, or raises {!Empty} if the queue is empty. *)
 
-    val pop_min_opt: t -> elt option
+    val pop_min_opt: 'a t -> (prio * 'a) option
     (** [pop_min_opt q] removes and returns an element in queue [q] with
         minimal priority, or returns [None] if the queue is empty. *)
 
-    val remove_min: t -> unit
+    val remove_min: 'a t -> unit
     (** [remove_min q] removes an element in queue [q] with
         minimal priority, or raises {!Empty} if the queue is empty. *)
 
-    val clear: t -> unit
+    val clear: 'a t -> unit
     (** Discard all elements from a priority queue. *)
 
-    val copy: t -> t
+    val copy: 'a t -> 'a t
     (** [copy q] is a shallow copy of [q] (a new priority queue
         containing the same elements as [q]). *)
 
     (** {1:conversions Conversions from other data structures} *)
 
-    val of_array: elt array -> t
+    val of_array: (prio * 'a) array -> 'a t
     (** [of_array a] returns a new priority queue containing the
         elements of array [a]. Runs in linear time. *)
 
-    val of_list: elt list -> t
+    val of_list: (prio * 'a) list -> 'a t
     (** [of_list l] returns a new priority queue containing the
         elements of list [l]. Runs in linear time. *)
 
-    val of_seq: elt Seq.t -> t
+    val of_seq: (prio * 'a) Seq.t -> 'a t
     (** [of_seq s] returns a new priority queue containing the
         elements of sequence [s]. Runs in linear time. *)
 
@@ -148,22 +140,69 @@ module type S =
         Such an error may be detected and signaled by the backing dynamic
         array implementation, but this is not guaranteed. *)
 
-    val iter: (elt -> unit) -> t -> unit
+    val iter: (prio -> 'a -> unit) -> 'a t -> unit
     (** [iter f q] calls [f] on each element of [q] in some
         unspecified order. *)
 
-    val fold: ('acc -> elt -> 'acc) -> 'acc -> t -> 'acc
+    val fold: ('acc -> prio -> 'a -> 'acc) -> 'acc -> 'a t -> 'acc
     (** [fold f accu q] is [(f (... (f (f accu x1) x2) ...) xn)]
         where [x1,x2,...,xn] are the elements of [q] in some
         unspecified order. *)
 
-    val to_seq: t -> elt Seq.t
+    val to_seq: 'a t -> (prio * 'a) Seq.t
     (** [to_seq q] is the sequence of elements of [q] in some
         unspecified order. *)
 
   end
 (** Output signature of the functor {!Make}. *)
 
-module Make (Ord : OrderedType) : S with type elt = Ord.t
+module Make (Prio : OrderedType) : S with type prio = Prio.t
 (** Functor building an implementation of the priority queue structure
     given a totally ordered type. *)
+
+module MinQueue : S with type prio = int
+(** Min-priority queues with integer priorities. *)
+
+module MaxQueue : S with type prio = int
+(** Max-priority queues with integer priorities. *)
+
+module MakePoly (E : sig
+    type 'a t
+    val compare: 'a t -> 'a t -> int
+  end) : sig
+    type 'a elt = 'a E.t
+    type 'a t
+    val create: unit -> 'a t
+    val length: 'a t -> int
+    val is_empty: 'a t -> bool
+    val add: 'a t -> 'a elt -> unit
+    val add_seq: 'a t -> 'a elt Seq.t -> unit
+    exception Empty
+    val min_elt: 'a t -> 'a elt
+    val min_elt_opt: 'a t -> 'a elt option
+    val pop_min: 'a t -> 'a elt
+    val pop_min_opt: 'a t -> 'a elt option
+    val remove_min: 'a t -> unit
+    val clear: 'a t -> unit
+    val copy: 'a t -> 'a t
+    val of_array: 'a elt array -> 'a t
+    val of_list: 'a elt list -> 'a t
+    val of_seq: 'a elt Seq.t -> 'a t
+    val iter: ('a elt -> unit) -> 'a t -> unit
+    val fold: ('acc -> 'a elt -> 'acc) -> 'acc -> 'a t -> 'acc
+    val to_seq: 'a t -> 'a elt Seq.t
+  end
+(** Functor building an implementation of priority queues
+    given a totally ordered type of elements. This is convenient when
+    the type of elements is already equipped with a comparison function.
+    For instance, we get priority queues with integer elements as follows:
+
+{[
+  module IntPQ = Pqueue.MakePoly(struct
+    type _ t = int
+    let compare = Int.compare
+  end)
+]}
+
+    Note that we cannot apply functor [MakePoly] to module [Int] directly,
+    as the type of elements is polymorphic. *)

vendor/opam/seq/testsuite/tests/lib-pqueue/test.ml
@@ -1,6 +1,6 @@
 (* TEST *)
 
-module Q = Pqueue.Make(Int)
+module Q = Pqueue.MinQueue
 
 let does_raise f q =
   try
@@ -18,25 +18,25 @@ let check_is_empty q =
 let () =
   let q = Q.create () in
   check_is_empty q;
-  Q.add q 1;
+  Q.add q 1 "a";
   assert (Q.length q = 1); assert (not (Q.is_empty q));
-  assert (Q.min_elt q = 1); assert (Q.min_elt_opt q = Some 1);
+  assert (Q.min_elt q = (1, "a")); assert (Q.min_elt_opt q = Some (1, "a"));
   assert (Q.length q = 1);
-  assert (Q.pop_min q = 1); check_is_empty q;
-  Q.add q 2;
-  Q.add q 1;
-  assert (Q.min_elt q = 1); assert (Q.min_elt_opt q = Some 1);
-  assert (Q.pop_min_opt q = Some 1); assert (Q.length q = 1);
-  assert (Q.min_elt q = 2); assert (Q.min_elt_opt q = Some 2);
+  assert (Q.pop_min q = (1, "a")); check_is_empty q;
+  Q.add q 2 "b";
+  Q.add q 1 "a";
+  assert (Q.min_elt q = (1, "a")); assert (Q.min_elt_opt q = Some (1, "a"));
+  assert (Q.pop_min_opt q = Some (1, "a")); assert (Q.length q = 1);
+  assert (Q.min_elt q = (2, "b")); assert (Q.min_elt_opt q = Some (2, "b"));
   Q.remove_min q; check_is_empty q;
-  Q.add q 2;
-  Q.add q 1;
+  Q.add q 2 "b";
+  Q.add q 1 "a";
   Q.clear q; check_is_empty q;
   ()
 
 let () =
   let q = Q.create () in
-  Q.add q 1; Q.add q 2;
+  Q.add q 1 "a"; Q.add q 2 "b";
   let q' = Q.copy q in
   Q.clear q; check_is_empty q;
   assert (Q.length q' = 2)
@@ -44,48 +44,74 @@ let () =
 let () =
   let q = Q.create () in
   for n = 0 to 10 do
-    for x = n-1 downto 0 do Q.add q x done;
-    for x = 0 to n-1 do assert (Q.pop_min q = x) done;
+    for x = n-1 downto 0 do Q.add q x () done;
+    for x = 0 to n-1 do assert (Q.pop_min q = (x, ())) done;
     check_is_empty q
   done
 
 let () =
   let q = Q.create () in
   for n = 0 to 10 do
-    for x = n-1 downto 0 do Q.add q x done;
-    for x = 0 to n-1 do assert (Q.pop_min q = x) done;
+    for x = n-1 downto 0 do Q.add q x () done;
+    for x = 0 to n-1 do assert (Q.pop_min q = (x, ())) done;
     check_is_empty q
   done
 
 let () =
   for n = 0 to 10 do
-    let a = Array.init n (fun i -> i/3) in
+    let a = Array.init n (fun i -> (i/3, string_of_int i)) in
     let q = Q.of_array a in
     assert (Q.length q = n);
-    for i = 0 to n - 1 do assert (Q.pop_min_opt q = Some a.(i)) done;
+    for i = 0 to n - 1 do match Q.pop_min_opt q with
+                          | None -> assert false
+                          | Some (x, _) -> assert (x = fst a.(i))
+    done;
     check_is_empty q
   done
 
 let () =
   let q = Q.create () in
-  let s = List.to_seq [2;3;1;4;0] in
+  let s = List.to_seq [2, "b"; 3, "c"; 1, "a"; 4, "d"; 0, ""] in
   Q.add_seq q s;
-  assert (Q.min_elt q = 0);
-  assert (Q.fold (+) 0 q = 10)
+  assert (Q.min_elt q = (0, ""));
+  assert (Q.fold (fun acc x _ -> acc+x) 0 q = 10)
 
-(* check that min_elt and pop_min are consistent
-   (we cannot do that with integers) *)
-module Qs = Pqueue.Make(struct
-  type t = int * string
-  let compare x y = Int.compare (fst x) (fst y)
-end)
-
+(* check that min_elt and pop_min are consistent when several elements
+   have the same priority *)
 let () =
-  let q = Qs.create () in
-  Qs.add q (1, "b"); Qs.add q (1, "a"); Qs.add q (1, "d"); Qs.add q (1, "c");
+  let q = Q.create () in
+  Q.add q 1 "b"; Q.add q 1 "a"; Q.add q 1 "d"; Q.add q 1 "c";
   for _ = 1 to 4 do
-    let x = Qs.min_elt q in assert (x = Qs.pop_min q)
+    let x = Q.min_elt q in assert (x = Q.pop_min q)
   done;
   ()
+
+(* check that MaxQueue is indeed a max-pqueue *)
+let () =
+  let open Pqueue.MaxQueue in
+  let q = create () in
+  add q 2 "b"; add q 1 "a"; add q 4 "d"; add q 3 "c";
+  for i = 4 downto 1 do let x = pop_min q in assert (fst x = i) done
+
+let () =
+  let module E = struct type _ t = string let compare = String.compare end in
+  let open Pqueue.MakePoly(E) in
+  let a = [| "b"; "bar"; "a"; "aa"; "foo"; "ocaml" |] in
+  let n = Array.length a in
+  let q = create () in
+  for i = 0 to n - 1 do add q a.(i) done;
+  assert (length q = n);
+  Array.sort String.compare a;
+  for i = 0 to n - 1 do match pop_min_opt q with
+                          | None -> assert false
+                          | Some x -> assert (x = a.(i))
+  done;
+  assert (is_empty q)
+
+(* check that the code snippet in the documentation type-checks *)
+module IntPQ = Pqueue.MakePoly(struct
+    type _ t = int
+    let compare = Int.compare
+  end)
 
 let () = print_endline "OK"