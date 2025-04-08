ocaml/opam

Fix "opam install --deps/--show <local_dir>" not updating pinned packages' metadata
The reinstall field from the switch state wasn't updated according to
the implicit pin and thus opam didn't think there was anything to do.

src/client/opamAuxCommands.ml
@@ -386,6 +386,29 @@ let simulate_local_pinnings ?quiet ?(for_view=false) st to_pin =
            st.switch_global st.switch st.switch_config ~pinned
            ~opams:local_opams)
     );
+    reinstall = lazy (
+      let open OpamPackage.Set.Op in
+      let installed_pinned = st.pinned %% st.installed in
+      OpamPackage.Set.fold (fun pinned_pkg reinstall ->
+          match
+            OpamPackage.Set.find_opt
+              (fun pkg ->
+                 OpamPackage.Name.equal
+                   (OpamPackage.name pinned_pkg)
+                   (OpamPackage.name pkg))
+              local_packages
+          with
+          | None -> reinstall
+          | Some to_pin ->
+            let old_opam = OpamPackage.Map.find pinned_pkg st.installed_opams in
+            let new_opam = OpamPackage.Map.find to_pin local_opams in
+            if OpamFile.OPAM.effectively_equal old_opam new_opam then
+              reinstall
+            else
+              OpamPackage.Set.add to_pin
+                (OpamPackage.Set.remove pinned_pkg reinstall))
+        installed_pinned (Lazy.force st.reinstall)
+    );
     pinned;
   } in
   st, local_packages

avsm/unpac

vendor/opam/opam-core/src/client/opamAuxCommands.ml
@@ -401,6 +401,29 @@ let simulate_local_pinnings ?quiet ?(for_view=false) st to_pin =
            st.switch_global st.switch st.switch_config ~pinned
            ~opams:local_opams)
     );
+    reinstall = lazy (
+      let open OpamPackage.Set.Op in
+      let installed_pinned = st.pinned %% st.installed in
+      OpamPackage.Set.fold (fun pinned_pkg reinstall ->
+          match
+            OpamPackage.Set.find_opt
+              (fun pkg ->
+                 OpamPackage.Name.equal
+                   (OpamPackage.name pinned_pkg)
+                   (OpamPackage.name pkg))
+              local_packages
+          with
+          | None -> reinstall
+          | Some local_pkg ->
+            let old_opam = OpamPackage.Map.find pinned_pkg st.installed_opams in
+            let new_opam = OpamPackage.Map.find local_pkg local_opams in
+            if OpamFile.OPAM.effectively_equal old_opam new_opam then
+              reinstall
+            else
+              OpamPackage.Set.add local_pkg
+                (OpamPackage.Set.remove pinned_pkg reinstall))
+        installed_pinned (Lazy.force st.reinstall)
+    );
     pinned;
   } in
   st, local_packages