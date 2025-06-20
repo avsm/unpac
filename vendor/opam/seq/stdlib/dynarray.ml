ocaml/ocaml

Dynarray.rev_iter, rev_iteri

stdlib/dynarray.ml
@@ -611,6 +611,22 @@ let iteri k a =
   done;
   check_same_length "iteri" a ~length
 
+let rev_iter k a =
+  let {arr; length} = a in
+  check_valid_length length arr;
+  for i = length - 1 downto 0 do
+    k (unsafe_get arr ~i ~length);
+  done;
+  check_same_length "rev_iter" a ~length
+
+let rev_iteri k a =
+  let {arr; length} = a in
+  check_valid_length length arr;
+  for i = length - 1 downto 0 do
+    k i (unsafe_get arr ~i ~length);
+  done;
+  check_same_length "rev_iteri" a ~length
+
 let map f a =
   let {arr; length} = a in
   check_valid_length length arr;

avsm/unpac

vendor/opam/seq/stdlib/dynarray.ml
@@ -915,6 +915,22 @@ let iteri f a =
   done;
   check_same_length "iteri" a ~length
 
+let rev_iter f a =
+  let Pack {arr; length; dummy} = a in
+  check_valid_length length arr;
+  for i = length - 1 downto 0 do
+    f (unsafe_get arr ~dummy ~i ~length);
+  done;
+  check_same_length "rev_iter" a ~length
+
+let rev_iteri f a =
+  let Pack {arr; length; dummy} = a in
+  check_valid_length length arr;
+  for i = length - 1 downto 0 do
+    f i (unsafe_get arr ~dummy ~i ~length);
+  done;
+  check_same_length "rev_iteri" a ~length
+
 let map f a =
   let Pack {arr = arr_in; length; dummy} = a in
   check_valid_length length arr_in;