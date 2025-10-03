ocaml/opam

Preserve the diffs from OpamFilename.patch and propagate to OpamRepository.update

src/core/opamFilename.mli
@@ -276,13 +276,16 @@ val remove_prefix_dir: Dir.t -> Dir.t -> string
 (** Remove a suffix from a filename *)
 val remove_suffix: Base.t -> t -> string
 
-(** Apply a patch in a directory. If [preprocess] is set to false, there is no
-    CRLF translation. Returns [None] on success, the process error otherwise.
+(** [patch ~allow_unclean patch_source dir] applies a patch to directory [dir].
+    The patch source can be either [`Patch_file filename] for a patch file, or
+    [`Patch_diffs diffs] for a list of file-level changes.
 
     @param allow_unclean decides if applying a patch on a directory which
     differs slightly from the one described in the patch file is allowed.
     Allowing unclean applications imitates the default behaviour of GNU Patch. *)
-val patch: ?preprocess:bool -> allow_unclean:bool -> t -> Dir.t -> exn option
+val patch: allow_unclean:bool ->
+  [`Patch_file of t | `Patch_diffs of Patch.t list ] -> Dir.t ->
+  (Patch.operation list, exn) result
 
 (** Create an empty file *)
 val touch: t -> unit
@@ -374,3 +377,8 @@ end
 
 (** Convert a filename to an attribute, relatively to a root *)
 val to_attribute: Dir.t -> t -> Attribute.t
+
+(** [parse_patch_file ~dir patch_file] processes and parses a patch file.
+    Returns the parsed patch diffs or raises an exception if the patch file
+    doesn't exist or can't be parsed. *)
+val parse_patch_file : dir:Dir.t -> t -> Patch.t list

src/repository/opamRepositoryBackend.ml
@@ -15,7 +15,7 @@ let slog = OpamConsole.slog
 
 type update =
   | Update_full of dirname
-  | Update_patch of filename
+  | Update_patch of (filename * Patch.t list)
   | Update_empty
   | Update_err of exn
 
@@ -106,6 +106,25 @@ let get_files_for_diff parent_dir dir1 dir2 =
     in
     aux [] files1 files2
 
+(* Needed to be coherent with vcs patches that do not have repo name suffix *)
+let strip_repo_suffix d =
+  let rm_prefix f =
+    match OpamStd.String.cut_at f '/' with
+    | None ->
+      log "Warning: failed to remove prefix of %s" f;
+      f
+    | Some (_, r) -> r
+  in
+  let operation =
+    match d.Patch.operation with
+    | Patch.Create f -> Patch.Create (rm_prefix f)
+    | Patch.Delete f -> Patch.Delete (rm_prefix f)
+    | Patch.Edit (f1, f2) -> Patch.Edit (rm_prefix f1, rm_prefix f2)
+    | Patch.Git_ext (f1, f2, ext) ->
+      Patch.Git_ext (rm_prefix f1, rm_prefix f2, ext)
+  in
+  {d with operation}
+
 let get_diff parent_dir dir1 dir2 =
   let chrono = OpamConsole.timer () in
   log "diff: %a/{%a,%a}"
@@ -171,8 +190,9 @@ let get_diff parent_dir dir1 dir2 =
     log "Internal diff (empty) done in %.2fs." (chrono ());
     None
   | diffs ->
-    log "Internal diff (non-empty) done in %.2fs." (chrono ());
+    log "Internal diff (non-empty, %a changed files) done in %.2fs."
+      (fun fmt l -> Format.pp_print_int fmt (List.length l)) diffs (chrono ());
     let patch = OpamSystem.temp_file ~auto_clean:false "patch" in
     let patch_file = OpamFilename.of_string patch in
     OpamFilename.write patch_file (Format.asprintf "%a" Patch.pp_list diffs);
-    Some patch_file
+    Some (patch_file, List.map strip_repo_suffix diffs)

src/repository/opamVCS.ml
@@ -52,7 +52,13 @@ module Make (VCS: VCS) = struct
         (VCS.diff repo_root repo_url)
       @@| function
       | None -> OpamRepositoryBackend.Update_empty
-      | Some patch -> OpamRepositoryBackend.Update_patch patch
+      | Some patch_file ->
+        try
+          let diffs =
+            OpamFilename.parse_patch_file ~dir:repo_root patch_file
+          in
+          OpamRepositoryBackend.Update_patch (patch_file, diffs)
+        with exn -> Update_err exn
     else
       OpamProcess.Job.catch (fun e ->
           OpamFilename.rmdir repo_root;

avsm/unpac

vendor/opam/opam-core/src/core/opamFilename.mli
@@ -276,13 +276,21 @@ val remove_prefix_dir: Dir.t -> Dir.t -> string
 (** Remove a suffix from a filename *)
 val remove_suffix: Base.t -> t -> string
 
-(** Apply a patch in a directory. If [preprocess] is set to false, there is no
-    CRLF translation. Returns [None] on success, the process error otherwise.
+(** [patch ~allow_unclean patch_source dir] applies a patch to directory [dir].
+    The patch source can be either [`Patch_file filename] for a patch file, or
+    [`Patch_diffs diffs] for a list of file-level changes.
 
     @param allow_unclean decides if applying a patch on a directory which
     differs slightly from the one described in the patch file is allowed.
     Allowing unclean applications imitates the default behaviour of GNU Patch. *)
-val patch: ?preprocess:bool -> allow_unclean:bool -> t -> Dir.t -> exn option
+val patch: allow_unclean:bool ->
+  [`Patch_file of t | `Patch_diffs of Patch.t list ] -> Dir.t ->
+  (Patch.operation list, exn) result
+
+(** [parse_patch ~dir patch_file] processes and parses a patch file.
+    Returns the parsed patch diffs or raises an exception if the patch file
+    doesn't exist or can't be parsed. *)
+val parse_patch : dir:Dir.t -> t -> Patch.t list
 
 (** Create an empty file *)
 val touch: t -> unit

vendor/opam/opam-core/src/repository/opamRepositoryBackend.ml
@@ -15,7 +15,7 @@ let slog = OpamConsole.slog
 
 type update =
   | Update_full of dirname
-  | Update_patch of filename
+  | Update_patch of (filename * Patch.t list)
   | Update_empty
   | Update_err of exn
 
@@ -106,6 +106,26 @@ let get_files_for_diff parent_dir dir1 dir2 =
     in
     aux [] files1 files2
 
+(* Serves to remove the repository suffix since the quarantine mechanism in
+   local and http patches causes incoherencies with vcs patches *)
+let strip_repo_suffix patch =
+  let rm_prefix f =
+    match OpamStd.String.cut_at f '/' with
+    | None ->
+      log "Internal diff: failed to remove prefix of %s" f;
+      f
+    | Some (_, r) -> r
+  in
+  let operation =
+    match patch.Patch.operation with
+    | Patch.Create f -> Patch.Create (rm_prefix f)
+    | Patch.Delete f -> Patch.Delete (rm_prefix f)
+    | Patch.Edit (f1, f2) -> Patch.Edit (rm_prefix f1, rm_prefix f2)
+    | Patch.Git_ext (f1, f2, ext) ->
+      Patch.Git_ext (rm_prefix f1, rm_prefix f2, ext)
+  in
+  {patch with operation}
+
 let get_diff parent_dir dir1 dir2 =
   let chrono = OpamConsole.timer () in
   log "diff: %a/{%a,%a}"
@@ -171,8 +191,9 @@ let get_diff parent_dir dir1 dir2 =
     log "Internal diff (empty) done in %.2fs." (chrono ());
     None
   | diffs ->
-    log "Internal diff (non-empty) done in %.2fs." (chrono ());
+    log "Internal diff (non-empty, %a changed files) done in %.2fs."
+      (slog (fun l -> string_of_int (List.length l))) diffs (chrono ());
     let patch = OpamSystem.temp_file ~auto_clean:false "patch" in
     let patch_file = OpamFilename.of_string patch in
     OpamFilename.write patch_file (Format.asprintf "%a" Patch.pp_list diffs);
-    Some patch_file
+    Some (patch_file, List.map strip_repo_suffix diffs)

vendor/opam/opam-core/src/repository/opamVCS.ml
@@ -52,7 +52,13 @@ module Make (VCS: VCS) = struct
         (VCS.diff repo_root repo_url)
       @@| function
       | None -> OpamRepositoryBackend.Update_empty
-      | Some patch -> OpamRepositoryBackend.Update_patch patch
+      | Some patch_file ->
+        try
+          let diffs =
+            OpamFilename.parse_patch ~dir:repo_root patch_file
+          in
+          OpamRepositoryBackend.Update_patch (patch_file, diffs)
+        with exn -> Update_err exn
     else
       OpamProcess.Job.catch (fun e ->
           OpamFilename.rmdir repo_root;