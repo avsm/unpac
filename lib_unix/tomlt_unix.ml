6921900a243502bdbfb4efc0ef7aaa2a00559292
Anil Madhavapeddy <anil@recoil.org>
2025-12-20 14:29:50 +0000

    metadata and cleanup

--- lib_unix/tomlt_unix.ml
+++ lib_unix/tomlt_unix.ml
@@ -7,16 +7,13 @@ module Bytes = Bytesrw.Bytes
 
 (* Time utilities *)
 let current_tz_offset_s = Ptime_clock.current_tz_offset_s
-
 let now = Ptime_clock.now
 
 let today_date ?tz_offset_s () =
-  let tz_offset_s = match tz_offset_s with
-    | Some tz -> tz
-    | None ->
-        match current_tz_offset_s () with
-        | Some tz -> tz
-        | None -> 0
+  let tz_offset_s =
+    tz_offset_s
+    |> Option.fold ~none:(current_tz_offset_s ()) ~some:Option.some
+    |> Option.value ~default:0
   in
   Ptime.to_date ~tz_offset_s (now ())
 
